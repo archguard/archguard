ae8e61d9e7683ea7ec94217ef3ce14dc6995d3cb
yoogodong <yoogodong@gmail.com>
2020-02-14 00:15:11 +0800

    use service to coordinate  gitAdapter and DBRepository

--- scan_git/src/main/kotlin/Service.kt
+++ scan_git/src/main/kotlin/Service.kt
@@ -5,4 +5,9 @@ package com.thoughtworks.archguard.git.scanner
 *
 * */
 
-class ScannerService(val gitAdapter: GitAdapter, val dbRepository: DBRepository)
+class ScannerService(private val gitAdapter: GitAdapter, private val dbRepository: DBRepository) {
+    fun scan(config: Config) {
+        val gitRepository = gitAdapter.scan(config)
+        dbRepository.saveGitRepository(gitRepository)
+    }
+}
