32d0d0e312e632fe501885ff155d5d18cac58a18
xiang.li2 <xiang.li2@thoughtworks.com>
2020-03-04 13:33:34 +0800

    add jdbi inject

---
+++ dependence_package/src/main/kotlin/com/thoughtworks/archguard/archguardaddition/infrostracture/DB.kt
@@ -0,0 +1,30 @@
+package com.thoughtworks.archguard.archguardaddition.infrostracture
+
+import org.jdbi.v3.core.Jdbi
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.datasource.DriverManagerDataSource
+
+
+@Configuration
+class DB {
+
+    @Value("\${spring.datasource.url}")
+    private lateinit var url: String
+
+    @Value("\${spring.datasource.username}")
+    private lateinit var user: String
+
+    @Value("\${spring.datasource.password}")
+    private lateinit var password: String
+
+    @Bean
+    fun getDBI(): Jdbi {
+        val dataSource = DriverManagerDataSource()
+        dataSource.url = url
+        dataSource.username = user
+        dataSource.password = password
+        return Jdbi.create(dataSource)
+    }
+}
