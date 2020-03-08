e1148261d926c3e4499764e53379162274a20148
yoogodong <yoogodong@gmail.com>
2020-03-09 00:57:29 +0800

    integrate spring with jdbi

--- git_commit/src/main/kotlin/Runner.kt
+++ git_commit/src/main/kotlin/Runner.kt
@@ -1,15 +1,24 @@
 package com.thoughtworks.archguard.git.analyzer
 
+import org.jdbi.v3.spring4.JdbiFactoryBean
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import javax.sql.DataSource
 
 @SpringBootApplication
 open class Runner : ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
     }
 
+
+    @Bean
+    open fun jdbiFactory(@Autowired ds: DataSource): JdbiFactoryBean {
+        return JdbiFactoryBean(ds)
+    }
 
 }
 
--- git_commit/src/test/kotlin/CommonTest.kt
+++ git_commit/src/test/kotlin/CommonTest.kt
@@ -1,13 +1,41 @@
 import com.thoughtworks.archguard.git.analyzer.Runner
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.spring4.JdbiFactoryBean
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest(classes = [Runner::class])
 class CommonTest {
+
+    @Autowired
+    private lateinit var factory: JdbiFactoryBean
+
+    private var jdbi: Jdbi? = null
+
+
+    @BeforeEach
+    internal fun setUp() {
+        jdbi = factory.`object`
+    }
+
     @Test
     internal fun testJupiterConfig() {
         assertTrue(true)
+
+        val result = jdbi!!.withHandle<Int, Exception> { handle ->
+            handle.execute("create table mytest(id int)")
+            handle.execute("insert into mytest(id) values(?) ", 100)
+
+            handle.createQuery("select * from mytest")
+                    .mapTo(Int::class.java)
+                    .one()
+        }
+
+        assertEquals(100, result)
     }
 
 
