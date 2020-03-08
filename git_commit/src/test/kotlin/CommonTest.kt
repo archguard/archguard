5cb3cdb3c19963ee2f317c1b226823ff3d4cfc5a
yoogodong <yoogodong@gmail.com>
2020-03-08 22:16:20 +0800

    setup junit5

---
+++ git_commit/src/test/kotlin/CommonTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class CommonTest {
+    @Test
+    internal fun testJupiterConfig() {
+        assertTrue(true)
+    }
+}
