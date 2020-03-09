fde85e93f6298cb24a4bcdf578a54892879cc557
yoogodong <yoogodong@gmail.com>
2020-03-09 09:07:48 +0800

    create ddl for mysql

---
+++ scan_git/src/main/resources/ddl.sql
@@ -0,0 +1,31 @@
+-- for mysql
+
+
+drop table if exists GitRepository;
+create table GitRepository(
+    id bigint primary key,
+    rep_path  varchar(300) not null,
+    branch  varchar(100) not null
+);
+
+drop table if exists Commit;
+create table Commit(
+    id varchar(50) primary key,
+    commit_time  int,
+    committer_name varchar(50),
+    commit_email varchar(100),
+    rep_id bigint
+);
+
+
+drop table if exists ChangeEntry;
+create table ChangeEntry(
+    old_path varchar(200),
+    new_path varchar(200),
+    cognitiveComplexity  int,
+    mode  varchar(10),
+    commit_id varchar(50)
+);
+
+
+
