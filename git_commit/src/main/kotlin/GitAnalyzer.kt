3ab9a5fc97027fa4689c350b05d44da94109ced4
yoogodong <yoogodong@gmail.com>
2020-03-10 16:20:04 +0800

    select RevCommit

--- git_commit/src/main/kotlin/GitAnalyzer.kt
+++ git_commit/src/main/kotlin/GitAnalyzer.kt
@@ -1,6 +1,7 @@
 package com.thoughtworks.archguard.git.analyzer
 
 import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.mapper.RowMapper
 import org.jdbi.v3.spring4.JdbiFactoryBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
@@ -23,9 +24,23 @@ class GitAnalyzerByJdbi(@Autowired val jdbiFactoryBean: JdbiFactoryBean) : GitAn
 
     override fun findScatterCommits(): List<RevCommit> {
         return jdbi.withHandle<List<RevCommit>, Exception> {
-            it.createQuery("select * from RevCommit")
-                    .mapToBean(RevCommit::class.java)
-                    .list()
+            val commitMap = RowMapper { rs, ctx ->
+                val commitId = rs.getString("id")
+
+
+
+                RevCommit(id = commitId,
+                        commit_time = rs.getInt("commit_time"),
+                        committer_name = rs.getString("committer_name"),
+                        rep_id = rs.getLong("rep_id"),
+                        entries = setOf(ChangeEntry("", 4, "ADD"))
+                )
+            }
+
+
+            val queryCommit = "select id, commit_time, committer_name, rep_id from RevCommit"
+            it.createQuery(queryCommit)
+                    .map(commitMap).list()
         }
     }
 
