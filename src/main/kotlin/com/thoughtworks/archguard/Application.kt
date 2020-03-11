70250bf301eceaebce1197d0ed0360e892cd5227
hbzhu <hbzhu@thoughtworks.com>
2020-03-11 18:15:13 +0800

    [fix] add clean step in Jenkinsfile.

--- src/main/kotlin/com/thoughtworks/archguard/Application.kt
+++ src/main/kotlin/com/thoughtworks/archguard/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class PackageApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<PackageApplication>(*args)
+	runApplication<Application>(*args)
 }
--- src/test/kotlin/com/thoughtworks/archguard/ApplicationTests.kt
+++ src/test/kotlin/com/thoughtworks/archguard/ApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class PackageApplicationTests {
+class ApplicationTests {
 
 	@Test
 	fun contextLoads() {
