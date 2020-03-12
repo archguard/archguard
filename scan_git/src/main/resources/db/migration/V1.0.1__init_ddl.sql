84727d1329756ebe1a70764b07438a6af7f24339
yoogodong <yoogodong@gmail.com>
2020-03-12 22:13:57 +0800

    add these two columns to optimize performance  in the future  by log the analyzed data

--- scan_git/src/main/resources/db/migration/V1.0.1__init_ddl.sql
+++ scan_git/src/main/resources/db/migration/V1.0.1__init_ddl.sql
@@ -22,7 +22,9 @@ create table ChangeEntry(
     new_path varchar(200),
     cognitiveComplexity  int,
     mode  varchar(10),
-    commit_id varchar(50)
+    commit_id varchar(50),
+    prvs_cmt_id varchar(50),    -- previous commit id  TODO: 待优化，在分析的时候保存下来这两个值
+    prvs_cgn_cmplxty int        -- previous cognitive complexity
 );
 
 
