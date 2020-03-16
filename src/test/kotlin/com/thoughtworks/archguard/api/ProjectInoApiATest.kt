0d1a0bc730db2f50c1ec6a7eaa73ad48edf53bb2
xiang.li2 <xiang.li2@thoughtworks.com>
2020-03-16 15:21:10 +0800

    fix test error

--- src/test/kotlin/com/thoughtworks/archguard/api/ProjectInoApiATest.kt
+++ src/test/kotlin/com/thoughtworks/archguard/api/ProjectInoApiATest.kt
@@ -31,7 +31,7 @@ class ProjectInoApiATest {
                 .andReturn()
 
         val content = result.response.contentAsString
-        val status = result.response.contentAsString
+        val status = result.response.status
 
         val except = "{\"id\":\"c06da91f-6742-11ea-8188-0242ac110002\",\"projectName\":\"spring\",\"gitRepo\":\"https://github.com/spring-projects/spring-framework.git\"}"
 
--- src/test/resources/db/migration/V20200316125148__add_ProjectInfo_data.sql
+++ src/test/resources/db/migration/V20200316125148__add_ProjectInfo_data.sql
@@ -1,2 +1,2 @@
 insert into ProjectInfo(id, name, repo, updatedAt, createdAt)
-values (UUID(), 'spring', 'https://github.com/spring-projects/spring-framework.git', NOW(), NOW())
+values ('c06da91f-6742-11ea-8188-0242ac110002', 'spring', 'https://github.com/spring-projects/spring-framework.git', NOW(), NOW())
