bc6b7c8ba6eabb37f86b40c8badfc9660dd056b5
xiang.li2 <xiang.li2@thoughtworks.com>
2020-03-16 10:28:24 +0800

    move hello api from dependence_package to hello

--- src/main/kotlin/com/thoughtworks/archguard/hello/HelloController.kt
+++ src/main/kotlin/com/thoughtworks/archguard/hello/HelloController.kt
@@ -1,4 +1,4 @@
-package com.thoughtworks.archguard.dependence_package.controller
+package com.thoughtworks.archguard.hello
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
