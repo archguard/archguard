42b48051dcc7048a045a1ed17afb2068a349916f
Hu Le <le.hu.2010@163.com>
2020-03-18 10:40:23 +0800

    feat: add coca scanner

---
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/toolscanners/ScannerLifecycle.kt
@@ -0,0 +1,10 @@
+package com.thoughtworks.archgard.scanner.domain.toolscanners
+
+import com.thoughtworks.archgard.scanner.domain.ScanContext
+
+internal interface ScannerLifecycle {
+    fun preProcess(context: ScanContext?)
+    fun scan(context: ScanContext?)
+    fun storeReport(context: ScanContext?)
+    fun clean(context: ScanContext?)
+}
