d76a31e12acc26bca4532e7d6dbe3dfa03f8ded2
Hu Le <le.hu.2010@163.com>
2020-03-19 22:52:15 +0800

    feat: add flyway

--- src/test/kotlin/com/thoughtworks/archgard/scanner/infrastructure/db/BadSmellRepoImplTest.kt
+++ src/test/kotlin/com/thoughtworks/archgard/scanner/infrastructure/db/BadSmellRepoImplTest.kt
@@ -1,6 +1,9 @@
 package com.thoughtworks.archgard.scanner.infrastructure.db
 
 import com.thoughtworks.archgard.scanner.domain.bs.BadSmell
+import org.jdbi.v3.core.Handle
+import org.jdbi.v3.core.Jdbi
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -8,12 +11,18 @@ import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
 @ActiveProfiles("test")
-internal class BadSmellRepoImplTest(@Autowired val badSmellRepoImpl: BadSmellRepoImpl) {
+internal class BadSmellRepoImplTest(@Autowired val badSmellRepoImpl: BadSmellRepoImpl, @Autowired val jdbi: Jdbi) {
 
     @Test
     fun save() {
         badSmellRepoImpl.save(listOf(
                 BadSmell("1", "enity_name", 11, "description", 3, "longMethod"),
                 BadSmell("2", "enity_name1", 13, "description1", 43, "longMethod")))
+
+        val count = jdbi.withHandle<Int, RuntimeException> { handle: Handle ->
+            handle.createQuery("select count(*) from badSmell")
+                    .mapTo(Int::class.java).one()
+        }
+        assertEquals(count, 2)
     }
 }
