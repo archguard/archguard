fe5c0e68e1c228f45e949a5f153b7b8a0ab6e225
Hu Le <le.hu.2010@163.com>
2020-03-19 15:26:58 +0800

    feat: correct conflict

--- src/main/kotlin/com/thoughtworks/archgard/scanner/domain/ScanContext.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/ScanContext.kt
@@ -1,18 +1,10 @@
 package com.thoughtworks.archgard.scanner.domain
 
-<<<<<<< HEAD
+import java.io.File
+
 class ScanContext {
     var repo: String = ""
     var workspace: String = ""
     var sourcePath: String = ""
-=======
-import java.io.File
-
-<<<<<<< HEAD
-class ScanContext {
-    val projectRoot: File = TODO("initialize me")
->>>>>>> feat: add badsmell
-=======
-data class ScanContext(val projectRoot: File) {
->>>>>>> feat: add test for bad smell
+    var projectRoot: File? = null
 }
