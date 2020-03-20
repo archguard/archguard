f9d3f2d674dfbadd0b0ae03c6e064b9368f77cd3
Hu Le <le.hu.2010@163.com>
2020-03-20 09:32:34 +0800

    feat: move config to application

--- src/main/kotlin/com/thoughtworks/archguard/Application.kt
+++ src/main/kotlin/com/thoughtworks/archguard/Application.kt
@@ -1,11 +1,19 @@
 package com.thoughtworks.archguard
 
+import org.jdbi.v3.core.Jdbi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import javax.sql.DataSource
 
 @SpringBootApplication
-class Application
+class Application {
+    @Bean
+    fun getDB(dataSource: DataSource): Jdbi {
+		return Jdbi.create(dataSource)
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<Application>(*args)
+    runApplication<Application>(*args)
 }
