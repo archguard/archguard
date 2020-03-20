6ad6a4aeaeb626d66cbd0d6fc800fb307c069b33
Hu Le <le.hu.2010@163.com>
2020-03-20 17:38:04 +0800

    feat: correct bad smell

--- src/main/kotlin/com/thoughtworks/archguard/report/controller/TestBadSmellController.kt
+++ src/main/kotlin/com/thoughtworks/archguard/report/controller/TestBadSmellController.kt
@@ -1,6 +1,6 @@
 package com.thoughtworks.archguard.report.controller
 
-import com.thoughtworks.archguard.report.infrastructure.BadSmellCountDBO
+import com.thoughtworks.archguard.report.infrastructure.TestBadSmellCountDBO
 import com.thoughtworks.archguard.report.infrastructure.TestBadSmellRepo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 class TestBadSmellController(@Autowired val testBadSmellRepo: TestBadSmellRepo) {
 
     @GetMapping("/reports/test-bad-smells")
-    fun getBadSmellReport(): List<BadSmellCountDBO> {
+    fun getBadSmellReport(): List<TestBadSmellCountDBO> {
         return testBadSmellRepo.getTestBadSmellCount()
     }
 
