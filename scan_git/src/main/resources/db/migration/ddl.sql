382207571b7bf7855b508698399211488adae39b
yoogodong <yoogodong@gmail.com>
2020-03-20 16:07:34 +0800

    add index change_entry(new_path)

--- scan_git/src/main/resources/db/migration/ddl.sql
+++ scan_git/src/main/resources/db/migration/ddl.sql
@@ -28,7 +28,8 @@ create table change_entry(
     chng_mode           varchar(10),
     cmt_id              varchar(50),
     prvs_cmt_id         varchar(50),    -- previous commit id  TODO: 待优化，在分析的时候保存下来这两个值
-    prvs_cgn_cmplxty    int        -- previous cognitive complexity
+    prvs_cgn_cmplxty    int,        -- previous cognitive complexity
+    index new_path (new_path)
 );
 
 
