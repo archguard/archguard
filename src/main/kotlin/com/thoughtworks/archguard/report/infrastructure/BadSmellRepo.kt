1d70866379ca837a8660ab5acebd717f879408d6
Hu Le <le.hu.2010@163.com>
2020-03-20 12:11:08 +0800

    feat: add row mapper

--- src/main/kotlin/com/thoughtworks/archguard/report/infrastructure/BadSmellRepo.kt
+++ src/main/kotlin/com/thoughtworks/archguard/report/infrastructure/BadSmellRepo.kt
@@ -1,6 +1,8 @@
 package com.thoughtworks.archguard.report.infrastructure
 
+import com.thoughtworks.archguard.dependence.infrastructure.modules.ModuleDependenceDTO
 import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.mapper.reflect.ConstructorMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 
@@ -9,8 +11,9 @@ class BadSmellRepo(@Autowired private val jdbi: Jdbi) {
 
     fun getBadSmellCount(): List<BadSmellCountDBO> {
         return jdbi.withHandle<List<BadSmellCountDBO>, Nothing> { handle ->
+            handle.registerRowMapper(ConstructorMapper.factory(BadSmellCountDBO::class.java))
             handle.createQuery("select type, count(id) as size from badSmell group by type")
-                    .mapToBean(BadSmellCountDBO::class.java)
+                    .mapTo(BadSmellCountDBO::class.java)
                     .list()
         }
     }
