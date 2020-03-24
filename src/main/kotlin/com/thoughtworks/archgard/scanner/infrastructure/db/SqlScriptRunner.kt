540f7a6d8cb733fcb1802e2edffb73e861499377
Hu Le <le.hu.2010@163.com>
2020-03-24 11:26:00 +0800

    feat: add git tables

--- src/main/kotlin/com/thoughtworks/archgard/scanner/infrastructure/db/SqlScriptRunner.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/infrastructure/db/SqlScriptRunner.kt
@@ -9,4 +9,8 @@ class SqlScriptRunner(val jdbi: Jdbi) {
     fun run(sql: File) {
         jdbi.open().use { h -> h.createScript(sql.readText()).execute() }
     }
+
+    fun run(sql: String) {
+        jdbi.open().use { h -> h.createScript(sql).execute() }
+    }
 }
