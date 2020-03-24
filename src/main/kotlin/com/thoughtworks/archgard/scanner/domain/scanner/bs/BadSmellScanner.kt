fbf94b2f7632ff5fbd4edc5c6d4a656f715a1cbb
Hu Le <le.hu.2010@163.com>
2020-03-24 18:22:21 +0800

    feat: add designite Java bad smell

--- src/main/kotlin/com/thoughtworks/archgard/scanner/domain/scanner/bs/BadSmellScanner.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/scanner/bs/BadSmellScanner.kt
@@ -6,14 +6,14 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.thoughtworks.archgard.scanner.domain.ScanContext
 import com.thoughtworks.archgard.scanner.domain.scanner.Scanner
 import com.thoughtworks.archgard.scanner.domain.tools.CocaTool
-import com.thoughtworks.archgard.scanner.infrastructure.Processor
+import com.thoughtworks.archgard.scanner.domain.tools.DesigniteJavaTool
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class BadSmellScanner( @Autowired val badSmellRepo: BadSmellRepo) : Scanner {
+class BadSmellScanner(@Autowired val badSmellRepo: BadSmellRepo) : Scanner {
 
     private val log = LoggerFactory.getLogger(BadSmellScanner::class.java)
 
@@ -21,12 +21,35 @@ class BadSmellScanner( @Autowired val badSmellRepo: BadSmellRepo) : Scanner {
 
     override fun scan(context: ScanContext) {
         log.info("start scan bad smell report")
-        val cocaTool = CocaTool(context.workspace)
-        val report = cocaTool.getBadSmellReport()
-        val badSmell = mapper.readValue<CocaBadSmellModel>(report?.readText()?:"{}").toBadSmell()
+        var badSmell = getCocaBadSmell(context)
+        if (badSmell.isEmpty()) {
+            badSmell = getDesigniteJavaBadSmell(context)
+        }
         badSmellRepo.save(badSmell)
 
         log.info("finished scan bad smell report")
+    }
+
+    private fun getDesigniteJavaBadSmell(context: ScanContext): List<BadSmell> {
+        val designiteJavaTool = DesigniteJavaTool(context.workspace)
+        val badSmellReport = designiteJavaTool.getBadSmellReport()
+        val lines = badSmellReport?.readLines()
+        val readLines = lines?.subList(1, lines.size).orEmpty()
+
+        val result = ArrayList<BadSmell>()
+        for (line in readLines) {
+            val elements = line.split(",")
+            result.add(BadSmell(UUID.randomUUID().toString(), elements[1] + "." + elements[2], 0,
+                    elements[3], 0, elements[3]))
+        }
+        return result
+    }
+
+    private fun getCocaBadSmell(context: ScanContext): List<BadSmell> {
+        val cocaTool = CocaTool(context.workspace)
+        val report = cocaTool.getBadSmellReport()
+        val cocaModels = mapper.readValue<CocaBadSmellModel>(report?.readText() ?: "{}")
+        return cocaModels.toBadSmell()
     }
 
     data class CocaBadSmellModel(val complexCondition: List<CocaBadSmellItem>?,
---
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/tools/DesigniteJavaTool.kt
@@ -0,0 +1,42 @@
+package com.thoughtworks.archgard.scanner.domain.tools
+
+import com.thoughtworks.archgard.scanner.infrastructure.FileOperator
+import com.thoughtworks.archgard.scanner.infrastructure.Processor
+import java.io.File
+import java.net.URL
+
+class DesigniteJavaTool(val projectRoot: File) : BadSmellReport {
+
+    override fun getBadSmellReport(): File? {
+        val report = File(projectRoot.toString() + "/designCodeSmells.csv")
+        if (report.exists()) {
+            return report
+        }
+        process()
+        return if (report.exists()) {
+            report
+        } else {
+            null
+        }
+    }
+
+    private fun process() {
+        download()
+        scan(listOf("java", "-jar", "DesigniteJava.jar", "-i", ".", "-o", "."))
+    }
+
+
+    private fun download() {
+        val downloadUrl = "http://ci.archguard.org/job/DesigniteJava/lastSuccessfulBuild/artifact/target/DesigniteJava.jar"
+        FileOperator.download(URL(downloadUrl), File(projectRoot.toString() + "/DesigniteJava.jar"))
+        val chmod = ProcessBuilder("chmod", "+x", "DesigniteJava.jar")
+        chmod.directory(projectRoot)
+        chmod.start().waitFor()
+    }
+
+    private fun scan(cmd: List<String>) {
+        Processor.executeWithLogs(ProcessBuilder(cmd), projectRoot)
+    }
+
+
+}
