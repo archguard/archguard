14b27827f006f6f3d1a5c8c29af4ba6316674f55
Hu Le <le.hu.2010@163.com>
2020-03-24 15:42:22 +0800

    feat: log line

--- src/main/kotlin/com/thoughtworks/archgard/scanner/infrastructure/Processor.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/infrastructure/Processor.kt
@@ -1,6 +1,7 @@
 package com.thoughtworks.archgard.scanner.infrastructure
 
 import org.slf4j.LoggerFactory
+import java.io.BufferedReader
 import java.io.File
 import java.io.InputStream
 import java.io.InputStreamReader
@@ -13,14 +14,10 @@ object Processor {
         pb.directory(workspace)
         val p = pb.start()
         val inputStream: InputStream = p.inputStream
-        //转成字符输入流
-        val inputStreamReader = InputStreamReader(inputStream, "gbk")
-        var len: Int
-        val c = CharArray(1024)
-        //读取进程输入流中的内容
-        while (inputStreamReader.read(c).also { len = it } != -1) {
-            val s = String(c, 0, len)
-            log.info(s)
+        val bufferedReader = BufferedReader(InputStreamReader(inputStream, "gbk"))
+        while (true) {
+            val line = bufferedReader.readLine() ?: break
+            log.info(line)
         }
         inputStream.close()
     }
