fc34bf9503f65ddb866f3658c29b43835ab935eb
xiang.li2 <xiang.li2@thoughtworks.com>
2020-03-24 19:08:09 +0800

    add CheckstyleScanner

---
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/scanner/checkstyle/CheckstyleScanner.kt
@@ -0,0 +1,57 @@
+package com.thoughtworks.archgard.scanner.domain.scanner.checkstyle
+
+import com.thoughtworks.archgard.scanner.domain.ScanContext
+import com.thoughtworks.archgard.scanner.domain.scanner.Scanner
+import org.dom4j.Element
+import org.dom4j.io.SAXReader
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.io.File
+import java.util.*
+import kotlin.collections.ArrayList
+
+@Service
+class CheckstyleScanner : Scanner {
+
+    @Autowired
+    lateinit var checkStyleRepo: CheckStyleRepo
+
+    lateinit var buildTool: String
+
+    lateinit var reportFile: ArrayList<String>
+
+    val reports = ArrayList<CheckStyle>()
+
+    override fun scan(context: ScanContext) {
+        getInfo()
+        analysis()
+        save()
+    }
+
+    private fun getInfo() {
+        reportFile = ArrayList()
+    }
+
+    private fun analysis() {
+        reportFile.forEach {
+            val saxReader = SAXReader()
+            val document = saxReader.read(File(it))
+            val rootElement = document.rootElement
+            rootElement.elements().map { i -> i as Element }.forEach { e ->
+                val name = e.attributeValue("name")
+                e.elements().forEach { i ->
+                    val element = i as Element
+                    val source = element.attributeValue("source")
+                    val message = element.attributeValue("message")
+                    val severity = element.attributeValue("severity")
+                    val line = element.attributeValue("line").toInt()
+                    val column = element.attributeValue("column").orEmpty().toIntOrNull() ?: 0
+                    reports.add(CheckStyle(UUID.randomUUID().toString(), name, source, message, line, column, severity))
+                }
+            }
+        }
+    }
+
+    private fun save() {
+    }
+}
