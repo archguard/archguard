65464760a9e307a319df9631288624c0ec2a7e3d
yoogodong <yoogodong@gmail.com>
2020-03-25 17:16:19 +0800

    Merge branch 'master' of /Users/ygdong/git/code-scanners with conflicts.

--- scan_git/src/test/kotlin/com/thoughtworks/archguard/git/scanner/RunnerTest.kt
+++ scan_git/src/test/kotlin/com/thoughtworks/archguard/git/scanner/RunnerTest.kt
@@ -2,15 +2,24 @@ package com.thoughtworks.archguard.git.scanner
 
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import java.io.File
+import java.util.*
 
 internal class RunnerTest {
 
+    val log= LoggerFactory.getLogger(RunnerTest::class.java);
+
     @Test
     fun run() {
+        log.info("start")
         val file = File("output.sql")
-        file.deleteOnExit();
-        Runner().main(arrayOf("--git-path=../code-scanners", "--branch=master"))
+//        file.deleteOnExit();
+        val calendar = Calendar.getInstance()
+        calendar.set(2019,1,1)
+//        Runner().main(arrayOf("--git-path=/Users/ygdong/git/spring-framework", "--branch=master","--after=${calendar.timeInMillis}"))
+//        Runner().main(arrayOf("--git-path=/Users/ygdong/git/jeesite", "--branch=master"))
         assertTrue(file.exists())
+        log.info("结束")
     }
 }
