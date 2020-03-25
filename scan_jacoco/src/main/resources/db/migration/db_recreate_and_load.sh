de66dacaf7a6522247d6dcec52706a503c59fa2a
yoogodong <yoogodong@gmail.com>
2020-03-25 19:12:16 +0800

    use relative path instead of absolute path

--- scan_jacoco/src/main/resources/db/migration/db_recreate_and_load.sh
+++ scan_jacoco/src/main/resources/db/migration/db_recreate_and_load.sh
@@ -1,2 +1,2 @@
-mysql --verbose --database=archguard --user=root -e "source /Users/ygdong/git/code-scanners/scan_jacoco/src/main/resources/db/migration/ddl.sql"
-mysql --database=archguard --user=root -e "source /Users/ygdong/git/code-scanners/scan_jacoco/output.sql"
+mysql --verbose --database=archguard --user=root -e "source ddl.sql"
+mysql --database=archguard --user=root -e "source ../../../../../output.sql"
