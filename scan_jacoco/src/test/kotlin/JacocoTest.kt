437d8adf23602a66b882f6026377bffacf7dacc6
yoogodong <yoogodong@gmail.com>
2020-03-25 19:12:44 +0800

    normalize testing code

--- scan_jacoco/src/test/kotlin/JacocoTest.kt
+++ scan_jacoco/src/test/kotlin/JacocoTest.kt
@@ -5,7 +5,6 @@ class JacocoTest {
 
     @Test
     internal fun runTest() {
-        println("0000000")
         Runner().main(arrayOf("--target-project=/Users/ygdong/git/auto_test_demo"))
     }
 
