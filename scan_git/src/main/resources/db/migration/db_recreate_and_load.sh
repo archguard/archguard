ae54aaba2d715c52e554a74519aad71cd59f6163
yoogodong <yoogodong@gmail.com>
2020-03-25 19:12:03 +0800

    use relative path instead of absolute path

--- scan_git/src/main/resources/db/migration/db_recreate_and_load.sh
+++ scan_git/src/main/resources/db/migration/db_recreate_and_load.sh
@@ -1,3 +1,2 @@
-mysql -uroot -e "drop database if exists archguard; create database archguard"
-mysql --verbose --database=archguard --user=root -e "source /Users/ygdong/git/code-scanners/scan_git/src/main/resources/db/migration/ddl.sql"
-mysql --database=archguard --user=root -e "source /Users/ygdong/git/code-scanners/scan_git/output.sql"
+mysql --verbose --database=archguard --user=root -e "source ddl.sql"
+mysql --database=archguard --user=root -e "source ../../../../../output.sql"
