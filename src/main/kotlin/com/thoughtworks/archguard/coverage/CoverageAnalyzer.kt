2c05f7a9dc55c3a2cda53660c1af1a5937b77311
yoogodong <yoogodong@gmail.com>
2020-03-25 21:30:15 +0800

    read coverage data from db

--- src/main/kotlin/com/thoughtworks/archguard/coverage/CoverageAnalyzer.kt
+++ src/main/kotlin/com/thoughtworks/archguard/coverage/CoverageAnalyzer.kt
@@ -1,27 +1,60 @@
 package com.thoughtworks.archguard.coverage
 
+import org.jdbi.v3.core.Jdbi
 import org.springframework.stereotype.Component
 
 @Component
-class CoverageAnalyzer {
+class CoverageAnalyzer(val jdbi: Jdbi) {
 
     fun analyzeExecFile(): Coverage {
-        return Coverage(1,2,3,4,5,6,7,8,9,10,11,12)
+        return jdbi.withHandle<Coverage, Exception> {
+            val queryCoverage = """
+|               select 
+|                   instruction_missed as instructionMissed, 
+|                   instruction_covered as instructionCovered, 
+                    line_missed as lineMissed,
+|                   line_covered as lineCovered, 
+                    branch_missed as branchMissed, 
+                    branch_covered as branchCovered, 
+                    complexity_missed as complexityMissed, 
+                    complexity_covered as complexityCovered, 
+                    method_missed as methodMissed,
+                    method_covered as methodCovered, 
+                    class_missed as classMissed, 
+                    class_covered as classCovered, 
+                    project, 
+                    scan_time as scanTime
+|               from coverage 
+|               order by scanTime desc
+|               limit 1
+|               """.trimMargin()
+
+
+            it.createQuery(queryCoverage)
+                    .mapToBean(Coverage::class.java)
+                    .findFirst().orElse(Coverage())
+        }
+
     }
 
 }
 
 data class Coverage(
-        val instructionMissed: Int,
-        val instructionCovered: Int,
-        val lineMissed: Int,
-        val lineCovered: Int,
-        val branchMissed: Int,
-        val branchCovered: Int,
-        val complexityMissed: Int,
-        val complexityCovered: Int,
-        val methodMissed: Int,
-        val methodCovered: Int,
-        val classMissed: Int,
-        val classCovered: Int
-)
+        var instructionMissed: Int,
+        var instructionCovered: Int,
+        var lineMissed: Int,
+        var lineCovered: Int,
+        var branchMissed: Int,
+        var branchCovered: Int,
+        var complexityMissed: Int,
+        var complexityCovered: Int,
+        var methodMissed: Int,
+        var methodCovered: Int,
+        var classMissed: Int,
+        var classCovered: Int,
+        var project: String,
+        var scanTime: Long
+) {
+    constructor() : this(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "no info in db", 0)
+}
+
