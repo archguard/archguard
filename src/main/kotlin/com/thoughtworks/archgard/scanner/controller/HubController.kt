d444f99613109574a60bd35d467c29cda7d9609e
Hu Le <le.hu.2010@163.com>
2020-03-26 11:01:35 +0800

    feat: add scan

--- src/main/kotlin/com/thoughtworks/archgard/scanner/controller/HubController.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/controller/HubController.kt
@@ -2,7 +2,7 @@ package com.thoughtworks.archgard.scanner.controller
 
 import com.thoughtworks.archgard.scanner.domain.hubexecutor.HubService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,8 +13,10 @@ class HubController {
     @Autowired
     private lateinit var hubService: HubService
 
-    @GetMapping("/do")
-    fun getModuleDependence() {
-        return hubService.doScan()
+    @PostMapping("/reports")
+    fun scanModule(): ModuleScanResponse {
+        return ModuleScanResponse(hubService.doScan())
     }
+
+    data class ModuleScanResponse(val isRunning: Boolean)
 }
--- src/main/kotlin/com/thoughtworks/archgard/scanner/domain/hubexecutor/HubService.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/hubexecutor/HubService.kt
@@ -11,20 +11,26 @@ class HubService {
     @Autowired
     private lateinit var manager: ScannerManager
 
+    private var isRunning: Boolean = false
     private val log = LoggerFactory.getLogger(HubService::class.java)
 
     @Autowired
     private lateinit var hubRepository: ProjectRepository
 
-    fun doScan() {
-        val gitRepo = hubRepository.getProjectInfo().gitRepo
-        val workspace = createTempDir()
-        log.info("workspace is: {}, gitRepo is: {}", workspace.toPath().toString(), gitRepo)
+    fun doScan(): Boolean {
+        if (!isRunning) {
+            isRunning = true
+            val gitRepo = hubRepository.getProjectInfo().gitRepo
+            val workspace = createTempDir()
+            log.info("workspace is: {}, gitRepo is: {}", workspace.toPath().toString(), gitRepo)
 
-        val config: Map<String, Any> = HashMap()
+            val config: Map<String, Any> = HashMap()
 
-        val context = ScanContext(gitRepo, workspace, config)
-        val hubExecutor = HubExecutor(context, manager)
-        hubExecutor.execute()
+            val context = ScanContext(gitRepo, workspace, config)
+            val hubExecutor = HubExecutor(context, manager)
+            hubExecutor.execute()
+            isRunning = false
+        }
+        return isRunning
     }
 }
