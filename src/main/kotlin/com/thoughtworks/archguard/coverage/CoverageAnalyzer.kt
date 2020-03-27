896bbe1fde33d08b3dfcae4c5a4527b6c3f05869
yoogodong <yoogodong@gmail.com>
2020-03-27 11:49:04 +0800

    add feature of getting range of coverage rate and topN coverage rate

--- src/main/kotlin/com/thoughtworks/archguard/coverage/CoverageAnalyzer.kt
+++ src/main/kotlin/com/thoughtworks/archguard/coverage/CoverageAnalyzer.kt
@@ -30,6 +30,31 @@ class CoverageAnalyzer(val jdbi: Jdbi) {
 
     }
 
+
+    fun countRateBetween(dmsType: Dimension, left: Float, right: Float): Int {
+        return jdbi.withHandle<Int, Exception> {
+            val rate = dmsType.rateSnippet()
+            val sql = "select count(*) from item where $rate>=:left and $rate<:right and item_type='FILE'"
+            it.createQuery(sql)
+                    .bind("left", left)
+                    .bind("right", right)
+                    .mapTo(Int::class.java).one()
+        }
+    }
+
+    fun top(dmsType: Dimension, n: Int): List<TopItem> {
+
+        return jdbi.withHandle<List<TopItem>, Exception> {
+            val sql = """
+                select item_name, ${dmsType.rateSnippet()} coverageRate 
+                from item where item_type='FILE'
+                order by coverageRate desc limit :n """.trimIndent()
+            println("sql = ${sql}")
+            it.createQuery(sql)
+                    .bind("n", n)
+                    .mapToBean(TopItem::class.java).list()
+        }
+    }
 }
 
 data class Bundle(
@@ -51,3 +76,16 @@ data class Bundle(
     constructor() : this(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "no info in db", 0)
 }
 
+
+data class TopItem(var itemName: String, var coverageRate: Float) {
+    constructor() : this("", 0.0F)
+}
+
+enum class Dimension {
+    INSTRUCTION, LINE, BRANCH, COMPLEXITY, METHOD, CLASS;
+
+    fun rateSnippet(): String {
+        val dms = this.toString().toLowerCase()
+        return "${dms}_covered*1.00/(${dms}_covered+${dms}_missed)"
+    }
+}
--- src/main/kotlin/com/thoughtworks/archguard/coverage/CoverageController.kt
+++ src/main/kotlin/com/thoughtworks/archguard/coverage/CoverageController.kt
@@ -3,6 +3,7 @@ package com.thoughtworks.archguard.coverage
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,7 +14,27 @@ class CoverageController(@Autowired val coverageAnalyzer: CoverageAnalyzer) {
     fun bundleCoverage(): Bundle {
         return coverageAnalyzer.analyzeExecFile()
     }
+
+
+    @GetMapping("/rateBetween")
+    fun countRateBetween(@RequestParam(name = "dms", defaultValue = "line") dms: String,
+                         @RequestParam(name = "left", defaultValue = "0") left: Float,
+                         @RequestParam(name = "right", defaultValue = "2") right: Float): Int {
+
+        return coverageAnalyzer.countRateBetween(Dimension.valueOf(dms.toUpperCase()), left, right)
+    }
+
+
+    @GetMapping("/top")
+    fun topN(@RequestParam(name = "dms", defaultValue = "line") dms: String,
+             @RequestParam(name = "n", defaultValue = "5") n: Int
+    ): List<TopItem> {
+        val dmsType = Dimension.valueOf(dms.toUpperCase())
+        return coverageAnalyzer.top(dmsType, n)
+    }
 }
+
+
 
 
 
