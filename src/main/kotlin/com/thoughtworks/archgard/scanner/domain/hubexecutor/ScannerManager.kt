7cdcb4113a3a44994ebb33542fcb696b07bdbfc1
Hu Le <le.hu.2010@163.com>
2020-04-01 19:00:32 +0800

    feat: add scanners

--- src/main/kotlin/com/thoughtworks/archgard/scanner/domain/hubexecutor/ScannerManager.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/hubexecutor/ScannerManager.kt
@@ -6,8 +6,6 @@ import com.thoughtworks.archgard.scanner.domain.scanner.Scanner
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import java.util.concurrent.Callable
-import java.util.concurrent.Executors
 
 
 @Component
@@ -19,21 +17,30 @@ class ScannerManager(@Autowired private val scanners: List<Scanner>) {
     private val log = LoggerFactory.getLogger(ScannerManager::class.java)
 
     fun execute(context: ScanContext) {
-        val WORKER_THREAD_POOL = Executors.newFixedThreadPool(4)
+//        val WORKER_THREAD_POOL = Executors.newFixedThreadPool(4)
 
         val registered = configureRepository.getRegistered().filter { it.value == "true" }.map { it.type }
 
-        val callables: List<Callable<Unit>> = scanners.map { s ->
-            Callable {
-                try {
-                    s.scan(context)
-                } catch (e: Exception) {
-                    log.error("failed to scan {}", s.javaClass.simpleName, e)
-                }
+//        val callables: List<Callable<Unit>> = scanners.map { s ->
+//            Callable {
+//                try {
+//                    s.scan(context)
+//                } catch (e: Exception) {
+//                    log.error("failed to scan {}", s.javaClass.simpleName, e)
+//                }
+//            }
+//        }
+//
+//        WORKER_THREAD_POOL.invokeAll(callables)
+
+        scanners.forEach {
+            try {
+                log.info("start to scan {}", it.javaClass.simpleName)
+                it.scan(context)
+            } catch (e: Exception) {
+                log.error("failed to scan {}", it.javaClass.simpleName, e)
             }
         }
-
-        WORKER_THREAD_POOL.invokeAll(callables)
     }
 
     fun register() {
