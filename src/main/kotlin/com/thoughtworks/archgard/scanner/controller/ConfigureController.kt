5975baf701c68ae4af509659e9d943502254fb71
xiang.li2 <xiang.li2@thoughtworks.com>
2020-04-02 10:57:44 +0800

    remove /scanner from api url

--- src/main/kotlin/com/thoughtworks/archgard/scanner/controller/ConfigureController.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/controller/ConfigureController.kt
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/scanner")
+@RequestMapping("/")
 class ConfigureController {
 
     @Autowired
