9f81ad40dc9b4d9db2a3e2a2debd71d9f72c9cd3
Hu Le <le.hu.2010@163.com>
2020-04-13 11:39:11 +0800

    feat: add controller

--- src/main/kotlin/com/thoughtworks/archgard/scanner/controller/AnalysisController.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/controller/AnalysisController.kt
@@ -1,8 +1,6 @@
 package com.thoughtworks.archgard.scanner.controller
 
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
