addb9fb0a3607b6dfa711c45789564ac64fc8588
Hu Le <le.hu.2010@163.com>
2020-04-26 15:09:36 +0800

    feat: add level

--- src/main/kotlin/com/thoughtworks/archguard/evaluation/domain/analysis/TestProtectionAnalysis.kt
+++ src/main/kotlin/com/thoughtworks/archguard/evaluation/domain/analysis/TestProtectionAnalysis.kt
@@ -1,6 +1,7 @@
 package com.thoughtworks.archguard.evaluation.domain.analysis
 
 import com.thoughtworks.archguard.evaluation.domain.analysis.report.Report
+import com.thoughtworks.archguard.evaluation.domain.analysis.report.ReportLevel
 import com.thoughtworks.archguard.report.infrastructure.CoverageRepo
 import com.thoughtworks.archguard.report.infrastructure.HotSpotRepo
 import com.thoughtworks.archguard.report.infrastructure.TestBadSmellRepo
@@ -54,12 +55,54 @@ class TestProtectionAnalysis(@Autowired val testBadSmellRepo: TestBadSmellRepo,
     }
 }
 
-data class TestProtectionQualityReport(val UselessPercent: Double,
+data class TestProtectionQualityReport(val uselessPercent: Double,
                                        val latestUselessTest: Int,
                                        val latestTestCoverage: Double,
                                        val latestModuleTestCoverage: Double) : Report {
     override fun getImprovements(): List<String> {
         TODO("Not yet implemented")
+    }
+
+    override fun getLevel(): Map<String, ReportLevel> {
+        val result = HashMap<String, ReportLevel>()
+        when {
+            uselessPercent > 0.05 -> {
+                result["uselessPercent"] = ReportLevel.NEED_IMPROVED
+            }
+            uselessPercent > 0.03 -> {
+                result["uselessPercent"] = ReportLevel.WELL
+            }
+            uselessPercent < 0.03 -> {
+                result["uselessPercent"] = ReportLevel.GOOD
+            }
+        }
+
+        val testCoverage = latestTestCoverage - latestUselessTest * 0.01
+        when {
+            testCoverage > 0.8 -> {
+                result["latestTestCoverage"] = ReportLevel.GOOD
+            }
+            testCoverage > 0.6 -> {
+                result["latestTestCoverage"] = ReportLevel.WELL
+            }
+            testCoverage < 0.6 -> {
+                result["latestTestCoverage"] = ReportLevel.NEED_IMPROVED
+            }
+        }
+
+        val coverage = latestModuleTestCoverage - latestUselessTest * 0.01
+        when {
+            coverage > 0.8 -> {
+                result["latestModuleTestCoverage"] = ReportLevel.GOOD
+            }
+            uselessPercent > 0.6 -> {
+                result["latestModuleTestCoverage"] = ReportLevel.WELL
+            }
+            uselessPercent < 0.6 -> {
+                result["latestModuleTestCoverage"] = ReportLevel.NEED_IMPROVED
+            }
+        }
+        return result
     }
 }
 
--- src/main/kotlin/com/thoughtworks/archguard/evaluation/domain/analysis/report/Report.kt
+++ src/main/kotlin/com/thoughtworks/archguard/evaluation/domain/analysis/report/Report.kt
@@ -2,4 +2,12 @@ package com.thoughtworks.archguard.evaluation.domain.analysis.report
 
 interface Report {
     fun getImprovements(): List<String>
+    fun getLevel(): Map<String, ReportLevel>
+}
+
+enum class ReportLevel {
+    GOOD,
+    WELL,
+    NEED_IMPROVED
+
 }
