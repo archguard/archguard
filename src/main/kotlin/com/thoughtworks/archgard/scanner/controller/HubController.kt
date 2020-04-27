8a0bd9ca03ae7fd4392d1f7c9378fce0ff94d912
Hu Le <le.hu.2010@163.com>
2020-04-27 11:18:11 +0800

    feat: add status query

--- src/main/kotlin/com/thoughtworks/archgard/scanner/controller/HubController.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/controller/HubController.kt
@@ -2,10 +2,7 @@ package com.thoughtworks.archgard.scanner.controller
 
 import com.thoughtworks.archgard.scanner.domain.hubexecutor.HubService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/")
@@ -22,6 +19,11 @@ class HubController {
     @PostMapping("/evaluations")
     fun evaluate(@RequestBody evaluation: EvaluationRequest): ModuleScanResponse {
         return ModuleScanResponse(hubService.evaluate(evaluation.type))
+    }
+
+    @GetMapping("/evaluations/status")
+    fun evaluate(@PathVariable type: String): ModuleScanResponse {
+        return ModuleScanResponse(hubService.getEvaluationStatus(type))
     }
 
     data class ModuleScanResponse(val isRunning: Boolean)
--- src/main/kotlin/com/thoughtworks/archgard/scanner/domain/hubexecutor/HubService.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/hubexecutor/HubService.kt
@@ -54,4 +54,8 @@ class HubService {
         return isRunning
     }
 
+    fun getEvaluationStatus(type: String): Boolean {
+        return isRunning;
+    }
+
 }
