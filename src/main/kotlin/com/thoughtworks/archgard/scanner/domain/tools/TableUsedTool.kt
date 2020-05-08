def0b37664048b1bca346840583d077091af520b
Hu Le <le.hu.2010@163.com>
2020-05-08 10:08:59 +0800

    feat: get table name

--- src/main/kotlin/com/thoughtworks/archgard/scanner/domain/tools/TableUsedTool.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/tools/TableUsedTool.kt
@@ -1,8 +1,27 @@
 package com.thoughtworks.archgard.scanner.domain.tools
 
+import com.thoughtworks.archgard.scanner.infrastructure.Processor
 import java.io.File
 
 class TableUsedTool(val projectRoot: File, val sql: String?) {
+
     fun analyse() {
+        if (sql != null) {
+            getTableName(sql)
+        }
+    }
+
+    private fun getTableName(sql: String) {
+        val sqlFile = File(projectRoot.toString() + "/sql_tables.sql")
+        sqlFile.writeText(sql, charset("UTF-8"))
+        val outputFile = File(projectRoot.toString() + "table_names.log")
+        call(listOf("/bin/sh", "-c",
+                "grep \"CREATE TABLE\" $sqlFile " +
+                        "|awk '{print \$3}'" +
+                        "|awk -F '`' '{print \$2}' > $outputFile"))
+    }
+
+    private fun call(cmd: List<String>) {
+        Processor.executeWithLogs(ProcessBuilder(cmd), projectRoot)
     }
 }
