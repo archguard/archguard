1ad19f10aa70e5992e705d6d1a7e9247c9edaeb1
superPershing <15770165+superPershing@users.noreply.github.com>
2020-06-30 18:02:31 +0800

    chore: add integration-test

---
+++ src/test/kotlin/com/thoughtworks/archguard/module/infrastructure/LogicModuleRepositoryImplTest.kt
@@ -0,0 +1,26 @@
+package com.thoughtworks.archguard.module.infrastructure
+
+import com.thoughtworks.archguard.module.domain.LogicModule
+import com.thoughtworks.archguard.module.domain.LogicModuleRepository
+import com.thoughtworks.archguard.module.domain.LogicModuleStatus
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.web.WebAppConfiguration
+
+@SpringBootTest
+@WebAppConfiguration
+internal class LogicModuleRepositoryImplTest {
+
+    @Autowired
+    lateinit var logicModuleRepository: LogicModuleRepository
+
+    @Test
+    internal fun `should only select normal data from logic module`() {
+        val normalLogicModules = logicModuleRepository.getAllByShowStatus(true)
+        assertThat(normalLogicModules.size).isEqualTo(1)
+        assertThat(normalLogicModules[0]).isEqualTo(
+                LogicModule("id1", "dubbo-provider", listOf("dubbo-provider"), LogicModuleStatus.NORMAL))
+    }
+}
