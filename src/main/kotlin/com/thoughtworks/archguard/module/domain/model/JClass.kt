c9eb7811a10d5b40141ffad2399559e3f7b52a47
superPershing <15770165+superPershing@users.noreply.github.com>
2020-07-13 18:39:47 +0800

    refactor: delete useless annotation

--- src/main/kotlin/com/thoughtworks/archguard/module/domain/model/JClass.kt
+++ src/main/kotlin/com/thoughtworks/archguard/module/domain/model/JClass.kt
@@ -1,14 +1,11 @@
 package com.thoughtworks.archguard.module.domain.model
 
-import org.jdbi.v3.core.mapper.reflect.JdbiConstructor
-
 class JClass(val name: String, val module: String) : ModuleMember {
     var dependencees: List<JClass> = ArrayList()
     var dependencers: List<JClass> = ArrayList()
     var id: String? = null
     var classType: ClazzType = ClazzType.NOT_DEFINED
 
-    @JdbiConstructor
     constructor(id: String, name: String, module: String) : this(name, module) {
         this.id = id
     }
