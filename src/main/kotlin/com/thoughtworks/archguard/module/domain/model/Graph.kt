d3b1c2d921cf7a7c27edf15271a2ad5f1d1b1310
superPershing <15770165+superPershing@users.noreply.github.com>
2020-07-14 15:54:20 +0800

    refactor: remove Domain object in New Graph Edge

--- src/main/kotlin/com/thoughtworks/archguard/module/domain/model/Graph.kt
+++ src/main/kotlin/com/thoughtworks/archguard/module/domain/model/Graph.kt
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicInteger
 
 class GraphStore<T> {
     private val nodes = CopyOnWriteArrayList<NodeWrapper<T>>()
-    private val edges = CopyOnWriteArrayList<Edge<T>>()
+    private val edges = CopyOnWriteArrayList<Edge>()
 
     private var index: AtomicInteger = AtomicInteger(0)
 
@@ -22,7 +22,7 @@ class GraphStore<T> {
     private fun containsNode(node: T) = nodes.map { it.node.node }.contains(node)
 
     private fun addEdge(a: NodeWrapper<T>, b: NodeWrapper<T>, num: Int) {
-        edges.add(Edge(a, b, num))
+        edges.add(Edge(a.id, b.id, num))
     }
 
     fun addEdge(a: T, b: T, num: Int) {
@@ -37,6 +37,6 @@ class GraphStore<T> {
 }
 
 data class Node<T>(val node: T)
-data class Graph<T>(val nodes: List<NodeWrapper<T>>, val edges: List<Edge<T>>)
-data class Edge<T>(val a: NodeWrapper<T>, val b: NodeWrapper<T>, val num: Int)
+data class Graph<T>(val nodes: List<NodeWrapper<T>>, val edges: List<Edge>)
+data class Edge(val a: Int, val b: Int, val num: Int)
 data class NodeWrapper<T>(val id: Int, val node: Node<T>)
