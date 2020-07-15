7fa74968752a3aae5c6a56543724228480f5b41e
superPershing <15770165+superPershing@users.noreply.github.com>
2020-07-15 18:39:22 +0800

    refactor: use JClassVO in JMethod

--- src/main/kotlin/com/thoughtworks/archguard/module/domain/model/JMethod.kt
+++ src/main/kotlin/com/thoughtworks/archguard/module/domain/model/JMethod.kt
@@ -1,9 +1,9 @@
 package com.thoughtworks.archguard.module.domain.model
 
-class JMethod(val name: String, val jClass: JClass) {
+class JMethod(val name: String, val jClass: JClassVO) {
     lateinit var id: String
 
-    constructor(id: String, name: String, module: String) : this(name, JClass(name, module)) {
+    constructor(id: String, name: String, module: String) : this(name, JClassVO(name, module)) {
         this.id = id
     }
 
--- src/main/kotlin/com/thoughtworks/archguard/module/infrastructure/dto/MethodDependencyDto.kt
+++ src/main/kotlin/com/thoughtworks/archguard/module/infrastructure/dto/MethodDependencyDto.kt
@@ -2,11 +2,12 @@ package com.thoughtworks.archguard.module.infrastructure.dto
 
 import com.thoughtworks.archguard.module.domain.model.Dependency
 import com.thoughtworks.archguard.module.domain.model.JClass
+import com.thoughtworks.archguard.module.domain.model.JClassVO
 import com.thoughtworks.archguard.module.domain.model.JMethod
 
 class MethodDependencyDto(var caller: String, var callerClass: String, var callerMethod: String, var callee: String, var calleeClass: String, var calleeMethod: String) {
     fun toMethodDependency(): Dependency<JMethod> {
-        return Dependency(JMethod(callerMethod, JClass(callerClass, caller)), JMethod(calleeMethod, JClass(calleeClass, callee)))
+        return Dependency(JMethod(callerMethod, JClassVO(callerClass, caller)), JMethod(calleeMethod, JClassVO(calleeClass, callee)))
     }
 
     companion object {
