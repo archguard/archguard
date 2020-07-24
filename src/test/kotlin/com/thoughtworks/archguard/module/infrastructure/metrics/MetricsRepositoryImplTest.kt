b5c4f3df875322357cef8bb036df8481c401d454
liguangzheng <guangzheng.li@thoughtworks.com>
2020-07-24 14:51:19 +0800

    feat: modify repo test transactional

--- src/test/kotlin/com/thoughtworks/archguard/module/infrastructure/metrics/MetricsRepositoryImplTest.kt
+++ src/test/kotlin/com/thoughtworks/archguard/module/infrastructure/metrics/MetricsRepositoryImplTest.kt
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.transaction.annotation.Transactional
 import kotlin.test.assertEquals
 
 @SpringBootTest
 @WebAppConfiguration
+@Transactional
 internal class MetricsRepositoryImplTest {
     @Autowired
     private lateinit var metricsRepositoryImpl: MetricsRepositoryImpl
