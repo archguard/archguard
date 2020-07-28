47e5507ee017bed3eda69ef989cfc70c235d0565
liguangzheng <guangzheng.li@thoughtworks.com>
2020-07-28 16:13:26 +0800

    feat: add log

--- src/main/kotlin/com/thoughtworks/archgard/scanner/infrastructure/client/AnalysisModuleClient.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/infrastructure/client/AnalysisModuleClient.kt
@@ -1,12 +1,17 @@
 package com.thoughtworks.archgard.scanner.infrastructure.client
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
 @Component
 class AnalysisModuleClient (@Value("\${module.client.host}") val url: String) {
+    private val log = LoggerFactory.getLogger(AnalysisModuleClient::class.java)
+
     fun calculateCoupling() {
         RestTemplate().postForLocation("$url/logic-modules/calculate-coupling", null)
+        log.info("send calculate coupling request to module service")
     }
+
 }
