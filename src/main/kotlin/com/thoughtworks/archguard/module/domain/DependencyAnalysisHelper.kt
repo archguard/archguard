979cf081ae442b369c146a8faf2cecf161ea8d10
hang.liu <hang.liu@thoughtworks.com>
2020-07-28 15:23:31 +0800

    refactor: remove GraphServiceLegacy and Test

--- src/main/kotlin/com/thoughtworks/archguard/module/domain/DependencyAnalysisHelper.kt
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.archguard.module.domain
-
-import com.thoughtworks.archguard.module.domain.model.Dependency
-import com.thoughtworks.archguard.module.domain.model.JClassVO
-import com.thoughtworks.archguard.module.domain.model.LogicModule
-
-interface DependencyAnalysisHelper {
-    fun analysis(classDependency: Dependency<JClassVO>, logicModules: List<LogicModule>): List<LogicModule>
-}
--- src/main/kotlin/com/thoughtworks/archguard/module/domain/dubbo/DubboXmlDependencyAnalysisHelper.kt
+++ src/main/kotlin/com/thoughtworks/archguard/module/domain/dubbo/DubboXmlDependencyAnalysisHelper.kt
@@ -1,14 +1,13 @@
 package com.thoughtworks.archguard.module.domain.dubbo
 
-import com.thoughtworks.archguard.module.domain.DependencyAnalysisHelper
 import com.thoughtworks.archguard.module.domain.getModule
 import com.thoughtworks.archguard.module.domain.model.*
 import org.springframework.stereotype.Service
 
 @Service
-class DubboXmlDependencyAnalysisHelper(val xmlConfigService: XmlConfigService) : DependencyAnalysisHelper {
+class DubboXmlDependencyAnalysisHelper(val xmlConfigService: XmlConfigService){
 
-    override fun analysis(classDependency: Dependency<JClassVO>, logicModules: List<LogicModule>): List<LogicModule> {
+    fun analysis(classDependency: Dependency<JClassVO>, logicModules: List<LogicModule>): List<LogicModule> {
         val calleeSubModuleByXml = xmlConfigService.getRealCalleeModuleByXmlConfig(classDependency.caller, classDependency.callee)
         return calleeSubModuleByXml.map { getModule(logicModules, SubModule(it.name)) }
                 .flatten().toSet().toList()
