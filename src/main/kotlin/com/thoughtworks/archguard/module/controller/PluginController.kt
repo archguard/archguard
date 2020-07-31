710d505ee547229e3d3a6ebe6347106f78d57632
hang.liu <hang.liu@thoughtworks.com>
2020-07-31 15:23:23 +0800

    feat: add queryPluginTypes api

---
+++ src/main/kotlin/com/thoughtworks/archguard/module/controller/PluginController.kt
@@ -0,0 +1,16 @@
+package com.thoughtworks.archguard.module.controller
+
+import com.thoughtworks.archguard.module.domain.plugin.PluginType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/module/plugin")
+class PluginController {
+
+    @GetMapping("/type")
+    fun getPluginTypes(): List<String> {
+        return PluginType.values().map { it.name }
+    }
+}
