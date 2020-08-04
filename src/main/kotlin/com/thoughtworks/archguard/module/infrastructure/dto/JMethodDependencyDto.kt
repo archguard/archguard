5f7554f382f6232a9e86764c9817e69be9d92aca
hang.liu <hang.liu@thoughtworks.com>
2020-08-04 18:13:27 +0800

    fix: JMethodDependency callerMethod

--- src/main/kotlin/com/thoughtworks/archguard/module/infrastructure/dto/JMethodDependencyDto.kt
+++ src/main/kotlin/com/thoughtworks/archguard/module/infrastructure/dto/JMethodDependencyDto.kt
@@ -5,6 +5,6 @@ import com.thoughtworks.archguard.module.domain.model.JMethodVO
 
 data class JMethodDependencyDto(val callerModule: String, val callerClass: String, val callerMethod: String, val callerReturnType: String, val callerArgumentTypes: String, val calleeModule: String, val calleeClass: String, val calleeMethod: String, val calleeReturnType: String, val calleeArgumentTypes: String) {
     fun toJMethodDependency(): Dependency<JMethodVO> {
-        return Dependency(JMethodVO(calleeMethod, callerClass, callerModule, callerReturnType, callerArgumentTypes.split(",")), JMethodVO(calleeMethod, calleeClass, calleeModule, calleeReturnType, calleeArgumentTypes.split(",")))
+        return Dependency(JMethodVO(callerMethod, callerClass, callerModule, callerReturnType, callerArgumentTypes.split(",")), JMethodVO(calleeMethod, calleeClass, calleeModule, calleeReturnType, calleeArgumentTypes.split(",")))
     }
 }
