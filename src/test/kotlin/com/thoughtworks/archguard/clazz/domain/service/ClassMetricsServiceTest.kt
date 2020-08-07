501acf8cde469d2bbc1c774927d0c4774b7590c1
hang.liu <hang.liu@thoughtworks.com>
2020-08-07 16:02:34 +0800

    test: ClassMetricsServiceTest

---
+++ src/test/kotlin/com/thoughtworks/archguard/clazz/domain/service/ClassMetricsServiceTest.kt
@@ -0,0 +1,47 @@
+package com.thoughtworks.archguard.clazz.domain.service
+
+import com.thoughtworks.archguard.clazz.domain.JClass
+import com.thoughtworks.archguard.clazz.domain.JClassRepository
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.MockKAnnotations.init
+import io.mockk.every
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ClassMetricsServiceTest {
+    @InjectMockKs
+    var service = ClassMetricsService()
+
+    @MockK
+    private lateinit var repo: JClassRepository
+
+    @BeforeEach
+    internal fun setUp() {
+        init(this)
+    }
+
+    @Test
+    fun `should get DepthOfInheritance`() {
+        //given
+        val child = JClass("any", "Child", "module")
+        val parent = JClass("any", "Parent", "module")
+        val grandparent = JClass("any", "Grandparent", "module")
+
+        every { repo.findClassParents("module", "Child") } returns listOf(parent)
+        every { repo.findClassParents("module", "Parent") } returns listOf(grandparent)
+        every { repo.findClassParents("module", "Grandparent") } returns listOf()
+
+        //when
+        val childDIT = service.getDepthOfInheritance(child)
+        val parentDIT = service.getDepthOfInheritance(parent)
+        val grandparentDIT = service.getDepthOfInheritance(grandparent)
+
+        //then
+        assertEquals(2, childDIT)
+        assertEquals(1, parentDIT)
+        assertEquals(0, grandparentDIT)
+
+    }
+}
