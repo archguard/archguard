72e3ec66bf03a343f8510ab9f64642cad10e7e70
dabuzhang <15770165+superPershing@users.noreply.github.com>
2020-08-12 00:26:07 +0800

    fix: returnn abc with class set count

--- src/main/kotlin/com/thoughtworks/archguard/metrics/domain/abc/AbcServiceImpl.kt
+++ src/main/kotlin/com/thoughtworks/archguard/metrics/domain/abc/AbcServiceImpl.kt
@@ -5,10 +5,14 @@ import com.thoughtworks.archguard.method.domain.JMethodRepository
 import com.thoughtworks.archguard.module.domain.model.JMethodVO
 import org.springframework.stereotype.Service
 
+/**
+ * Association Between Class (ABC): The Association Between Classes metric for a particular class or structure
+ * is the number of members of others types it directly uses in the body of its methods.
+ */
 @Service
 class AbcServiceImpl(val jMethodRepository: JMethodRepository) : AbcService {
     override fun calculateAbc(jClass: JClass): Int {
-        val map = jClass.methods.map { jMethodRepository.findMethodCallees(it.id) }.flatten().map { JMethodVO.fromJMethod(it) }
-        return map.toSet().count()
+        val allMethod = jClass.methods.map { jMethodRepository.findMethodCallees(it.id) }.flatten().map { JMethodVO.fromJMethod(it) }
+        return allMethod.map { it.clazz }.toSet().count()
     }
 }
