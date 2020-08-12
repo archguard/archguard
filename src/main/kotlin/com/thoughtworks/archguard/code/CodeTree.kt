af2afe788eb7616551a3ebb7a2b14f74850d71c3
dabuzhang <15770165+superPershing@users.noreply.github.com>
2020-08-13 00:03:32 +0800

    feat: codeTree support top class file

--- src/main/kotlin/com/thoughtworks/archguard/code/CodeTree.kt
+++ src/main/kotlin/com/thoughtworks/archguard/code/CodeTree.kt
@@ -4,30 +4,30 @@ class CodeTree {
     var trees: HashMap<String, Node> = HashMap()
 
     fun addClass(nodeName: String) {
-        // TODO 顶层文件
         val split = nodeName.split(".")
         val classTopPackage = split[0]
+        if (split.size == 1) {
+            trees.put(classTopPackage, Node(classTopPackage, TypeEnum.FILE))
+            return
+        }
         if (!trees.keys.contains(classTopPackage)) {
             val topNode = Node(classTopPackage, TypeEnum.PACKAGE)
-            var currentNode: Node = topNode
-            for (node in split.subList(1, split.size - 1)) {
-                val nextNode = Node(node, TypeEnum.PACKAGE)
-                currentNode.children.add(nextNode)
-                currentNode = currentNode.children.find { it.node == node }!!
-            }
-            val leafNode = Node(split[split.size - 1], TypeEnum.FILE)
-            currentNode.children.add(leafNode)
+            addOrUpdateNodes(topNode, split)
             trees.put(classTopPackage, topNode)
-        } else {
-            val topNode = trees.get(classTopPackage)!!
-            var currentNode: Node = topNode
-            for (node in split.subList(1, split.size - 1)) {
-                val nextNode = Node(node, TypeEnum.PACKAGE)
-                currentNode.children.add(nextNode)
-                currentNode = currentNode.children.find { it.node == node }!!
-            }
-            val leafNode = Node(split[split.size - 1], TypeEnum.FILE)
-            currentNode.children.add(leafNode)
+            return
         }
+        val topNode = trees.get(classTopPackage)!!
+        addOrUpdateNodes(topNode, split)
+    }
+
+    private fun addOrUpdateNodes(topNode: Node, split: List<String>) {
+        var currentNode: Node = topNode
+        for (node in split.subList(1, split.size - 1)) {
+            val nextNode = Node(node, TypeEnum.PACKAGE)
+            currentNode.children.add(nextNode)
+            currentNode = currentNode.children.find { it.node == node }!!
+        }
+        val leafNode = Node(split[split.size - 1], TypeEnum.FILE)
+        currentNode.children.add(leafNode)
     }
 }
--- src/test/kotlin/com/thoughtworks/archguard/code/CodeTreeTest.kt
+++ src/test/kotlin/com/thoughtworks/archguard/code/CodeTreeTest.kt
@@ -12,8 +12,11 @@ internal class CodeTreeTest {
         codeTree.addClass("a.c")
         codeTree.addClass("m.n.p")
         codeTree.addClass("m.q")
+        codeTree.addClass("x")
 
-        assertThat(codeTree.trees.keys).containsExactlyInAnyOrderElementsOf(listOf("a", "m"))
+        assertThat(codeTree.trees.keys).containsExactlyInAnyOrderElementsOf(listOf("a", "m", "x"))
+
+        assertThat(codeTree.trees.get("a")).isEqualTo(Node("a", TypeEnum.PACKAGE))
         assertThat(codeTree.trees.get("a")!!.children)
                 .containsExactlyInAnyOrderElementsOf(listOf(Node("b", TypeEnum.PACKAGE), Node("c", TypeEnum.FILE)))
         assertThat(codeTree.trees.get("a")!!.children.first { it.node == "b" }.children)
@@ -21,9 +24,11 @@ internal class CodeTreeTest {
         assertThat(codeTree.trees.get("a")!!.children.first { it.node == "b" }.children.first { it.node == "e" }.children)
                 .containsOnly(Node("f", TypeEnum.FILE))
 
+        assertThat(codeTree.trees.get("m")).isEqualTo(Node("m", TypeEnum.PACKAGE))
         assertThat(codeTree.trees.get("m")!!.children)
                 .containsExactlyInAnyOrderElementsOf(listOf(Node("n", TypeEnum.PACKAGE), Node("q", TypeEnum.FILE)))
         assertThat(codeTree.trees.get("m")!!.children.first { it.node == "n" }.children)
                 .containsOnly(Node("p", TypeEnum.FILE))
+        assertThat(codeTree.trees.get("x")).isEqualTo(Node("x", TypeEnum.FILE))
     }
 }
