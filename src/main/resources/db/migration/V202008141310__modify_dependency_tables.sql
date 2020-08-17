012ac8e2742776d78cc55b6b4a2c9da4bd56d5e5
liguangzheng <guangzheng.li@thoughtworks.com>
2020-08-17 16:25:52 +0800

    feat: modify dependency by project id

--- src/main/resources/db/migration/V202008141310__modify_dependency_tables.sql
+++ src/main/resources/db/migration/V202008141310__modify_dependency_tables.sql
@@ -13,3 +13,23 @@ ALTER TABLE `JMethod`
 ALTER TABLE `JAnnotation`
     ADD COLUMN `project_id` bigint(20) NOT NULL AFTER `id`,
     ADD INDEX `idx_project_id`(`project_id`) USING BTREE;
+
+ALTER TABLE `_ClassFields`
+    ADD COLUMN `project_id` bigint(20) NOT NULL AFTER `id`,
+    ADD INDEX `idx_project_id`(`project_id`) USING BTREE;
+
+ALTER TABLE `_ClassMethods`
+    ADD COLUMN `project_id` bigint(20) NOT NULL AFTER `id`,
+    ADD INDEX `idx_project_id`(`project_id`) USING BTREE;
+
+ALTER TABLE `_ClassParent`
+    ADD COLUMN `project_id` bigint(20) NOT NULL AFTER `id`,
+    ADD INDEX `idx_project_id`(`project_id`) USING BTREE;
+
+ALTER TABLE `_MethodCallees`
+    ADD COLUMN `project_id` bigint(20) NOT NULL AFTER `id`,
+    ADD INDEX `idx_project_id`(`project_id`) USING BTREE;
+
+ALTER TABLE `_ClassDependences`
+    ADD COLUMN `project_id` bigint(20) NOT NULL AFTER `id`,
+    ADD INDEX `idx_project_id`(`project_id`) USING BTREE;
--- src/main/resources/db/migration/V202008171310__modify_dubbo_dependency_tables.sql
+++ src/main/resources/db/migration/V202008171310__modify_dubbo_dependency_tables.sql
@@ -1,3 +1,15 @@
 ALTER TABLE `dubbo_module`
     ADD COLUMN `project_id` bigint(20) NOT NULL AFTER `id`,
     ADD INDEX `idx_project_id`(`project_id`) USING BTREE;
+
+ALTER TABLE `dubbo_bean`
+    ADD COLUMN `project_id` bigint(20) NOT NULL AFTER `id`,
+    ADD INDEX `idx_project_id`(`project_id`) USING BTREE;
+
+ALTER TABLE `dubbo_reference_config`
+    ADD COLUMN `project_id` bigint(20) NOT NULL AFTER `id`,
+    ADD INDEX `idx_project_id`(`project_id`) USING BTREE;
+
+ALTER TABLE `dubbo_service_config`
+    ADD COLUMN `project_id` bigint(20) NOT NULL AFTER `id`,
+    ADD INDEX `idx_project_id`(`project_id`) USING BTREE;
