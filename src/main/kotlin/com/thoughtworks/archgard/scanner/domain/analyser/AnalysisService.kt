7b969d933dcb4240387048eb243d4f2d2e28ee7d
HeZhong <hezhong0711@gmail.com>
2020-08-20 10:37:39 +0800

    check zip file exists for analysis zip project.

--- src/main/kotlin/com/thoughtworks/archgard/scanner/domain/analyser/AnalysisService.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/analyser/AnalysisService.kt
@@ -6,12 +6,28 @@ import com.thoughtworks.archgard.scanner.domain.project.ProjectRepository
 import com.thoughtworks.archguard.common.exception.EntityNotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.io.FileNotFoundException
+import java.nio.file.Files
+import java.nio.file.Paths
 
 @Service
 class AnalysisService(@Autowired val projectRepository: ProjectRepository) {
     fun getProjectOperator(id: Long): ProjectOperator {
         val projectInfo = projectRepository.getProjectInfo(id)
                 ?: throw EntityNotFoundException(ProjectInfo::class.java, id)
+        checkAnalysable(projectInfo)
         return ProjectOperator(projectInfo)
+    }
+
+    fun checkAnalysable(projectInfo: ProjectInfo) {
+        if (projectInfo.repoType == "ZIP") {
+            projectInfo.getRepoList().forEach { repo ->
+                run {
+                    if (!Files.exists(Paths.get(repo))) {
+                        throw FileNotFoundException("zip file has been deleted: $repo")
+                    }
+                }
+            }
+        }
     }
 }
