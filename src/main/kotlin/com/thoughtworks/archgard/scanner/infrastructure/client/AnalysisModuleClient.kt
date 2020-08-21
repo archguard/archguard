0bf1b693a3cfdb5dce31f05ceebce79188cd40e9
liguangzheng <guangzheng.li@thoughtworks.com>
2020-08-21 10:38:51 +0800

    feat: modify auto define post project id

--- src/main/kotlin/com/thoughtworks/archgard/scanner/infrastructure/client/AnalysisModuleClient.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/infrastructure/client/AnalysisModuleClient.kt
@@ -4,13 +4,18 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
+import java.util.HashMap
+
+
 
 @Component
 class AnalysisModuleClient (@Value("\${module.client.host}") val url: String) {
     private val log = LoggerFactory.getLogger(AnalysisModuleClient::class.java)
 
     fun autoDefine(projectId: Long) {
-        RestTemplate().postForLocation("$url/logic-modules/{projectId}/auto-define", Long::class.java, projectId)
+        val params = HashMap<String, Any>()
+        params["projectId"] = projectId
+        RestTemplate().postForLocation("$url/logic-modules/{projectId}/auto-define", null, params)
         log.info("send auto define request to module service")
     }
 
