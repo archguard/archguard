6a3d9b35a190eccbeac92da215dbaf62b94d13e9
qicaisheng <qicaisheng@qq.com>
2020-08-22 16:48:51 +0800

    refactor: reformat request body

--- src/main/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingDtoListForWriteInfluxDB.kt
+++ src/main/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingDtoListForWriteInfluxDB.kt
@@ -9,8 +9,9 @@ data class ClassCouplingDtoListForWriteInfluxDB(val classCouplings: List<ClassCo
 data class ClassCouplingDtoForWriteInfluxDB(val classCoupling: ClassCoupling) {
     fun toInfluxDBRequestBody() : String {
         return "class_coupling,class_name=${classCoupling.jClassVO.name},package_name=${classCoupling.jClassVO.getPackageName()},module_name=${classCoupling.jClassVO.module} " +
-                "inner_fan_in=${classCoupling.innerFanIn},inner_fan_out=${classCoupling.innerFanOut},outer_fan_in=${classCoupling.outerFanIn},outer_fan_out=${classCoupling.outerFanOut}," +
-                "inner_instability=${classCoupling.innerInstability},inner_coupling=${classCoupling.innerCoupling},outer_instability=${classCoupling.outerInstability}," +
-                "outer_coupling=${classCoupling.outerCoupling}"
+                "inner_fan_in=${classCoupling.innerFanIn},inner_fan_out=${classCoupling.innerFanOut}," +
+                "outer_fan_in=${classCoupling.outerFanIn},outer_fan_out=${classCoupling.outerFanOut}," +
+                "inner_instability=${classCoupling.innerInstability},inner_coupling=${classCoupling.innerCoupling}," +
+                "outer_instability=${classCoupling.outerInstability},outer_coupling=${classCoupling.outerCoupling}"
     }
 }
