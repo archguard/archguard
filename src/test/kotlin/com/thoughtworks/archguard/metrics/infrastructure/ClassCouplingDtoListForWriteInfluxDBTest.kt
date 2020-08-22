c5bec5bf10521b356d9e6ef34609e9f63df686c1
qicaisheng <qicaisheng@qq.com>
2020-08-22 16:30:27 +0800

    refactor: inner ClassCouplingDtoForWriteInfluxDB.kt

--- src/test/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingDtoListForWriteInfluxDBTest.kt
+++ src/test/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingDtoListForWriteInfluxDBTest.kt
@@ -19,4 +19,21 @@ internal class ClassCouplingDtoListForWriteInfluxDBTest {
         assertEquals("class_coupling,class_name=net.aimeizi.dubbo.service.service.UserService,package_name=net.aimeizi.dubbo.service.service,module_name=dubbo-service inner_fan_in=0,inner_fan_out=0,outer_fan_in=0,outer_fan_out=4,inner_instability=0.0,inner_coupling=0.0,outer_instability=1.0,outer_coupling=0.75\n" +
                 "class_coupling,class_name=net.aimeizi.dubbo.service.service.OtherService,package_name=net.aimeizi.dubbo.service.service,module_name=dubbo-service inner_fan_in=0,inner_fan_out=0,outer_fan_in=1,outer_fan_out=3,inner_instability=0.0,inner_coupling=0.0,outer_instability=0.75,outer_coupling=0.75", influxDBRequestBody)
     }
+
+    @Test
+    internal fun shouldConvertObjectToInfluxDBRequestBody() {
+        val classCouplingDtoForWriteInfluxDB = ClassCouplingDtoForWriteInfluxDB(
+                "net.aimeizi.dubbo.service.service.UserService",
+                "net.aimeizi.dubbo.service.service", "dubbo-service",
+                0, 0, 4, 0,
+                0.0, 0.0, 0.0, 0.75);
+
+        val toInfluxDBRequestBody = classCouplingDtoForWriteInfluxDB.toInfluxDBRequestBody();
+
+        assertEquals("class_coupling,class_name=net.aimeizi.dubbo.service.service.UserService," +
+                "package_name=net.aimeizi.dubbo.service.service,module_name=dubbo-service " +
+                "inner_fan_in=0,inner_fan_out=0,outer_fan_in=4,outer_fan_out=0,inner_instability=0.0," +
+                "inner_coupling=0.0,outer_instability=0.0,outer_coupling=0.75", toInfluxDBRequestBody)
+    }
+
 }
