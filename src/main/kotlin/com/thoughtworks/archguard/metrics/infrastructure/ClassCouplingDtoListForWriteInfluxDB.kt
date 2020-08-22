65735dfea66ee79121391c2013e6d95190f014f0
qicaisheng <qicaisheng@qq.com>
2020-08-22 16:46:18 +0800

    refactor: remove from method

--- src/main/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingDtoListForWriteInfluxDB.kt
+++ src/main/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingDtoListForWriteInfluxDB.kt
@@ -3,7 +3,7 @@ package com.thoughtworks.archguard.metrics.infrastructure
 import com.thoughtworks.archguard.metrics.domain.coupling.ClassCoupling
 
 data class ClassCouplingDtoListForWriteInfluxDB(val classCouplings: List<ClassCoupling>) {
-    fun toRequestBody() = classCouplings.joinToString("\n") { ClassCouplingDtoForWriteInfluxDB.fromClassCoupling(it).toInfluxDBRequestBody() }
+    fun toRequestBody() = classCouplings.joinToString("\n") { ClassCouplingDtoForWriteInfluxDB(it).toInfluxDBRequestBody() }
 }
 
 data class ClassCouplingDtoForWriteInfluxDB(val classCoupling: ClassCoupling) {
@@ -12,11 +12,5 @@ data class ClassCouplingDtoForWriteInfluxDB(val classCoupling: ClassCoupling) {
                 "inner_fan_in=${classCoupling.innerFanIn},inner_fan_out=${classCoupling.innerFanOut},outer_fan_in=${classCoupling.outerFanIn},outer_fan_out=${classCoupling.outerFanOut}," +
                 "inner_instability=${classCoupling.innerInstability},inner_coupling=${classCoupling.innerCoupling},outer_instability=${classCoupling.outerInstability}," +
                 "outer_coupling=${classCoupling.outerCoupling}"
-    }
-
-    companion object {
-        fun fromClassCoupling(classCoupling: ClassCoupling): ClassCouplingDtoForWriteInfluxDB {
-            return ClassCouplingDtoForWriteInfluxDB(classCoupling)
-        }
     }
 }
