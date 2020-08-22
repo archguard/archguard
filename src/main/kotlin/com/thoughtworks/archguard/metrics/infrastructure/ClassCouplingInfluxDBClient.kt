1eac89dd3f6a018ece203024b67caf431ab8582d
qicaisheng <qicaisheng@qq.com>
2020-08-22 17:32:33 +0800

    fix: fix @Value url with hardcode tmp

--- src/main/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingInfluxDBClient.kt
+++ src/main/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingInfluxDBClient.kt
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
 @Component
-class ClassCouplingInfluxDBClient (@Value("\${influxdb.url}") val url: String) {
+class ClassCouplingInfluxDBClient {
+    
     private val log = LoggerFactory.getLogger(ClassCouplingInfluxDBClient::class.java)
 
     fun save(requestBody: String) {
+        val url = "http://influxdb:8086"
         RestTemplate().postForLocation("$url/api/v2/write?bucket=db0&precision=s", String::class.java, requestBody)
         log.info("save class coupling to InfluxDB")
     }
