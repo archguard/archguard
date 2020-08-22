ad4cc50f013156d63bbe7afc09087e4433e06114
qicaisheng <qicaisheng@qq.com>
2020-08-22 16:34:04 +0800

    refactor: rename method

--- src/main/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingDtoListForWriteInfluxDB.kt
+++ src/main/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingDtoListForWriteInfluxDB.kt
@@ -3,7 +3,7 @@ package com.thoughtworks.archguard.metrics.infrastructure
 import com.thoughtworks.archguard.metrics.domain.coupling.ClassCoupling
 
 data class ClassCouplingDtoListForWriteInfluxDB(val classCouplings: List<ClassCoupling>) {
-    fun toRequestString() = classCouplings.joinToString("\n") { ClassCouplingDtoForWriteInfluxDB.fromClassCoupling(it).toInfluxDBRequestBody() }
+    fun toRequestBody() = classCouplings.joinToString("\n") { ClassCouplingDtoForWriteInfluxDB.fromClassCoupling(it).toInfluxDBRequestBody() }
 }
 
 data class ClassCouplingDtoForWriteInfluxDB(val className: String,
--- src/test/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingDtoListForWriteInfluxDBTest.kt
+++ src/test/kotlin/com/thoughtworks/archguard/metrics/infrastructure/ClassCouplingDtoListForWriteInfluxDBTest.kt
@@ -14,7 +14,7 @@ internal class ClassCouplingDtoListForWriteInfluxDBTest {
         )
         val classCouplingDtoListForWriteInfluxDB = ClassCouplingDtoListForWriteInfluxDB(classCouplings);
         
-        val influxDBRequestBody = classCouplingDtoListForWriteInfluxDB.toRequestString()
+        val influxDBRequestBody = classCouplingDtoListForWriteInfluxDB.toRequestBody()
         
         assertEquals("class_coupling,class_name=net.aimeizi.dubbo.service.service.UserService,package_name=net.aimeizi.dubbo.service.service,module_name=dubbo-service inner_fan_in=0,inner_fan_out=0,outer_fan_in=0,outer_fan_out=4,inner_instability=0.0,inner_coupling=0.0,outer_instability=1.0,outer_coupling=0.75\n" +
                 "class_coupling,class_name=net.aimeizi.dubbo.service.service.OtherService,package_name=net.aimeizi.dubbo.service.service,module_name=dubbo-service inner_fan_in=0,inner_fan_out=0,outer_fan_in=1,outer_fan_out=3,inner_instability=0.0,inner_coupling=0.0,outer_instability=0.75,outer_coupling=0.75", influxDBRequestBody)
