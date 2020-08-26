4d51b56e6ecf76c71e348f304fb282365a78edd6
HeZhong <hezhong0711@gmail.com>
2020-08-26 15:54:10 +0800

    fix base module api.

--- src/main/kotlin/com/thoughtworks/archguard/module/controller/BaseModuleController.kt
+++ src/main/kotlin/com/thoughtworks/archguard/module/controller/BaseModuleController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/{projectId}")
+@RequestMapping("/projects/{projectId}")
 class BaseModuleController {
 
     @Autowired
