d6e8bfaac7b1d4eee3dd44cc4ebd5dda37db1582
qicaisheng <qicaisheng@qq.com>
2020-09-01 15:50:30 +0800

    #430 fix: add test environment db scripts

---
+++ src/test/resources/db/migration/V202009311101__add_MethodStatistic_table.sql
@@ -0,0 +1,13 @@
+create table MethodStatistic
+(
+    id          char(36)   not null
+        primary key,
+    systemId    char(36)   not null,
+    moduleName  mediumtext null,
+    packageName mediumtext not null,
+    typeName    mediumtext not null,
+    methodName  mediumtext not null,
+    `lines`     int        not null,
+    updateAt    datetime   not null,
+    createAt    datetime   not null
+);
