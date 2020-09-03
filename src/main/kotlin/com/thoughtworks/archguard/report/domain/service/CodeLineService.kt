1c6fe128f861438bb6f4f296ead86604bdcc68a6
dabuzhang <15770165+superPershing@users.noreply.github.com>
2020-09-03 10:21:43 +0800

    #475 refactor: move codeline code here

---
+++ src/main/kotlin/com/thoughtworks/archguard/report/domain/service/CodeLineService.kt
@@ -0,0 +1,21 @@
+package com.thoughtworks.archguard.report.domain.service
+
+import com.thoughtworks.archguard.report.domain.repository.CodeLineRepository
+import org.springframework.stereotype.Service
+
+@Service
+class CodeLineService(val codeLineRepository: CodeLineRepository) {
+
+    fun getMethodLinesAboveThreshold(systemId: Long, threshold: Int, limit: Long, offset: Long): MethodLinesDto {
+        val methodLinesCount = codeLineRepository.getMethodLinesAboveThresholdCount(systemId, threshold)
+
+        if (limit == 0L) {
+            val methodLinesAboveThreshold = codeLineRepository.getMethodLinesAboveThreshold(systemId, threshold)
+            return MethodLinesDto(methodLinesAboveThreshold, methodLinesCount, 0)
+        }
+        val methodLinesAboveThreshold = codeLineRepository.getMethodLinesAboveThreshold(systemId, threshold, limit, offset)
+        return MethodLinesDto(methodLinesAboveThreshold, methodLinesCount, offset / limit)
+    }
+
+
+}
