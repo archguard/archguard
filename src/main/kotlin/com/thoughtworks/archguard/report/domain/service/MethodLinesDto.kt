0970f29684838a5f47ac09d26cb1e6af1a2cdf61
dabuzhang <15770165+superPershing@users.noreply.github.com>
2020-09-03 16:03:28 +0800

    #434 refactor: rename data list to data

--- src/main/kotlin/com/thoughtworks/archguard/report/domain/service/MethodLinesDto.kt
+++ src/main/kotlin/com/thoughtworks/archguard/report/domain/service/MethodLinesDto.kt
@@ -3,4 +3,4 @@ package com.thoughtworks.archguard.report.domain.service
 import com.thoughtworks.archguard.report.domain.model.MethodLine
 
 
-data class MethodLinesDto(val methodLines: List<MethodLine>, val count: Long, val currentPageNumber: Long)
+data class MethodLinesDto(val data: List<MethodLine>, val count: Long, val currentPageNumber: Long)
