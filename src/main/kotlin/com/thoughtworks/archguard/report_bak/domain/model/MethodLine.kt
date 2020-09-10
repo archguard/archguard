d558422dc3af7e7fdca5146c938d20efdba6b4c7
dabuzhang <15770165+superPershing@users.noreply.github.com>
2020-09-10 11:51:25 +0800

    #477 delete useless code and config

--- src/main/kotlin/com/thoughtworks/archguard/report_bak/domain/model/MethodLine.kt
+++
@@ -1,8 +0,0 @@
-package com.thoughtworks.archguard.report_bak.domain.model
-
-data class MethodLine(val systemId: Long,
-                      val moduleName: String? = null,
-                      val packageName: String,
-                      val typeName: String,
-                      val methodName: String,
-                      val lines: Int)
--- src/main/kotlin/com/thoughtworks/archguard/report_bak/domain/service/MethodLinesDto.kt
+++
@@ -1,5 +0,0 @@
-package com.thoughtworks.archguard.report_bak.domain.service
-
-import com.thoughtworks.archguard.report_bak.domain.model.MethodLine
-
-data class MethodLinesDto(val methodLines: List<MethodLine>, val count: Long, val currentPageNumber: Long)
