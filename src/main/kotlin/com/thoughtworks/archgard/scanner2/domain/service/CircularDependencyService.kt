a8fde80e90746773ec41bbe028c4e7801a12ccd9
dabuzhang <15770165+superPershing@users.noreply.github.com>
2020-09-14 23:01:29 +0800

    #446 refactor CircularDependencyService

--- src/main/kotlin/com/thoughtworks/archgard/scanner2/domain/service/CircularDependencyService.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner2/domain/service/CircularDependencyService.kt
@@ -1,10 +1,12 @@
 package com.thoughtworks.archgard.scanner2.domain.service
 
 import com.thoughtworks.archgard.scanner2.domain.CycleDetector
+import com.thoughtworks.archgard.scanner2.domain.model.Graph
 import com.thoughtworks.archgard.scanner2.domain.model.GraphStore
 import com.thoughtworks.archgard.scanner2.domain.model.IdNode
 import com.thoughtworks.archgard.scanner2.domain.model.JClassVO
 import com.thoughtworks.archgard.scanner2.domain.model.JMethodVO
+import com.thoughtworks.archgard.scanner2.domain.model.Node
 import com.thoughtworks.archgard.scanner2.domain.repository.JClassRepository
 import com.thoughtworks.archgard.scanner2.domain.repository.JMethodRepository
 import org.springframework.stereotype.Service
@@ -14,31 +16,32 @@ import org.springframework.stereotype.Service
 class CircularDependencyService(val jClassRepository: JClassRepository, val jMethodRepository: JMethodRepository) {
     fun getClassCircularDependency(systemId: Long): List<List<JClassVO>> {
         val allClassDependencies = jClassRepository.getAllClassDependencies(systemId)
-        val graph = GraphStore()
-        allClassDependencies.forEach { graph.addEdge(IdNode(it.caller), IdNode(it.callee)) }
-        val cycleDetector = CycleDetector(graph.toDirectedGraph())
-        val cycles = cycleDetector.findCycles()
-        val jClassesHasModules = jClassRepository.getJClassesHasModules(systemId)
-        val jClassVOCycles = mutableListOf<List<JClassVO>>()
-        cycles.forEach {
-            val cycle = it.map { jClassesHasModules.first { jClass -> jClass.id == it.getNodeId() }.toVO() }
-            jClassVOCycles.add(cycle)
+        val cycles = findCyclesFromDependencies(allClassDependencies)
+        if (cycles.isEmpty()) {
+            return emptyList()
         }
-        return jClassVOCycles
+        val jClassesHasModules = jClassRepository.getJClassesHasModules(systemId)
+        return cycles.map { it.map { jClassesHasModules.first { jClass -> jClass.id == it.getNodeId() }.toVO() } }
     }
 
     fun getMethodCircularDependency(systemId: Long): List<List<JMethodVO>> {
         val allMethodDependencies = jMethodRepository.getAllMethodDependencies(systemId)
-        val graph = GraphStore()
-        allMethodDependencies.forEach { graph.addEdge(IdNode(it.caller), IdNode(it.callee)) }
-        val cycleDetector = CycleDetector(graph.toDirectedGraph())
-        val cycles = cycleDetector.findCycles()
-        val methodsHasModules = jMethodRepository.getMethodsHasModules(systemId)
-        val jMethodVOCycles = mutableListOf<List<JMethodVO>>()
-        cycles.forEach {
-            val cycle = it.map { methodsHasModules.first { jClass -> jClass.id == it.getNodeId() }.toVO() }
-            jMethodVOCycles.add(cycle)
+        val cycles = findCyclesFromDependencies(allMethodDependencies)
+        if (cycles.isEmpty()) {
+            return emptyList()
         }
-        return jMethodVOCycles
+        val methodsHasModules = jMethodRepository.getMethodsHasModules(systemId)
+        return cycles.map { it.map { methodsHasModules.first { jMethod -> jMethod.id == it.getNodeId() }.toVO() } }
+    }
+
+    private fun findCyclesFromDependencies(dependencies: List<Dependency<String>>): List<List<Node>> {
+        val graph = buildGraph(dependencies)
+        return CycleDetector(graph).findCycles()
+    }
+
+    private fun buildGraph(allClassDependencies: List<Dependency<String>>): Graph {
+        val graph = GraphStore()
+        allClassDependencies.forEach { graph.addEdge(IdNode(it.caller), IdNode(it.callee)) }
+        return graph.toDirectedGraph()
     }
 }
