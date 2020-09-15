c5fd8eeea98d987d36ba71c924693fdeb161c2f6
qicaisheng <qicaisheng@qq.com>
2020-09-15 14:09:42 +0800

    #474 refactor: remove unused code

--- src/main/kotlin/com/thoughtworks/archgard/scanner/domain/scanner/git/GitHotFileRepo.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/scanner/git/GitHotFileRepo.kt
@@ -2,6 +2,5 @@ package com.thoughtworks.archgard.scanner.domain.scanner.git
 
 interface GitHotFileRepo {
     fun save(gitHotFiles: List<GitHotFile>)
-    fun deleteBySystemId(systemId: Long)
     fun findBySystemId(systemId: Long) : List<GitHotFile>
 }
--- src/main/kotlin/com/thoughtworks/archgard/scanner/infrastructure/db/GitHotFileRepoImpl.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/infrastructure/db/GitHotFileRepoImpl.kt
@@ -11,11 +11,7 @@ class GitHotFileRepoImpl(val gitHotFileDao: GitHotFileDao) : GitHotFileRepo {
         gitHotFileDao.deleteBySystemId(gitHotFiles[0].systemId)
         gitHotFileDao.saveAll(gitHotFiles)
     }
-
-    override fun deleteBySystemId(systemId: Long) {
-        TODO("Not yet implemented")
-    }
-
+    
     override fun findBySystemId(systemId: Long): List<GitHotFile> {
         return gitHotFileDao.findBySystemId(systemId)
     }
