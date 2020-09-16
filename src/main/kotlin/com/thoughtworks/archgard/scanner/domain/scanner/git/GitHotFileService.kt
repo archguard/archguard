35e6ab19077b6b0571d404c5eac1965da9f004ad
qicaisheng <qicaisheng@qq.com>
2020-09-16 12:15:40 +0800

    #474 feat: add filter for getGitHotFilesBySystemId in GitHotFileService.kt

--- src/main/kotlin/com/thoughtworks/archgard/scanner/domain/scanner/git/GitHotFileService.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner/domain/scanner/git/GitHotFileService.kt
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Service
 @Service
 class GitHotFileService(val gitHotFileRepo: GitHotFileRepo) {
     fun getGitHotFilesBySystemId(systemId: Long) : List<GitHotFile> {
-        return gitHotFileRepo.findBySystemId(systemId)
+        return gitHotFileRepo.findBySystemId(systemId).filter { (it.jclassId != null) && (it.modifiedCount >= MODIFIED_COUNT_BASELINE) }
+    }
+
+    companion object {
+        const val MODIFIED_COUNT_BASELINE = 10
     }
 
 }
