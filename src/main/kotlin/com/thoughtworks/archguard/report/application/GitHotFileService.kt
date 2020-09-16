01ce3d81b8d0a20b2eab6375172803729aa650ba
qicaisheng <qicaisheng@qq.com>
2020-09-16 18:02:25 +0800

    #474 refactor: move modified_count_baseline config to application.properties

--- src/main/kotlin/com/thoughtworks/archguard/report/application/GitHotFileService.kt
+++ src/main/kotlin/com/thoughtworks/archguard/report/application/GitHotFileService.kt
@@ -2,16 +2,12 @@ package com.thoughtworks.archguard.report.application
 
 import com.thoughtworks.archguard.report.domain.githotfile.GitHotFile
 import com.thoughtworks.archguard.report.domain.githotfile.GitHotFileRepo
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class GitHotFileService(val gitHotFileRepo: GitHotFileRepo) {
+class GitHotFileService(val gitHotFileRepo: GitHotFileRepo, @Value("\${git_hot_file.modified_count_baseline}") val modifiedCountBaseline: Int) {
     fun getGitHotFilesBySystemId(systemId: Long) : List<GitHotFile> {
-        return gitHotFileRepo.findBySystemId(systemId).filter { (it.jclassId != null) && (it.modifiedCount >= MODIFIED_COUNT_BASELINE) }
+        return gitHotFileRepo.findBySystemId(systemId).filter { (it.jclassId != null) && (it.modifiedCount >= modifiedCountBaseline) }
     }
-
-    companion object {
-        const val MODIFIED_COUNT_BASELINE = 10
-    }
-
 }
