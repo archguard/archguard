323810f72859b06159ad107f186d3da3968d76b9
dabuzhang <15770165+superPershing@users.noreply.github.com>
2020-09-16 17:05:35 +0800

    #446 refactor circular dependency dto field name

--- src/main/kotlin/com/thoughtworks/archguard/report/domain/circulardependency/CircularDependencyListDto.kt
+++ src/main/kotlin/com/thoughtworks/archguard/report/domain/circulardependency/CircularDependencyListDto.kt
@@ -1,3 +1,3 @@
 package com.thoughtworks.archguard.report.domain.circulardependency
 
-data class CircularDependencyListDto(val circularDependencyList: List<String>, val circularDependencyCount: Long, val currentPageNumber: Long)
+data class CircularDependencyListDto(val data: List<String>, val count: Long, val currentPageNumber: Long)
