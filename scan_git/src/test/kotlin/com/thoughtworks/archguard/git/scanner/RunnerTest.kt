2fcf8ef1440031ac7d4ee4fb66a5921bd1db785d
Hu Le <le.hu.2010@163.com>
2020-09-17 16:00:27 +0800

    #458 feat: remove slf4j

--- scan_git/src/test/kotlin/com/thoughtworks/archguard/git/scanner/RunnerTest.kt
+++ scan_git/src/test/kotlin/com/thoughtworks/archguard/git/scanner/RunnerTest.kt
@@ -2,24 +2,19 @@ package com.thoughtworks.archguard.git.scanner
 
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Disabled
-import org.slf4j.LoggerFactory
 import java.io.File
 import java.util.*
 
 internal class RunnerTest {
 
-    val log = LoggerFactory.getLogger(RunnerTest::class.java)
-
     @Disabled
     fun run() {
-        log.info("start")
         val file = File("output.sql")
 //        file.deleteOnExit();
         val calendar = Calendar.getInstance()
-        calendar.set(2019,1,1)
+        calendar.set(2019, 1, 1)
 //        Runner().main(arrayOf("--git-path=/Users/ygdong/git/spring-framework", "--branch=master","--after=${calendar.timeInMillis}"))
         Runner().main(arrayOf("--git-path=/Users/ygdong/git/jeesite", "--branch=master"))
         assertTrue(file.exists())
-        log.info("结束")
     }
 }
