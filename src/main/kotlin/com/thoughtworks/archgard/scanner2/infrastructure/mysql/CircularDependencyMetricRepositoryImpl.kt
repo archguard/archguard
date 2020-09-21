14fb244d5ac0731fed2db8ad20f6dd65536bba93
dabuzhang <15770165+superPershing@users.noreply.github.com>
2020-09-21 12:30:00 +0800

    #446: use ; to split as separator in circular dependency

--- src/main/kotlin/com/thoughtworks/archgard/scanner2/infrastructure/mysql/CircularDependencyMetricRepositoryImpl.kt
+++ src/main/kotlin/com/thoughtworks/archgard/scanner2/infrastructure/mysql/CircularDependencyMetricRepositoryImpl.kt
@@ -11,7 +11,7 @@ class CircularDependencyMetricRepositoryImpl(val jdbi: Jdbi) : CircularDependenc
     override fun insertOrUpdateClassCircularDependency(systemId: Long, classCircularDependency: List<List<JClassVO>>) {
         deleteCircularDependency(systemId, type = CircularDependencyType.CLASS)
         classCircularDependency.forEach {
-            val circularDependency = it.joinToString(separator = ",") { it.fullName }
+            val circularDependency = it.joinToString(separator = ";") { it.fullName }
             saveCircularDependency(systemId, circularDependency, CircularDependencyType.CLASS)
         }
     }
@@ -19,7 +19,7 @@ class CircularDependencyMetricRepositoryImpl(val jdbi: Jdbi) : CircularDependenc
     override fun insertOrUpdateMethodCircularDependency(systemId: Long, methodCircularDependency: List<List<JMethodVO>>) {
         deleteCircularDependency(systemId, type = CircularDependencyType.METHOD)
         methodCircularDependency.forEach {
-            val circularDependency = it.joinToString(separator = ",") { it.fullName }
+            val circularDependency = it.joinToString(separator = ";") { it.fullName }
             saveCircularDependency(systemId, circularDependency, CircularDependencyType.METHOD)
         }
     }
@@ -27,7 +27,7 @@ class CircularDependencyMetricRepositoryImpl(val jdbi: Jdbi) : CircularDependenc
     override fun insertOrUpdateModuleCircularDependency(systemId: Long, moduleCircularDependency: List<List<String>>) {
         deleteCircularDependency(systemId, type = CircularDependencyType.MODULE)
         moduleCircularDependency.forEach {
-            val circularDependency = it.joinToString(separator = ",")
+            val circularDependency = it.joinToString(separator = ";")
             saveCircularDependency(systemId, circularDependency, CircularDependencyType.MODULE)
         }
     }
@@ -35,7 +35,7 @@ class CircularDependencyMetricRepositoryImpl(val jdbi: Jdbi) : CircularDependenc
     override fun insertOrUpdatePackageCircularDependency(systemId: Long, packageCircularDependency: List<List<String>>) {
         deleteCircularDependency(systemId, type = CircularDependencyType.PACKAGE)
         packageCircularDependency.forEach {
-            val circularDependency = it.joinToString(separator = ",")
+            val circularDependency = it.joinToString(separator = ";")
             saveCircularDependency(systemId, circularDependency, CircularDependencyType.PACKAGE)
         }
     }
