540dbea7949bcaeb085c4432a78582d8e463fae0
dabuzhang <15770165+superPershing@users.noreply.github.com>
2020-09-23 08:43:45 +0800

    #503: add package and module controller

--- src/main/kotlin/com/thoughtworks/archguard/report/controller/HubController.kt
+++ src/main/kotlin/com/thoughtworks/archguard/report/controller/HubController.kt
@@ -3,6 +3,8 @@ package com.thoughtworks.archguard.report.controller
 import com.thoughtworks.archguard.report.domain.hub.ClassHubListDto
 import com.thoughtworks.archguard.report.domain.hub.HubService
 import com.thoughtworks.archguard.report.domain.hub.MethodHubListDto
+import com.thoughtworks.archguard.report.domain.hub.ModuleHubListDto
+import com.thoughtworks.archguard.report.domain.hub.PackageHubListDto
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -26,6 +28,18 @@ class HubController(val hubService: HubService) {
     @Value("\${threshold.method.fanOut}")
     private val methodFanOutThreshold: Int = 0
 
+    @Value("\${threshold.package.fanIn}")
+    private val packageFanInThreshold: Int = 0
+
+    @Value("\${threshold.package.fanOut}")
+    private val packageFanOutThreshold: Int = 0
+
+    @Value("\${threshold.module.fanIn}")
+    private val moduleFanInThreshold: Int = 0
+
+    @Value("\${threshold.module.fanOut}")
+    private val moduleFanOutThreshold: Int = 0
+
     @GetMapping("/classes/above-threshold")
     fun getClassesAboveHubThreshold(@PathVariable("systemId") systemId: Long,
                                     @RequestParam(value = "numberPerPage") limit: Long,
@@ -42,5 +56,23 @@ class HubController(val hubService: HubService) {
                                     @RequestParam(value = "orderByFanIn") orderByFanIn: Boolean): ResponseEntity<MethodHubListDto> {
         val offset = (currentPageNumber - 1) * limit
         return ResponseEntity.ok(hubService.getMethodHubListAboveThreshold(systemId, methodFanInThreshold, methodFanOutThreshold, limit, offset, orderByFanIn))
+    }
+
+    @GetMapping("/packages/above-threshold")
+    fun getPackagesAboveHubThreshold(@PathVariable("systemId") systemId: Long,
+                                     @RequestParam(value = "numberPerPage") limit: Long,
+                                     @RequestParam(value = "currentPageNumber") currentPageNumber: Long,
+                                     @RequestParam(value = "orderByFanIn") orderByFanIn: Boolean): ResponseEntity<PackageHubListDto> {
+        val offset = (currentPageNumber - 1) * limit
+        return ResponseEntity.ok(hubService.getPackageHubListAboveThreshold(systemId, packageFanInThreshold, packageFanOutThreshold, limit, offset, orderByFanIn))
+    }
+
+    @GetMapping("/modules/above-threshold")
+    fun getModulesAboveHubThreshold(@PathVariable("systemId") systemId: Long,
+                                    @RequestParam(value = "numberPerPage") limit: Long,
+                                    @RequestParam(value = "currentPageNumber") currentPageNumber: Long,
+                                    @RequestParam(value = "orderByFanIn") orderByFanIn: Boolean): ResponseEntity<ModuleHubListDto> {
+        val offset = (currentPageNumber - 1) * limit
+        return ResponseEntity.ok(hubService.getModuleHubListAboveThreshold(systemId, moduleFanInThreshold, moduleFanOutThreshold, limit, offset, orderByFanIn))
     }
 }
