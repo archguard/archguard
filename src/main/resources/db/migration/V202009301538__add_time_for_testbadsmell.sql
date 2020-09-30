fa3a4f06a05cc4249556929c0ba12eafd1ca05b3
dabuzhang <15770165+superPershing@users.noreply.github.com>
2020-09-30 15:54:59 +0800

    Revert "#N/A: run flyway in Jenkinsfile"
    This reverts commit 5ea0bf9f

--- src/main/resources/db/migration/V202009301538__add_time_for_testbadsmell.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE testBadSmell
-    ADD `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    ADD `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间';
