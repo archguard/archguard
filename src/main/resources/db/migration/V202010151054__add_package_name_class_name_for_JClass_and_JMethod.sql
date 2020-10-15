768c09198f60fae5c406dc6c35c858290fe7d08f
Hu Le <le.hu.2010@163.com>
2020-10-15 11:09:05 +0800

    #547 fix:add package name and class name

---
+++ src/main/resources/db/migration/V202010151054__add_package_name_class_name_for_JClass_and_JMethod.sql
@@ -0,0 +1,4 @@
+alter table JMethod add column `package_name` mediumtext;
+alter table JMethod add column `class_name` mediumtext;
+alter table JClass add column `class_name` mediumtext;
+alter table JClass add column `package_name` mediumtext;
