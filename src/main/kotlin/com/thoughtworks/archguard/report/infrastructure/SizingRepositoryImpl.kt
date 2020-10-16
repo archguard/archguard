8c7f5b0d571c6fd94e7306cbdb43c6f2905d2bfc
Hu Le <le.hu.2010@163.com>
2020-10-16 10:09:27 +0800

    547 fix: correct sql for package class count

--- src/main/kotlin/com/thoughtworks/archguard/report/infrastructure/SizingRepositoryImpl.kt
+++ src/main/kotlin/com/thoughtworks/archguard/report/infrastructure/SizingRepositoryImpl.kt
@@ -185,7 +185,7 @@ class SizingRepositoryImpl(val jdbi: Jdbi) : SizingRepository {
     override fun getPackageSizingListAboveClassCountThreshold(systemId: Long, threshold: Int, limit: Long, offset: Long): List<PackageSizing> {
         return jdbi.withHandle<List<PackageSizing>, Exception> {
             val sql = """
-                select uuid() as id, count(name) as classCount,sum(loc) as `lines` module as moduleName, system_id, package_name from JClass
+                select uuid() as id, count(name) as classCount,sum(loc) as `lines`, module as moduleName, system_id, package_name from JClass
                     where system_id =:systemId and is_test=false and loc is not NULL group by module, package_name
                     having count > :threshold order by count desc
                     limit :limit offset :offset
