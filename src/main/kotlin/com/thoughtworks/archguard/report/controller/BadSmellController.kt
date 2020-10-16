9936d9a149418cccee4f683d9b51101610abc3fd
Hu Le <le.hu.2010@163.com>
2020-10-16 15:40:09 +0800

    547 feature: make fake badsemll to list

--- src/main/kotlin/com/thoughtworks/archguard/report/controller/BadSmellController.kt
+++ src/main/kotlin/com/thoughtworks/archguard/report/controller/BadSmellController.kt
@@ -22,6 +22,17 @@ class BadSmellController {
                         BadSmellThreshold("枢纽包", "出向依赖或入向依赖  > 指标阈值", 8),
                         BadSmellThreshold("枢纽类", "出向依赖或入向依赖 > 指标阈值", 8)
                 ))
+        )), BadSmellSuite("id1", "架构评估二级指标", false, listOf(
+                BadSmellGroup("体量维度", listOf(
+                        BadSmellThreshold("过大的方法", "方法代码行数 > 指标阈值", 30),
+                        BadSmellThreshold("过大的类", "方法个数 > 指标阈值", 20),
+                        BadSmellThreshold("过大的类", "方法代码行数 > 指标阈值", 50)
+                )),
+                BadSmellGroup("耦合纬度", listOf(
+                        BadSmellThreshold("枢纽模块", "出向依赖或入向依赖 > 指标阈值", 8),
+                        BadSmellThreshold("枢纽包", "出向依赖或入向依赖  > 指标阈值", 8),
+                        BadSmellThreshold("枢纽类", "出向依赖或入向依赖 > 指标阈值", 8)
+                ))
         )))
     }
 }
