4f4b14a88bcad6b5524b2971bb288f6a9f3c1b10
LiangLliu <max1141847753@gmail.com>
2020-11-05 16:26:05 +0800

    update setting.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "archguard-addition"
+
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+        gradlePluginPortal()
+    }
+}
