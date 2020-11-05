c0725e2dcdd8b411eb215ded652f565d3e4a94d3
LiangLliu <max1141847753@gmail.com>
2020-11-05 10:37:36 +0800

    add zip file path to test

--- src/test/kotlin/com/thoughtworks/archguard/ApplicationTests.kt
+++ src/test/kotlin/com/thoughtworks/archguard/ApplicationTests.kt
@@ -2,8 +2,10 @@ package com.thoughtworks.archguard
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Test
