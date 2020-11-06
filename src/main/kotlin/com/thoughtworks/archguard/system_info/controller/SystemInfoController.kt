4f142494783709f2c7109427901ce2641e3ae001
LiangLliu <max1141847753@gmail.com>
2020-11-06 13:40:23 +0800

    update upload zip file API

--- src/main/kotlin/com/thoughtworks/archguard/system_info/controller/SystemInfoController.kt
+++ src/main/kotlin/com/thoughtworks/archguard/system_info/controller/SystemInfoController.kt
@@ -1,19 +1,11 @@
 package com.thoughtworks.archguard.system_info.controller
 
+import com.thoughtworks.archguard.common.CreateFileUtil
 import com.thoughtworks.archguard.system_info.domain.SystemInfo
 import com.thoughtworks.archguard.system_info.domain.SystemInfoService
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -58,7 +50,9 @@ class SystemInfoController(
             return "上传失败，请选择文件"
         }
 
-        val dir = createTempDir()
+        val createDir = CreateFileUtil.createDir(zipFilePath)
+
+        val dir = createTempDir("temp", ".tmp", createDir)
         val fileName = file.originalFilename ?: "System-Info"
         val filePath = this.prepareZipFile(dir.absolutePath, fileName)
         file.transferTo(filePath.toFile())
