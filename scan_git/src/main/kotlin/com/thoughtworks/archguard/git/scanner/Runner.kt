5a61a8e1e6d9fbf1894bf686ef11838aa1c81e1b
Phodal Huang <h@phodal.com>
2022-02-21 20:02:31 +0800

    fix: fix typos

--- scan_git/src/main/kotlin/com/thoughtworks/archguard/git/scanner/Runner.kt
+++ scan_git/src/main/kotlin/com/thoughtworks/archguard/git/scanner/Runner.kt
@@ -7,15 +7,15 @@ import com.thoughtworks.archguard.git.scanner.helper.Bean2Sql
 import com.thoughtworks.archguard.git.scanner.loc.LoCRepository
 import com.thoughtworks.archguard.git.scanner.loc.java.JavaLoCApp
 import com.thoughtworks.archguard.git.scanner.loc.kotlin.KotlinLoCApp
-import com.github.ajalt.clikt.parameters.options.default as default1
+import com.github.ajalt.clikt.parameters.options.default
 
 class Runner : CliktCommand(help = "scan git to sql") {
-    private val branch: String by option(help = "git repository branch").default1("master")
-    private val after: String by option(help = "scanner only scan commits after this timestamp").default1("0")
+    private val branch: String by option(help = "git repository branch").default("master")
+    private val after: String by option(help = "scanner only scan commits after this timestamp").default("0")
 
-    private val path: String by option(help = "local path").default1(".")
-    private val repoId: String by option(help = "repo id").default1("0")
-    private val systemId: String by option(help = "system id").default1("0")
+    private val path: String by option(help = "local path").default(".")
+    private val repoId: String by option(help = "repo id").default("0")
+    private val systemId: String by option(help = "system id").default("0")
     private val loc: String? by option(help = "scan loc")
 
     override fun run() {
