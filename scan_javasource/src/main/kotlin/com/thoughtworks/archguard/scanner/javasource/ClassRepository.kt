498677d69c08a561a8c6726779207fcf467cec01
Phodal Huang <h@phodal.com>
2022-02-22 08:12:15 +0800

    feat(javasoucre): add field support

--- scan_javasource/src/main/kotlin/com/thoughtworks/archguard/scanner/javasource/ClassRepository.kt
+++ scan_javasource/src/main/kotlin/com/thoughtworks/archguard/scanner/javasource/ClassRepository.kt
@@ -1,6 +1,7 @@
 package com.thoughtworks.archguard.scanner.javasource
 
 import chapi.domain.core.CodeDataStruct
+import chapi.domain.core.CodeField
 import infrastructure.SourceBatch
 import java.text.SimpleDateFormat
 import java.util.*
@@ -14,10 +15,35 @@ class ClassRepository(systemId: String) {
     }
 
     fun saveClassElement(clz: CodeDataStruct) {
-        saveClass(clz)
+        val clzId = saveClass(clz)
+        saveClassFields(clzId, clz.Fields, clz.NodeName)
     }
 
-    private fun saveClass(clz: CodeDataStruct) {
+    private fun saveClassFields(clzId: String, fields: Array<CodeField>, clzName: String) {
+        for (field in fields) {
+            val id = generateId()
+            val time: String = currentTime
+            val values: MutableMap<String, String> = HashMap()
+
+            values["id"] = id
+            values["system_id"] = systemId
+            values["name"] = field.TypeValue
+            values["clzname"] = clzName
+            values["type"] = field.TypeType
+            values["updatedAt"] = time
+            values["createdAt"] = time
+            batch.add("JField", values)
+
+            val relation: MutableMap<String, String> = HashMap()
+            relation["id"] = generateId()
+            relation["system_id"] = systemId
+            relation["a"] = clzId
+            relation["b"] = id
+            batch.add("_ClassFields", relation)
+        }
+    }
+
+    private fun saveClass(clz: CodeDataStruct): String {
         val time = currentTime
         val clzId = generateId()
         val values: MutableMap<String, String> = HashMap()
@@ -33,6 +59,8 @@ class ClassRepository(systemId: String) {
         values["class_name"] = clz.NodeName
         values["access"] = "todo"
         batch.add("JClass", values)
+
+        return clzId
     }
 
     companion object {
