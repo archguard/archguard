62e33eb7ce5fc5201ec7fbb59b97b1a77640f376
Phodal Huang <h@phodal.com>
2022-03-17 08:19:12 +0800

    test: add simple debug info for threhold suite

--- src/main/kotlin/com/thoughtworks/archguard/report/infrastructure/ThresholdSuiteRepositoryImpl.kt
+++ src/main/kotlin/com/thoughtworks/archguard/report/infrastructure/ThresholdSuiteRepositoryImpl.kt
@@ -5,16 +5,22 @@ import com.thoughtworks.archguard.report.domain.badsmell.ThresholdSuiteRepositor
 import com.thoughtworks.archguard.report.infrastructure.BadSmellSuitePO
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.mapper.reflect.ConstructorMapper
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 
 @Repository
 class ThresholdSuiteRepositoryImpl(val jdbi: Jdbi) : ThresholdSuiteRepository {
+
+    private val logger = LoggerFactory.getLogger(ThresholdSuiteRepositoryImpl::class.java)
 
     override fun getAllBadSmellThresholdSuites(): List<ThresholdSuite> {
         val sql = "select a.id, a.suite_name, a.thresholds, GROUP_CONCAT(a.systemId SEPARATOR ', ') as systemIds from " +
                 "(select bs.id, bs.suite_name, bs.thresholds, si.id as systemId " +
                 "from bad_smell_threshold_suite bs, system_info si where bs.id = si.threshold_suite_id) as a " +
                 "group by a.id;"
+
+        logger.info("ThresholdSuiteRepositoryImpl: $sql")
+
         return jdbi.withHandle<List<BadSmellSuitePO>, Nothing> {
             it.registerRowMapper(ConstructorMapper.factory(BadSmellSuitePO::class.java))
             it.createQuery(sql)
