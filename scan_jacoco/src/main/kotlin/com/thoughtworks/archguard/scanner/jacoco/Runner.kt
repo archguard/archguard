78af2291a5792e098cc3d88be8d11b46f1b1fee0
Phodal Huang <h@phodal.com>
2022-03-18 21:35:12 +0800

    fix(jacoco): fix default path issue

--- scan_jacoco/src/main/kotlin/com/thoughtworks/archguard/scanner/jacoco/Runner.kt
+++ scan_jacoco/src/main/kotlin/com/thoughtworks/archguard/scanner/jacoco/Runner.kt
@@ -6,8 +6,8 @@ import com.github.ajalt.clikt.parameters.options.option
 
 class Runner : CliktCommand() {
     private val targetProject: String by option(help = "the path of target proejct").default(".")
-    private val bin: String by option(help = "relative path of binary").default("target/classes")
-    private val exec: String by option(help = "jacoco data file").default("target/jacoco.exec")
+    private val bin: String by option(help = "relative path of binary").default("build/classes")
+    private val exec: String by option(help = "jacoco data file").default("build/jacoco（/jacoco.exec")
 
     override fun run() {
         Service(Bean2Sql()).readJacoco(Config(projectPath = targetProject, bin = bin, exec = exec))
