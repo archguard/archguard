2cda84507655050ccbcc303f5afc986b24a3d7e7
Yunzhi Liu <yzliu@thoughtworks.com>
2022-03-22 14:36:13 +0800

    refactor: remove unused codes in the Scanner2Client class

--- src/main/kotlin/com/thoughtworks/archguard/scanner/infrastructure/client/Scanner2Client.kt
+++ src/main/kotlin/com/thoughtworks/archguard/scanner/infrastructure/client/Scanner2Client.kt
@@ -5,19 +5,18 @@ import com.thoughtworks.archguard.scanner2.controller.MetricController
 import com.thoughtworks.archguard.scanner2.controller.ShotgunSurgeryController
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import java.util.*
 
 
 @Component
-class Scanner2Client(val metricController: MetricController,
-                     val shotgunSurgeryController: ShotgunSurgeryController,
-                     val accessController: AccessController) {
+class Scanner2Client(
+    val metricController: MetricController,
+    val shotgunSurgeryController: ShotgunSurgeryController,
+    val accessController: AccessController,
+) {
 
     private val log = LoggerFactory.getLogger(Scanner2Client::class.java)
 
     fun level2MetricsAnalysis(systemId: Long) {
-        val params = HashMap<String, Any>()
-        params["systemId"] = systemId
         metricController.persistBasicMetrics(systemId)
         metricController.persistCircularDependencyMetrics(systemId)
         metricController.persistDataClasses(systemId)
