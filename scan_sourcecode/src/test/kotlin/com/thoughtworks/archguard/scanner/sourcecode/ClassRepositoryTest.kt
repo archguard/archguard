1b4e28a9234efa735868498c86af2d358efd6f7e
Phodal Huang <h@phodal.com>
2022-03-22 11:37:47 +0800

    test(ts): add notify for package name

--- scan_sourcecode/src/test/kotlin/com/thoughtworks/archguard/scanner/sourcecode/ClassRepositoryTest.kt
+++ scan_sourcecode/src/test/kotlin/com/thoughtworks/archguard/scanner/sourcecode/ClassRepositoryTest.kt
@@ -2,6 +2,7 @@ package com.thoughtworks.archguard.scanner.sourcecode
 
 import chapi.domain.core.CodeDataStruct
 import chapi.domain.core.CodeFunction
+import chapi.domain.core.CodeImport
 import org.junit.jupiter.api.Test
 import java.util.HashMap
 import kotlin.test.assertEquals
@@ -37,7 +38,6 @@ internal class ClassRepositoryTest {
 
     @Test
     internal fun renaming_for_react_component() {
-        //  {"NodeName":"default","Package":"@.pages.analysis.dependence.ModuleDependence.index","FilePath":"src/pages/analysis/dependence/ModuleDependence/index.tsx","Fields":[],"MultipleExtend":[],"Implements":[],"Functions":[{"Name":"ModuleDependence","FilePath":"src/pages/analysis/dependence/ModuleDependence/index.tsx","MultipleReturns":[],"Parameters":[{"Modifiers":[],"TypeValue":"{location}","TypeType":"any","Annotations":[],"ObjectValue":[],"ReturnTypes":[],"Parameters":[]}],"FunctionCalls":[{"Parameters":[{"Modifiers":[],"TypeValue":"","TypeType":"object","Annotations":[],"ObjectValue":[],"ReturnTypes":[],"Parameters":[]}],"Position":{}}],"Annotations":[],"Modifiers":[],"InnerStructures":[],"InnerFunctions":[],"Position":{"StartLine":7,"StartLinePosition":7,"StopLine":15},"LocalVariables":[],"IsReturnHtml":true}],"InnerStructures":[],"Annotations":[],"FunctionCalls":[],"Parameters":[],"Imports":[{"Source":"react","UsageName":["React"]},{"Source":"./components/ModuleConfig","UsageName":["ModuleConfig"]},{"Source":"./components/ModuleDependenceGraph","UsageName":["ModuleDependenceGraph"]},{"Source":"./components/ModuleDependenceTable","UsageName":["ModuleDependenceTable"]},{"Source":"./index.less","UsageName":[]}],"Exports":[]}
         var ds: Array<CodeDataStruct> = arrayOf()
         val codeFunction = CodeFunction(Name = "ModuleDependence", IsReturnHtml = true)
         ds += CodeDataStruct(
@@ -56,6 +56,36 @@ internal class ClassRepositoryTest {
 
         val keys: MutableMap<String, String> = HashMap()
         keys["name"] = "@.pages.analysis.dependence.ModuleDependence.ModuleDependence"
+        keys["module"] = "root"
+
+        val value = clzRepo.findId("code_class", keys)
+        assertEquals(true, value!!.isPresent)
+
+        clzRepo.close()
+    }
+
+    @Test
+    internal fun dependencies_for_typescript_component() {
+        var ds: Array<CodeDataStruct> = arrayOf()
+        val codeFunction = CodeFunction(Name = "ModuleDependence", IsReturnHtml = true)
+        val imp = CodeImport(Source = "ModuleDependenceArgsForm", UsageName = arrayOf("ModuleDependenceArgsForm"))
+        ds += CodeDataStruct(
+            Package = "@.pages.analysis.dependence.ModuleDependence.index",
+            NodeName = "default",
+            Functions = arrayOf(codeFunction),
+            Imports = arrayOf(imp)
+        )
+
+        val clzRepo = ClassRepository("1", "typescript", "archguard")
+        ds.forEach {
+            clzRepo.saveClassItem(it)
+        }
+        ds.forEach {
+            clzRepo.saveClassBody(it)
+        }
+
+        val keys: MutableMap<String, String> = HashMap()
+        keys["name"] = "ModuleDependenceArgsForm"
         keys["module"] = "root"
 
         val value = clzRepo.findId("code_class", keys)
