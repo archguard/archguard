08822951924fb12cfc6bae4d3988730b284298e6
Phodal Huang <h@phodal.com>
2022-03-25 14:06:14 +0800

    feat(collector): init ci

---
+++ collector_ci/src/main/kotlin/org/archguard/collector/ci/Runner.kt
@@ -0,0 +1,17 @@
+package org.archguard.collector.ci
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+
+class Runner : CliktCommand() {
+    private val project: String by option(help = "the path of target project").default(".")
+
+    override fun run() {
+        println(project)
+    }
+}
+
+fun main(args: Array<String>) = Runner().main(args)
+
+
