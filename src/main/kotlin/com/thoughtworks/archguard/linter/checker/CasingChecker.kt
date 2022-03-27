ef01deb1a1411e94c73eb4ad88faf45ed1d7b09b
Phodal Huang <h@phodal.com>
2022-03-27 14:50:12 +0800

    feat: init api casing checker

--- src/main/kotlin/com/thoughtworks/archguard/linter/checker/CasingChecker.kt
+++ src/main/kotlin/com/thoughtworks/archguard/linter/checker/CasingChecker.kt
@@ -11,28 +11,74 @@ class CasingChecker {
     val SNAKE_CASING = Regex("[a-z][a-z0-9]*(?:_[a-z0-9]+)*")
     val MACRO_CASING = Regex("[A-Z][A-Z0-9]*(?:_[A-Z0-9]+)*")
 
+    fun checkNaming(list: List<String>): MutableMap<String, MutableList<String>> {
+        val multipleNaming: MutableMap<String, MutableList<String>> = hashMapOf()
+        list.forEach {
+            if (it.isNotEmpty()) {
+                if (this.IsPacal(it)) {
+                    val key = "pascal"
+                    val pascal = multipleNaming.getOrDefault(key, mutableListOf())
+                    pascal.add(it)
+                    multipleNaming[key] = pascal
+                }
+                if (this.`is-kebab`(it)) {
+                    val key = "kebab"
+                    val kebab = multipleNaming.getOrDefault(key, mutableListOf())
+                    kebab.add(it)
+                    multipleNaming[key] = kebab
+                }
+                if (this.`IS-COBOL`(it)) {
+                    val key = "cobol"
+                    val cobol = multipleNaming.getOrDefault(key, mutableListOf())
+                    cobol.add(it)
+                    multipleNaming[key] = cobol
+                }
+                if (this.is_nake(it)) {
+                    val key = "snake"
+                    val snake = multipleNaming.getOrDefault(key, mutableListOf())
+                    snake.add(it)
+                    multipleNaming[key] = snake
+                }
+                if (this.IS_MACRO(it)) {
+                    val key = "macro"
+                    val macro = multipleNaming.getOrDefault(key, mutableListOf())
+                    macro.add(it)
+                    multipleNaming[key] = macro
+                }
+                if (this.isCamel(it)) {
+                    val key = "camel"
+                    val camel = multipleNaming.getOrDefault(key, mutableListOf())
+                    camel.add(it)
+                    multipleNaming[key] = camel
+                }
+            }
+        }
+
+        return multipleNaming
+    }
+
     // PascalNaming
-    fun isPacal(s: String): Boolean {
+    fun IsPacal(s: String): Boolean {
         return PASCAL_CASING.matches(s)
     }
 
     // kebab-naming
-    fun isKebab(s: String): Boolean {
+    fun `is-kebab`(s: String): Boolean {
         return KEBAB_CASING.matches(s)
     }
 
     // snake_naming
-    fun isSnake(s: String): Boolean {
+    fun is_nake(s: String): Boolean {
         return SNAKE_CASING.matches(s)
     }
 
     // SNAKE-NAMING
-    fun isCOBOL(s: String): Boolean {
+    fun `IS-COBOL`(s: String): Boolean {
         return COBOL_CASING.matches(s)
     }
 
     // MACRO_NAMING
-    fun isMacro(s: String): Boolean {
+    fun IS_MACRO(s: String): Boolean {
         return MACRO_CASING.matches(s)
     }
 
--- src/test/kotlin/com/thoughtworks/archguard/linter/checker/CasingCheckerTest.kt
+++ src/test/kotlin/com/thoughtworks/archguard/linter/checker/CasingCheckerTest.kt
@@ -1,15 +1,22 @@
 package com.thoughtworks.archguard.linter.checker
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class CasingCheckerTest {
     @Test
     fun testPascalUrl() {
-        assert(CasingChecker().isPacal("PascalNaming"))
-        assert(CasingChecker().isKebab("kebab-naming"))
-        assert(CasingChecker().isSnake("snake_naming"))
-        assert(CasingChecker().isCOBOL("SNAKE-NAMING"))
-        assert(CasingChecker().isMacro("MACRO_NAMING"))
+        assert(CasingChecker().IsPacal("PascalNaming"))
+        assert(CasingChecker().`is-kebab`("kebab-naming"))
+        assert(CasingChecker().is_nake("snake_naming"))
+        assert(CasingChecker().`IS-COBOL`("SNAKE-NAMING"))
+        assert(CasingChecker().IS_MACRO("MACRO_NAMING"))
         assert(CasingChecker().isCamel("camelNaming"))
+    }
+
+    @Test
+    fun checkMultipleNamingStyle() {
+        val url = "/api/demo-api/MACRO".split("/")
+        assertEquals(4, CasingChecker().checkNaming(url).size)
     }
 }
