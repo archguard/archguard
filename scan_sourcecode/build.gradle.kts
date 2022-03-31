44dffa4d418e0e0f478522f77956a2448fa64050
Phodal Huang <h@phodal.com>
2022-03-31 13:39:47 +0800

    build: update chapi to reduce log

--- scan_sourcecode/build.gradle.kts
+++ scan_sourcecode/build.gradle.kts
@@ -30,15 +30,15 @@ dependencies {
 //    implementation("com.phodal.chapi:chapi-application:1.3.0-SNAPSHOT")
 //    implementation("com.phodal.chapi:chapi-domain:1.3.0-SNAPSHOT")
 
-    implementation("com.phodal.chapi:chapi-application:1.4.0")
-    implementation("com.phodal.chapi:chapi-domain:1.4.0")
+    implementation("com.phodal.chapi:chapi-application:1.4.1")
+    implementation("com.phodal.chapi:chapi-domain:1.4.1")
 
     implementation(kotlin("test"))
     implementation(kotlin("test-junit"))
 
     // JUnit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 application {
