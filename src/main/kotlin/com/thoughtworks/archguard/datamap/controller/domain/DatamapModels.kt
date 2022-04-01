1454376479955566aa7d305c4e056ebd5d5e8574
Phodal Huang <h@phodal.com>
2022-04-01 23:26:30 +0800

    fix(datamap): fix id issues

--- src/main/kotlin/com/thoughtworks/archguard/datamap/controller/domain/DatamapModels.kt
+++ src/main/kotlin/com/thoughtworks/archguard/datamap/controller/domain/DatamapModels.kt
@@ -1,7 +1,7 @@
 package com.thoughtworks.archguard.datamap.controller.domain
 
 data class Datamap(
-    val id: Long,
+    val id: String,
     val packageName: String,
     val className: String,
     val functionName: String,
--- src/main/kotlin/com/thoughtworks/archguard/datamap/controller/infrastructure/DatamapRepositoryImpl.kt
+++ src/main/kotlin/com/thoughtworks/archguard/datamap/controller/infrastructure/DatamapRepositoryImpl.kt
@@ -10,7 +10,7 @@ class DatamapRepositoryImpl(val jdbi: Jdbi) : DatamapRepository {
     override fun getDatamapBySystemId(systemId: Long): List<Datamap> {
         val sql = "select id, package_name as packageName, class_name as className, function_name as functionName," +
                 " system_id as systemId, tables " +
-                " from data_code_database_relation where id=:systemId"
+                " from data_code_database_relation where system_id=:systemId"
         return jdbi.withHandle<List<Datamap>, Exception> {
             it.createQuery(sql)
                 .bind("systemId", systemId)
