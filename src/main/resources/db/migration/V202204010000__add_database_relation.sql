cabf2f85adffbdc1983033add1502174cfad3290
Phodal Huang <h@phodal.com>
2022-04-01 11:00:31 +0800

    docs: add lost systemid

--- src/main/resources/db/migration/V202204010000__add_database_relation.sql
+++ src/main/resources/db/migration/V202204010000__add_database_relation.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `data_code_database_relation`
 (
-    `id`            VARCHAR(36)    NOT NULL PRIMARY KEY,
-    `package_name`  VARCHAR(1024)  NOT NULL,
-    `class_name`    VARCHAR(1024)  NOT NULL,
-    `function_name` VARCHAR(1024)  NOT NULL,
-    `tables`        VARCHAR(2048)  NOT NULL,
-    `sqls`          TEXT(32768) NOT NULL
+    `id`            VARCHAR(36)   NOT NULL PRIMARY KEY,
+    `system_id`     VARCHAR(36)   NOT NULL,
+    `package_name`  VARCHAR(1024) NOT NULL,
+    `class_name`    VARCHAR(1024) NOT NULL,
+    `function_name` VARCHAR(1024) NOT NULL,
+    `tables`        VARCHAR(2048) NOT NULL,
+    `sqls`          TEXT(32768)   NOT NULL
 );
