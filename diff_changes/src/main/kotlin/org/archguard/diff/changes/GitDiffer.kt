0f39383e59eea18232cf46b4a8d21a2d6783393b
Phodal Huang <h@phodal.com>
2022-04-03 23:34:33 +0800

    feat: add differ for list names

--- diff_changes/src/main/kotlin/org/archguard/diff/changes/GitDiffer.kt
+++ diff_changes/src/main/kotlin/org/archguard/diff/changes/GitDiffer.kt
@@ -1,15 +1,23 @@
 package org.archguard.diff.changes
 
 import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.diff.DiffEntry
-import org.eclipse.jgit.diff.DiffFormatter
-import org.eclipse.jgit.diff.RawTextComparator
+import org.eclipse.jgit.dircache.DirCacheIterator
 import org.eclipse.jgit.lib.ObjectId
+import org.eclipse.jgit.lib.ObjectLoader
 import org.eclipse.jgit.lib.Repository
 import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.revwalk.RevWalk
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
-import org.eclipse.jgit.util.io.DisabledOutputStream
+import org.eclipse.jgit.treewalk.FileTreeIterator
+import org.eclipse.jgit.treewalk.TreeWalk
 import java.io.File
+import java.nio.charset.StandardCharsets
+
+class DifferFile(
+    val content: String,
+    val hash: String,
+    val path: String,
+)
 
 object GitDiffer {
     fun getRange(path: String, branch: String = "master", sinceRev: String, untilRev: String) {
@@ -19,45 +27,44 @@ object GitDiffer {
 
         val since: ObjectId = git.repository.resolve(sinceRev)
         val until: ObjectId = git.repository.resolve(untilRev)
+
+        createBaselineAst(repository, since)
+
         for (commit in git.log().addRange(since, until).call()) {
-            toChangeEntry(repository, commit)
+            // todo: add increment for path changes
         }
     }
 
-    private fun toChangeEntry(repository: Repository, revCommit: RevCommit) {
-        val diffFormatter = DiffFormatter(DisabledOutputStream.INSTANCE).config(repository)
-        diffFormatter.scan(getParent(revCommit)?.tree, revCommit.tree)
-            .map { d -> doConvertToChangeEntry(d, repository, revCommit, diffFormatter) }
-    }
+    private fun createBaselineAst(repository: Repository, since: ObjectId) {
+        val rw = RevWalk(repository)
+        val tw = TreeWalk(repository)
 
-    private fun doConvertToChangeEntry(
-        diffEntry: DiffEntry,
-        repository: Repository,
-        revCommit: RevCommit,
-        df: DiffFormatter
-    ) {
-        try {
-            println(diffEntry.oldPath)
-            println(diffEntry.newPath)
-            println(diffEntry.changeType.name)
-        } catch (ex: Exception) {
-            throw ex
+        val commitToCheck: RevCommit = rw.parseCommit(since)
+        tw.addTree(commitToCheck.tree)
+        tw.addTree(DirCacheIterator(repository.readDirCache()))
+        tw.addTree(FileTreeIterator(repository))
+
+        tw.isRecursive = true;
+        while (tw.next()) {
+
+            try {
+                val pathString = tw.pathString
+                val blobId: ObjectId = tw.getObjectId(0)
+
+                if (pathString.endsWith(".kt")) {
+                    println(tw.pathString)
+
+                    repository.newObjectReader().use { objectReader ->
+                        val objectLoader: ObjectLoader = objectReader.open(blobId)
+                        val bytes: ByteArray = objectLoader.bytes
+                        val content = String(bytes, StandardCharsets.UTF_8)
+                        content
+                    }
+                }
+            } catch (e: Exception) {
+
+            }
         }
-
-    }
-
-    private fun getParent(revCommit: RevCommit): RevCommit? {
-        return if (revCommit.parentCount == 0) {
-            null
-        } else
-            revCommit.getParent(0)
-    }
-
-    private fun DiffFormatter.config(repository: Repository): DiffFormatter {
-        setRepository(repository)
-        setDiffComparator(RawTextComparator.DEFAULT)
-        isDetectRenames = true
-        return this
     }
 
     private fun Git.specifyBranch(branch: String): Git {
