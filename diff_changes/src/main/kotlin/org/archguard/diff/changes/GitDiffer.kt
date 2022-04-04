c8f24f7e97353df24fead5ceb76a2a6b50e72393
Phodal Huang <h@phodal.com>
2022-04-04 21:15:33 +0800

    feat: add differ for changed files

--- diff_changes/src/main/kotlin/org/archguard/diff/changes/GitDiffer.kt
+++ diff_changes/src/main/kotlin/org/archguard/diff/changes/GitDiffer.kt
@@ -15,13 +15,21 @@ import org.eclipse.jgit.treewalk.TreeWalk
 import java.io.File
 import java.nio.charset.StandardCharsets
 import chapi.domain.core.CodeDataStruct
+import chapi.domain.core.CodeFunction
+import org.eclipse.jgit.diff.DiffEntry
+import org.eclipse.jgit.diff.DiffFormatter
+import org.eclipse.jgit.diff.RawTextComparator
+import org.eclipse.jgit.util.io.DisabledOutputStream
 
 class DifferFile(
     val path: String,
-    val dataStructs: Array<CodeDataStruct>
+    val dataStructs: Array<CodeDataStruct>,
+//  val functionStrings: List<String>  // todo: maybe just compare function in string will be quickly?
 )
 
 class GitDiffer(val path: String, val branch: String, val systemId: String, val language: String) {
+    val differFileMap: MutableMap<String, DifferFile> = mutableMapOf()
+
     fun countInRange(sinceRev: String, untilRev: String) {
         val repository = FileRepositoryBuilder().findGitDir(File(path)).build()
         val git = Git(repository).specifyBranch(branch)
@@ -30,10 +38,52 @@ class GitDiffer(val path: String, val branch: String, val systemId: String, val 
         val until: ObjectId = git.repository.resolve(untilRev)
 
         val baseLineDataTree = createBaselineAst(repository, since)
-
+        val changedFunction: MutableList<CodeFunction> = mutableListOf()
         for (commit in git.log().addRange(since, until).call()) {
             // todo: add increment for path changes
+            // first, we just got changed files.
+            getChangedFiles(repository, commit)
         }
+    }
+
+    private fun getChangedFiles(repository: Repository, revCommit: RevCommit) {
+        val diffFormatter = DiffFormatter(DisabledOutputStream.INSTANCE).config(repository)
+        diffFormatter.scan(getParent(revCommit)?.tree, revCommit.tree)
+            .map { d -> patchToDataStructs(d, repository, revCommit, diffFormatter) }
+    }
+
+    private fun patchToDataStructs(
+        diffEntry: DiffEntry,
+        repository: Repository,
+        revCommit: RevCommit,
+        df: DiffFormatter
+    ) {
+        try {
+            val treeWalk = TreeWalk.forPath(repository, diffEntry.newPath, revCommit.tree)
+            val pathString = treeWalk.pathString
+
+            val blobId = treeWalk.getObjectId(0)
+            val dataStructs = diffFileFromBlob(repository, blobId, pathString)
+
+            // todo: diff function changes
+
+        } catch (ex: Exception) {
+            throw ex
+        }
+    }
+
+    private fun DiffFormatter.config(repository: Repository): DiffFormatter {
+        setRepository(repository)
+        setDiffComparator(RawTextComparator.DEFAULT)
+        isDetectRenames = true
+        return this
+    }
+
+    private fun getParent(revCommit: RevCommit): RevCommit? {
+        return if (revCommit.parentCount == 0) {
+            null
+        } else
+            revCommit.getParent(0)
     }
 
     fun createBaselineAst(repository: Repository, since: ObjectId): List<DifferFile> {
@@ -54,24 +104,33 @@ class GitDiffer(val path: String, val branch: String, val systemId: String, val 
                 val blobId: ObjectId = tw.getObjectId(0)
 
                 if (pathString.endsWith(".kt")) {
-                    val content = repository.newObjectReader().use { objectReader ->
-                        val objectLoader: ObjectLoader = objectReader.open(blobId)
-                        val bytes: ByteArray = objectLoader.bytes
-                        val content = String(bytes, StandardCharsets.UTF_8)
-                        content
-                    }
+                    val dataStructs = diffFileFromBlob(repository, blobId, pathString)
 
-                    val file = AbstractFile(File(pathString).name, pathString, true, pathString, content)
-                    val dataStructs = KotlinAnalyserApp().analysisByFiles(arrayOf(file))
-
-                    files += DifferFile(path = pathString, dataStructs = dataStructs)
+                    differFileMap[path] = DifferFile(path = pathString, dataStructs = dataStructs)
                 }
             } catch (e: Exception) {
-
+                println(e)
             }
         }
 
         return files
+    }
+
+    private fun diffFileFromBlob(
+        repository: Repository,
+        blobId: ObjectId,
+        pathString: String
+    ): Array<CodeDataStruct> {
+        val content = repository.newObjectReader().use { objectReader ->
+            val objectLoader: ObjectLoader = objectReader.open(blobId)
+            val bytes: ByteArray = objectLoader.bytes
+            val content = String(bytes, StandardCharsets.UTF_8)
+            content
+        }
+
+        val file = AbstractFile(File(pathString).name, pathString, true, pathString, content)
+        val dataStructs = KotlinAnalyserApp().analysisByFiles(arrayOf(file))
+        return dataStructs
     }
 
     private fun Git.specifyBranch(branch: String): Git {
