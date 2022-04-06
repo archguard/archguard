43458eebcbfc431eb87c6ce908285d531072c676
Phodal Huang <h@phodal.com>
2022-04-06 09:28:31 +0800

    refactor: update test cases for java

--- diff_changes/src/test/kotlin/org/archguard/diff/changes/GitDifferTest.kt
+++ diff_changes/src/test/kotlin/org/archguard/diff/changes/GitDifferTest.kt
@@ -9,13 +9,13 @@ internal class GitDifferTest {
     fun should_get_range_for_local() {
         val differ = GitDiffer("..", "master", "", "")
         val diffList = differ
-            .countInRange("2ed0b8eb", "5fd08b56")
+            .countInRange("aa2b5379", "965be8c2")
 
         val functions = diffList.functions
 
         assertEquals(1, functions.size)
 
-        assertEquals("getKey", functions[0].functionName)
+        assertEquals("generateBatchInsertSql", functions[0].functionName)
 
         differ.generateProjectFunctionMap()
         differ.generateFunctionCallMap()
