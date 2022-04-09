30cc25302de6357448ef2c1da887169d04f76a9a
Phodal Huang <h@phodal.com>
2022-04-09 14:45:47 +0800

    fix: fix when over generalization empty issues

--- src/main/kotlin/com/thoughtworks/archguard/report/domain/redundancy/OverGeneralizationService.kt
+++ src/main/kotlin/com/thoughtworks/archguard/report/domain/redundancy/OverGeneralizationService.kt
@@ -10,6 +10,11 @@ class OverGeneralizationService(val overGeneralizationRepository: OverGeneraliza
     fun getOneExtendsWithTotalCount(systemId: Long, limit: Long, offset: Long): Pair<Long, List<OverGeneralizationPair>> {
         ValidPagingParam.validPagingParam(limit, offset)
         val parentClasses = overGeneralizationRepository.getOverGeneralizationParentClassId(systemId)
+        if (parentClasses.isEmpty()) {
+            val emptyList = listOf<OverGeneralizationPair>()
+            return (0L to emptyList)
+        }
+
         val pairList = overGeneralizationRepository.getOverGeneralizationPairList(parentClasses, limit, offset)
         return (parentClasses.size.toLong() to pairList)
     }
