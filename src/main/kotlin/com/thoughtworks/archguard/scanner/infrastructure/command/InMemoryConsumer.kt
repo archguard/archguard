1cc743e0b15d1a176dccf74dc7e33b7b0449065f
Phodal Huang <h@phodal.com>
2022-04-10 16:47:13 +0800

    docs: add faq link to error handle

--- src/main/kotlin/com/thoughtworks/archguard/scanner/infrastructure/command/InMemoryConsumer.kt
+++ src/main/kotlin/com/thoughtworks/archguard/scanner/infrastructure/command/InMemoryConsumer.kt
@@ -27,16 +27,16 @@ class InMemoryConsumer : StreamConsumer {
             when {
                 // try catch for catch error, like in: [https://dx.phodal.com/docs/factor/error-handling.html](https://dx.phodal.com/docs/factor/error-handling.html)
                 line.contains("Error: Unable to access jarfile ") -> {
-                    lines.add("下载 Scanner 可能出错，请尝试连接 VPN 下载")
+                    lines.add("下载 Scanner 可能出错，请尝试连接 VPN 下载。访问： https://archguard.org/docs/faq.html 了解更多")
                 }
                 line.contains("Invalid or corrupt jarfile") -> {
-                    lines.add("jar 包不完整，请尝试连接 VPN 下载")
+                    lines.add("jar 包不完整，请尝试连接 VPN 下载。访问： https://archguard.org/docs/faq.html 了解更多")
                 }
                 line.contains("Fail to clone source with exitCode 128") -> {
                     lines.add("Git Clone 出错，尝试根据: https://archguard.org/docs/faq.html#git 进行配置")
                 }
                 line.contains("Fail to identify build tool for compile") -> {
-                    lines.add("暂时不支持的构建命令，建议选择 Java/Kotlin，再重新扫描。")
+                    lines.add("暂时不支持的构建命令，建议选择 Java/Kotlin，再重新扫描。访问： https://archguard.org/docs/faq.html#jvm 了解更多")
                 }
             }
         } catch (e: RuntimeException) {
