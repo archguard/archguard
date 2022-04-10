377b7e7fe945184f78f2f953e83fecf832989930
Phodal Huang <h@phodal.com>
2022-04-10 22:21:32 +0800

    fix: fix typo

--- scan_sourcecode/src/main/kotlin/org/archguard/scanner/sourcecode/frontend/Naming.kt
+++ scan_sourcecode/src/main/kotlin/org/archguard/scanner/sourcecode/frontend/Naming.kt
@@ -1,9 +1,15 @@
 package org.archguard.scanner.sourcecode.frontend
 
+import chapi.app.frontend.path.OS
+import chapi.app.frontend.path.getOS
+
 fun naming(moduleName: String, nodeName: String): String {
+    var moduleName = moduleName
     if (nodeName == "default") {
         return moduleName
     }
+
+    if (getOS() == OS.WINDOWS) moduleName = moduleName.replace("\\", "/")
 
     return "${moduleName}::${nodeName}"
 }
