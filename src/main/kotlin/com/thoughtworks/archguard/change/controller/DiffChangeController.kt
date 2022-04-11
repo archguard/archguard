4d2e35c6df9c6e7ac547846fe21af2b05d0e268b
Phodal Huang <h@phodal.com>
2022-04-11 23:03:24 +0800

    refactor(diff): remove filte by time

--- src/main/kotlin/com/thoughtworks/archguard/change/controller/DiffChangeController.kt
+++ src/main/kotlin/com/thoughtworks/archguard/change/controller/DiffChangeController.kt
@@ -1,37 +1,16 @@
 package com.thoughtworks.archguard.change.controller
 
 import com.thoughtworks.archguard.change.application.DiffChangeService
-import com.thoughtworks.archguard.change.application.GitChangeService
 import com.thoughtworks.archguard.change.domain.DiffChange
 import com.thoughtworks.archguard.system_info.domain.SystemInfoService
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/systems/{systemId}/diff")
 class DiffChangeController(
-    val gitChangeService: GitChangeService,
     val diffChangeService: DiffChangeService,
     val systemInfoService: SystemInfoService,
 ) {
-
-    @GetMapping("/influence")
-    fun influence(
-        @PathVariable("systemId") systemId: Long,
-        @RequestParam(value = "startTime", required = true) startTime: String,
-        @RequestParam(value = "endTime", required = true) endTime: String,
-    ) {
-        val changes = gitChangeService.getChangesByRange(systemId, startTime, endTime)
-        if (changes.size < 2) {
-            throw ResponseStatusException(
-                HttpStatus.INTERNAL_SERVER_ERROR, "entity not found"
-            )
-        }
-        val systemInfo = systemInfoService.getSystemInfo(systemId)
-        diffChangeService.execute(systemInfo, changes.first(), changes.last())
-    }
-
     @GetMapping("/influence/history")
     fun historyInfluence(@PathVariable("systemId") systemId: Long, ): List<DiffChange> {
         return diffChangeService.findBySystemId(systemId)
