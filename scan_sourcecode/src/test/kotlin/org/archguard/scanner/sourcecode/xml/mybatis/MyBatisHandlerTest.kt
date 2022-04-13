02f8b04eeb5997d9a12daf19d96996ff80a67bb4
Phodal Huang <h@phodal.com>
2022-04-13 10:05:28 +0800

    feat(mybatis): use official test caes archguard/archguard#9

--- scan_sourcecode/src/test/kotlin/org/archguard/scanner/sourcecode/xml/mybatis/MyBatisHandlerTest.kt
+++ scan_sourcecode/src/test/kotlin/org/archguard/scanner/sourcecode/xml/mybatis/MyBatisHandlerTest.kt
@@ -7,10 +7,28 @@ import kotlin.test.assertEquals
 
 internal class MyBatisHandlerTest {
     @Test
-    internal fun dynamic_sql_source() {
+    internal fun support_for_foreach() {
         val resource = this.javaClass.classLoader.getResource("mybatis/OmsOrderOperateHistoryDao.xml")!!
         val toURI = resource.toURI().toPath().toAbsolutePath()
         val sqls = MyBatisHandler().streamToSqls(FileInputStream(toURI.toString()))
         assertEquals("INSERT INTO oms_order_operate_history (order_id, operate_man, create_time, order_status, note) VALUES (?, ?, ?, ?, ?)", sqls[0])
+    }
+
+    @Test
+    internal fun official_author_map_test_cases() {
+        val resource = this.javaClass.classLoader.getResource("mybatis/AuthorMapper.xml")!!
+        val toURI = resource.toURI().toPath().toAbsolutePath()
+        val sqls = MyBatisHandler().streamToSqls(FileInputStream(toURI.toString()))
+
+        assertEquals(15, sqls.size)
+    }
+
+    @Test
+    internal fun official_blog_map_test_cases() {
+        val resource = this.javaClass.classLoader.getResource("mybatis/BlogMapper.xml")!!
+        val toURI = resource.toURI().toPath().toAbsolutePath()
+        val sqls = MyBatisHandler().streamToSqls(FileInputStream(toURI.toString()))
+
+        assertEquals(6, sqls.size)
     }
 }
