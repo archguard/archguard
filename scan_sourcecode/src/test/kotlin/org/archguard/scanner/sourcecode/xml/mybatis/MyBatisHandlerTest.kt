32394cd84f720f6c4c6918e8d43ce654c3e0f2f0
Phodal Huang <h@phodal.com>
2022-04-13 08:45:08 +0800

    test(mybatis): sample for testing single parameter in # archguard/archguard#9

--- scan_sourcecode/src/test/kotlin/org/archguard/scanner/sourcecode/xml/mybatis/MyBatisHandlerTest.kt
+++ scan_sourcecode/src/test/kotlin/org/archguard/scanner/sourcecode/xml/mybatis/MyBatisHandlerTest.kt
@@ -1,10 +1,24 @@
 package org.archguard.scanner.sourcecode.xml.mybatis
 
 import org.apache.ibatis.builder.ParameterExpression
-import org.junit.jupiter.api.Assertions.*
+import org.apache.ibatis.parsing.GenericTokenParser
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class MyBatisHandlerTest {
+    @Test
+    internal fun should_parse_parameters() {
+        var key = ""
+        val parser = GenericTokenParser("#{", "}") { content: String ->
+            key = content
+            "#{$content}"
+        }
+
+        val parse = parser.parse("#{item.orderId}")
+
+        assertEquals("item.orderId", key)
+    }
+
     @Test
     internal fun should_handle_parameter_exp() {
         val parameterExpression = ParameterExpression("item.createTime,jdbcType=TIMESTAMP")
