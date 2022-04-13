5e063042c5e17491f6e3e61d467e1b54b721589e
Phodal Huang <h@phodal.com>
2022-04-13 09:56:19 +0800

    refactor(mybatis): extract get sql text  archguard/archguard#9

--- scan_sourcecode/src/main/kotlin/org/archguard/scanner/sourcecode/xml/mybatis/MyBatisHandler.kt
+++ scan_sourcecode/src/main/kotlin/org/archguard/scanner/sourcecode/xml/mybatis/MyBatisHandler.kt
@@ -55,26 +55,33 @@ class MyBatisHandler : BasedXmlHandler() {
     override fun compute(filePath: String): XmlConfig {
         val inputStream = FileInputStream(filePath)
 
+        streamToSqls(inputStream)
+
+        return this.config
+    }
+
+    fun streamToSqls(inputStream: FileInputStream): List<String> {
+        var sqls = listOf<String>()
         val config = Configuration()
         config.defaultResultSetType = ResultSetType.SCROLL_INSENSITIVE
         config.isShrinkWhitespacesInSql = true
 
-        val xPathParser = XPathParser(inputStream, true, config.variables, XMLMapperEntityResolver())
-        val context = xPathParser.evalNode("/mapper")
+        val parser = XPathParser(inputStream, true, config.variables, XMLMapperEntityResolver())
+        val context = parser.evalNode("/mapper")
         val list = context.evalNodes("select|insert|update|delete")
 
         list.forEach {
             val xmlScriptBuilder = XMLScriptBuilder(config, it)
             val sqlSource = xmlScriptBuilder.parseScriptNode()
 
-            // if is a foreacth
+            // if is a foreach
             val params = fakeParameters(it)
 
             val sqlString = sqlSource.getBoundSql(params).sql
-            println(sqlString)
+            sqls += sqlString
         }
 
-        return this.config
+        return sqls
     }
 
     private fun fakeParameters(node: XNode): MutableMap<String, Any> {
@@ -84,14 +91,7 @@ class MyBatisHandler : BasedXmlHandler() {
             val child = node.newXNode(children.item(i))
             if (child.node.nodeType == Node.CDATA_SECTION_NODE || child.node.nodeType == Node.TEXT_NODE) {
 //                val data = child.getStringBody("")
-//                val textSqlNode = TextSqlNode(data)
-//                if (textSqlNode.isDynamic) {
-//                    contents.add(textSqlNode)
-//                } else {
-//                    contents.add(StaticTextSqlNode(data))
-//                }
             } else if (child.node.nodeType == Node.ELEMENT_NODE) { // issue #628
-                // todo: parse text parameters from origin
 //                val data = child.getStringBody("")
                 when (child.node.nodeName) {
                     "trim" -> {}
--- scan_sourcecode/src/test/kotlin/org/archguard/scanner/sourcecode/xml/mybatis/MyBatisHandlerTest.kt
+++ scan_sourcecode/src/test/kotlin/org/archguard/scanner/sourcecode/xml/mybatis/MyBatisHandlerTest.kt
@@ -1,13 +1,16 @@
 package org.archguard.scanner.sourcecode.xml.mybatis
 
 import org.junit.jupiter.api.Test
+import java.io.FileInputStream
 import kotlin.io.path.toPath
+import kotlin.test.assertEquals
 
 internal class MyBatisHandlerTest {
     @Test
     internal fun dynamic_sql_source() {
         val resource = this.javaClass.classLoader.getResource("mybatis/OmsOrderOperateHistoryDao.xml")!!
         val toURI = resource.toURI().toPath().toAbsolutePath()
-        MyBatisHandler().compute(toURI.toString())
+        val sqls = MyBatisHandler().streamToSqls(FileInputStream(toURI.toString()))
+        assertEquals("INSERT INTO oms_order_operate_history (order_id, operate_man, create_time, order_status, note) VALUES (?, ?, ?, ?, ?)", sqls[0])
     }
 }
