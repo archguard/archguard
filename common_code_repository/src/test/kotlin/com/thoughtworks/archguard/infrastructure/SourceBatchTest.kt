a1aac3d2329d6af170dc3de81754bdbee1dcee2a
Phodal Huang <h@phodal.com>
2022-04-15 14:37:56 +0800

    test(common): add test for execute table

--- common_code_repository/src/test/kotlin/com/thoughtworks/archguard/infrastructure/SourceBatchTest.kt
+++ common_code_repository/src/test/kotlin/com/thoughtworks/archguard/infrastructure/SourceBatchTest.kt
@@ -3,6 +3,7 @@ package com.thoughtworks.archguard.infrastructure
 import nl.altindag.log.LogCaptor
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.io.File
 import kotlin.test.assertEquals
 
 internal class SourceBatchTest {
@@ -40,5 +41,22 @@ internal class SourceBatchTest {
         batch.execute()
 
         assertThat(logCaptor.debugLogs).containsExactly("INSERT INTO `code_method`(`a`,`clzname`,`b`,`id`)  VALUES   ('sourceId','clzName','targetId','123245')")
+    }
+
+    @Test
+    internal fun should_execute_by_table() {
+        val logCaptor: LogCaptor = LogCaptor.forClass(SourceBatch::class.java)
+
+        val batch = SourceBatch()
+        val callees: MutableMap<String, String> = HashMap()
+        callees["id"] = "123245"
+        batch.add("sample", callees)
+
+        batch.executeByTable("sample")
+
+        assertThat(logCaptor.debugLogs).containsExactly("INSERT INTO `sample`(`id`)  VALUES   ('123245')")
+
+        val text = File("sample.sql").readText()
+        assert(text.contains("123245"))
     }
 }
