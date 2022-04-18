69ce509df2baeb1439a3d5052b3735bdacfe09ab
Phodal Huang <h@phodal.com>
2022-04-18 09:00:50 +0800

    fix: fix typos

--- src/main/kotlin/com/thoughtworks/archguard/scanner/domain/scanner/javaext/checkstyle/CheckStyleTool.kt
+++ src/main/kotlin/com/thoughtworks/archguard/scanner/domain/scanner/javaext/checkstyle/CheckStyleTool.kt
@@ -5,6 +5,7 @@ import com.thoughtworks.archguard.scanner.domain.tools.StyleReport
 import java.io.File
 
 class CheckStyleTool(private val context: ScanContext) : StyleReport {
+    override fun getStyleReport(): List<File> {
         val configs = context.config.find { it.type == "checkstyle" }?.configs
         val reportFile = configs?.get("reportFile")?.split(',')
         return reportFile?.map { File(it) }?.filter { it.exists() } ?: emptyList()
