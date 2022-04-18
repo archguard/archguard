bf13422dec6abe41e79ca5fee12c142f2eadbbbe
Phodal Huang <h@phodal.com>
2022-04-18 09:00:15 +0800

    refactor: remove unused codes

--- src/main/kotlin/com/thoughtworks/archguard/scanner/domain/scanner/javaext/bs/BadSmellScanner.kt
+++ src/main/kotlin/com/thoughtworks/archguard/scanner/domain/scanner/javaext/bs/BadSmellScanner.kt
@@ -39,39 +39,4 @@ class BadSmellScanner(@Autowired val badSmellRepo: BadSmellRepo) : Scanner {
             )
         }
     }
-
-    data class CocaBadSmellModel(
-        val complexCondition: List<CocaBadSmellItem>?,
-        val dataClass: List<CocaBadSmellItem>?,
-        val graphConnectedCall: List<CocaBadSmellItem>?,
-        val largeClass: List<CocaBadSmellItem>?,
-        val lazyElement: List<CocaBadSmellItem>?,
-        val longMethod: List<CocaBadSmellItem>?,
-        val longParameterList: List<CocaBadSmellItem>?,
-        val repeatedSwitches: List<CocaBadSmellItem>?
-    ) {
-        fun toBadSmell(systemId: Long): List<BadSmell> {
-            return listOf(
-                complexCondition?.map { c -> BadSmell(UUID.randomUUID().toString(), systemId, c.EntityName, c.Line, c.Description, c.Size, "complexCondition") }
-                    ?: emptyList(),
-                dataClass?.map { c -> BadSmell(UUID.randomUUID().toString(), systemId, c.EntityName, c.Line, c.Description, c.Size, "dataClass") }
-                    ?: emptyList(),
-                graphConnectedCall?.map { c -> BadSmell(UUID.randomUUID().toString(), systemId, c.EntityName, c.Line, c.Description, c.Size, "graphConnectedCall") }
-                    ?: emptyList(),
-                largeClass?.map { c -> BadSmell(UUID.randomUUID().toString(), systemId, c.EntityName, c.Line, c.Description, c.Size, "largeClass") }
-                    ?: emptyList(),
-                lazyElement?.map { c -> BadSmell(UUID.randomUUID().toString(), systemId, c.EntityName, c.Line, c.Description, c.Size, "lazyElement") }
-                    ?: emptyList(),
-                longMethod?.map { c -> BadSmell(UUID.randomUUID().toString(), systemId, c.EntityName, c.Line, c.Description, c.Size, "longMethod") }
-                    ?: emptyList(),
-                longParameterList?.map { c -> BadSmell(UUID.randomUUID().toString(), systemId, c.EntityName, c.Line, c.Description, c.Size, "longParameterList") }
-                    ?: emptyList(),
-                repeatedSwitches?.map { c -> BadSmell(UUID.randomUUID().toString(), systemId, c.EntityName, c.Line, c.Description, c.Size, "repeatedSwitches") }
-                    ?: emptyList()
-            ).flatten()
-        }
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    data class CocaBadSmellItem(val EntityName: String, val Line: Int, val Description: String?, val Size: Int)
 }
--- src/main/kotlin/com/thoughtworks/archguard/scanner/domain/scanner/javaext/checkstyle/CheckStyleTool.kt
+++ src/main/kotlin/com/thoughtworks/archguard/scanner/domain/scanner/javaext/checkstyle/CheckStyleTool.kt
@@ -5,8 +5,6 @@ import com.thoughtworks.archguard.scanner.domain.tools.StyleReport
 import java.io.File
 
 class CheckStyleTool(private val context: ScanContext) : StyleReport {
-
-    override fun getStyleReport(): List<File> {
         val configs = context.config.find { it.type == "checkstyle" }?.configs
         val reportFile = configs?.get("reportFile")?.split(',')
         return reportFile?.map { File(it) }?.filter { it.exists() } ?: emptyList()
