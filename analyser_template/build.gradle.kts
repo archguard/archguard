a6bc90d988aa36fc1b481cff4c40396fbaf56eda
Andong Liao <adliao@thoughtworks.com>
2022-04-22 14:31:19 +0800

    feat(scan): add shadow package for template as well

--- analyser_template/build.gradle.kts
+++ analyser_template/build.gradle.kts
@@ -1,6 +1,10 @@
+import groovy.xml.dom.DOMCategory.attributes
+
 plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+
 }
 
 dependencies {
@@ -8,4 +12,17 @@ dependencies {
 
     testImplementation("io.mockk:mockk:1.12.3")
     testImplementation("org.assertj:assertj-core:3.22.0")
+}
+
+tasks {
+    shadowJar {
+        manifest {
+            attributes(Pair("Main-Class", "org.archguard.scanner.core.AnalyserKt"))
+        }
+        dependencies {
+            exclude(dependency("org.jetbrains.kotlin:.*:.*"))
+            exclude(dependency("org.jetbrains.kotlinx:.*:.*"))
+        }
+        minimize()
+    }
 }
