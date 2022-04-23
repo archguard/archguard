c4a898a3c0f66c758f48f25e777bd9ea1f9b662e
Phodal Huang <h@phodal.com>
2022-04-23 11:55:00 +0800

    chore: release 1.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
     apply(plugin = "jacoco")
 
     group = "com.thoughtworks.archguard"
-    version = "1.6.1"
+    version = "1.6.2"
     java.sourceCompatibility = JavaVersion.VERSION_1_8
     java.targetCompatibility = JavaVersion.VERSION_1_8
 
