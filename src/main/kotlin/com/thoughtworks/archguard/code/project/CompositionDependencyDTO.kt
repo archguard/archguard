dc2a38c287a503282e7e87f8d445a6f24eeaa9c8
Phodal Huang <h@phodal.com>
2022-04-23 10:39:29 +0800

    feat(sca): fix dto jdbi issue

--- src/main/kotlin/com/thoughtworks/archguard/code/project/CompositionDependencyDTO.kt
+++ src/main/kotlin/com/thoughtworks/archguard/code/project/CompositionDependencyDTO.kt
@@ -1,16 +1,48 @@
 package com.thoughtworks.archguard.code.project
 
-data class CompositionDependencyDTO(
-    val id: String = "",
-    val systemId: String = "",
-    val name: String = "",
-    val version: String = "",
-    val parentId: String = "",
-    val packageManager: String = "",
-    val depName: String = "",
-    val depGroup: String = "",
-    val depArtifact: String = "",
-    val depMetadata: String = "",
-    val depSource: String = "",
-    val depScope: String = ""
-)
+import org.jdbi.v3.core.mapper.reflect.JdbiConstructor
+
+class CompositionDependencyDTO {
+    val id: String
+    val systemId: String
+    val name: String
+    val version: String
+    val parentId: String
+    val packageManager: String
+    val depName: String
+    val depGroup: String
+    val depArtifact: String
+    val depMetadata: String
+    val depSource: String
+    val depScope: String
+
+    // JDBI issue?
+    @JdbiConstructor
+    constructor(
+        id: String = "",
+        systemId: String = "",
+        name: String = "",
+        version: String = "",
+        parentId: String = "",
+        packageManager: String = "",
+        depName: String = "",
+        depGroup: String = "",
+        depArtifact: String = "",
+        depMetadata: String = "",
+        depSource: String = "",
+        depScope: String = ""
+    ) {
+        this.id = id
+        this.systemId = systemId
+        this.name = name
+        this.version = version
+        this.parentId = parentId
+        this.packageManager = packageManager
+        this.depName = depName
+        this.depGroup = depGroup
+        this.depArtifact = depArtifact
+        this.depMetadata = depMetadata
+        this.depSource = depSource
+        this.depScope = depScope
+    }
+}
--- src/main/kotlin/com/thoughtworks/archguard/code/project/ProjectRepositoryImpl.kt
+++ src/main/kotlin/com/thoughtworks/archguard/code/project/ProjectRepositoryImpl.kt
@@ -17,7 +17,7 @@ class ProjectRepositoryImpl : ProjectRepository {
             it.createQuery(
                 "select id, name, version, system_id as systemId, parent_id as parentId, " +
                     "package_manager as packageManager, dep_name as depName, dep_artifact as depArtifact, " +
-                    "dep_group as depGroup, dep_metadata as depMetadata, dep_scope as depScope, dep_source as depSource, " +
+                    "dep_group as depGroup, dep_metadata as depMetadata, dep_scope as depScope, dep_source as depSource  " +
                     "from project_composition_dependencies where system_id=$systemId"
             )
                 .mapTo(CompositionDependencyDTO::class.java)
