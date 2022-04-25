d76d6743139355629cf31016e1e7bfed5d8c4004
Phodal Huang <h@phodal.com>
2022-04-25 21:14:25 +0800

    feat(docs): init api to align dokka compiler api

--- doc_generator/src/main/kotlin/org/archguard/doc/generator/compiler/KotlinAnalysis.kt
+++ doc_generator/src/main/kotlin/org/archguard/doc/generator/compiler/KotlinAnalysis.kt
@@ -4,6 +4,7 @@ import com.intellij.core.CoreApplicationEnvironment
 import com.intellij.mock.MockComponentManager
 import com.intellij.openapi.extensions.Extensions
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vfs.StandardFileSystems
 import com.intellij.psi.PsiNameHelper
 import com.intellij.psi.impl.PsiNameHelperImpl
 import com.intellij.psi.impl.source.javadoc.JavadocManagerImpl
@@ -11,44 +12,73 @@ import com.intellij.psi.javadoc.CustomJavadocTagProvider
 import com.intellij.psi.javadoc.JavadocManager
 import com.intellij.psi.javadoc.JavadocTagInfo
 import com.intellij.psi.search.GlobalSearchScope
-import org.jetbrains.kotlin.analyzer.ResolverForSingleModuleProject
-import org.jetbrains.kotlin.analyzer.common.CommonAnalysisParameters
-import org.jetbrains.kotlin.analyzer.common.CommonResolverForModuleFactory
+import org.jetbrains.kotlin.analyzer.AbstractResolverForProject
+import org.jetbrains.kotlin.analyzer.LibraryModuleInfo
+import org.jetbrains.kotlin.analyzer.ModuleContent
+import org.jetbrains.kotlin.analyzer.ModuleInfo
+import org.jetbrains.kotlin.analyzer.ResolverForModule
+import org.jetbrains.kotlin.analyzer.ResolverForProject
+import org.jetbrains.kotlin.analyzer.common.CommonPlatformAnalyzerServices
+import org.jetbrains.kotlin.builtins.KotlinBuiltIns
+import org.jetbrains.kotlin.builtins.jvm.JvmBuiltIns
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.cli.common.config.addKotlinSourceRoot
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
+import org.jetbrains.kotlin.cli.jvm.compiler.JvmPackagePartProvider
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.cli.jvm.compiler.TopDownAnalyzerFacadeForJVM
 import org.jetbrains.kotlin.cli.jvm.config.addJavaSourceRoot
 import org.jetbrains.kotlin.cli.jvm.config.addJvmClasspathRoots
+import org.jetbrains.kotlin.cli.jvm.config.jvmClasspathRoots
+import org.jetbrains.kotlin.cli.jvm.index.JavaRoot
+import org.jetbrains.kotlin.config.ApiVersion
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.config.LanguageFeature
-import org.jetbrains.kotlin.config.LanguageVersionSettings
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.config.LanguageVersionSettingsImpl
 import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.container.ComponentProvider
 import org.jetbrains.kotlin.container.tryGetService
 import org.jetbrains.kotlin.context.ProjectContext
+import org.jetbrains.kotlin.context.withModule
+import org.jetbrains.kotlin.descriptors.ModuleDescriptor
+import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
+import org.jetbrains.kotlin.library.KotlinLibrary
+import org.jetbrains.kotlin.load.java.structure.impl.JavaClassImpl
+import org.jetbrains.kotlin.load.java.structure.impl.classFiles.BinaryJavaClass
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.platform.CommonPlatforms
+import org.jetbrains.kotlin.platform.TargetPlatform
+import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
+import org.jetbrains.kotlin.resolve.CliSealedClassInheritorsProvider
 import org.jetbrains.kotlin.resolve.CompilerEnvironment
 import org.jetbrains.kotlin.resolve.LazyTopDownAnalyzer
+import org.jetbrains.kotlin.resolve.PlatformDependentAnalyzerServices
 import org.jetbrains.kotlin.resolve.TopDownAnalysisContext
 import org.jetbrains.kotlin.resolve.TopDownAnalysisMode
+import org.jetbrains.kotlin.resolve.jvm.JvmPlatformParameters
+import org.jetbrains.kotlin.resolve.jvm.JvmResolverForModuleFactory
 import org.jetbrains.kotlin.utils.PathUtil
 import org.slf4j.LoggerFactory
 import java.io.File
 
 class KotlinAnalysis {
     private val logger = LoggerFactory.getLogger(javaClass)
-    private val classPath: ArrayList<File> = run {
-        val classpath = arrayListOf<File>()
-        PathUtil.getResourcePathForClass(AnnotationTarget.CLASS.javaClass)
-        classpath
-    }
+    private var messageCollector: DocMessageCollector
+//    private val classPath: ArrayList<File> = run {
+//        val classpath = arrayListOf<File>()
+//        PathUtil.getResourcePathForClass(AnnotationTarget.CLASS.javaClass)
+//        classpath
+//    }
+
+
+    /**
+     * Classpath for this environment.
+     */
+    val classPath: List<File> get() = configuration.jvmClasspathRoots
 
     private val configuration = CompilerConfiguration()
 
     init {
-        val messageCollector = DocMessageCollector(logger)
+        this.messageCollector = DocMessageCollector(logger)
         configuration.put(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, messageCollector)
     }
 
@@ -73,7 +103,7 @@ class KotlinAnalysis {
         configuration.addJvmClasspathRoots(classPath)
 
         // todo: loadLanguageVersionSettings
-
+        loadLanguageVersionSettings()
 
         val environment = createCoreEnvironment()
         val container = createSingleModuleResolver(environment)
@@ -82,6 +112,15 @@ class KotlinAnalysis {
         val analysisContext = lazyTopDownAnalyzer.analyzeDeclarations(TopDownAnalysisMode.TopLevelDeclarations,
             environment.getSourceFiles())
         return analysisContext
+    }
+
+    private fun loadLanguageVersionSettings() {
+        val languageVersion = LanguageVersion.LATEST_STABLE
+        val apiVersion = ApiVersion.createByLanguageVersion(languageVersion)
+        configuration.languageVersionSettings = LanguageVersionSettingsImpl(
+            languageVersion = languageVersion,
+            apiVersion = apiVersion
+        )
     }
 
     private fun createCoreEnvironment(): KotlinCoreEnvironment {
@@ -121,39 +160,135 @@ class KotlinAnalysis {
 
 
     private fun createSingleModuleResolver(environment: KotlinCoreEnvironment): ComponentProvider {
-//        val projectContext = ProjectContext(environment.project, "Doc")
+        val projectContext = ProjectContext(environment.project, "Doc")
         val sourceFiles = environment.getSourceFiles()
 
-        val moduleInfo = SourceModuleInfo(Name.special("<main"), mapOf(), false)
-        val project = sourceFiles.firstOrNull()?.project ?: throw AssertionError("No files to analyze")
+        val targetPlatform = JvmPlatforms.defaultJvmPlatform
 
-        val multiplatformLanguageSettings = object : LanguageVersionSettings by configuration.languageVersionSettings {
-            override fun getFeatureSupport(feature: LanguageFeature): LanguageFeature.State =
-                if (feature == LanguageFeature.MultiPlatformProjects) LanguageFeature.State.ENABLED
-                else configuration.languageVersionSettings.getFeatureSupport(feature)
+        val kotlinLibraries: Map<String, KotlinLibrary> = emptyMap()
+        val extraModuleDependencies: Map<String, KotlinLibrary> = emptyMap()
+
+        val library = object : LibraryModuleInfo {
+            override val analyzerServices: PlatformDependentAnalyzerServices = CommonPlatformAnalyzerServices
+            override val name: Name = Name.special("<library>")
+            override val platform: TargetPlatform = targetPlatform
+            override fun dependencies(): List<ModuleInfo> = listOf(this)
+            override fun getLibraryRoots(): Collection<String> = classPath
+                .map { libraryFile -> libraryFile.absolutePath }
+                .filter { path -> path !in kotlinLibraries }
         }
 
-        val resolverForModuleFactory = CommonResolverForModuleFactory(
-            CommonAnalysisParameters { content ->
-                environment.createPackagePartProvider(content.moduleContentScope)
-            },
-            CompilerEnvironment,
-            CommonPlatforms.defaultCommonPlatform,
-            shouldCheckExpectActual = false
-        )
 
-        val resolver = ResolverForSingleModuleProject(
-            "sources for metadata serializer",
-            ProjectContext(project, "metadata serializer"),
+//        val moduleInfo = SourceModuleInfo(Name.special("<main"), mapOf(), false)
+
+        val moduleInfo = object : ModuleInfo {
+            override val analyzerServices: PlatformDependentAnalyzerServices = CommonPlatformAnalyzerServices
+            override val name: Name = Name.special("<module>")
+            override val platform: TargetPlatform = targetPlatform
+            override fun dependencies(): List<ModuleInfo> = listOf(this, library)
+        }
+
+        val sourcesScope = TopDownAnalyzerFacadeForJVM.newModuleSearchScope(environment.project, sourceFiles)
+
+        val modulesContent: (ModuleInfo) -> ModuleContent<ModuleInfo> = {
+            when (it) {
+                library -> ModuleContent(it, emptyList(), GlobalSearchScope.notScope(sourcesScope))
+                moduleInfo -> ModuleContent(it, emptyList(), GlobalSearchScope.allScope(environment.project))
+//                is CommonKlibModuleInfo -> ModuleContent(it, emptyList(), GlobalSearchScope.notScope(sourcesScope))
+                else -> null
+            } ?: throw IllegalArgumentException("Unexpected module info")
+        }
+
+        var builtIns: JvmBuiltIns? = null
+
+        builtIns = JvmBuiltIns(
+            projectContext.storageManager,
+            JvmBuiltIns.Kind.FROM_CLASS_LOADER
+        ) // TODO we should use FROM_DEPENDENCIES
+        val resolverForProject = createJvmResolverForProject(
+            projectContext,
             moduleInfo,
-            resolverForModuleFactory,
-            GlobalSearchScope.allScope(project),
-            languageVersionSettings = multiplatformLanguageSettings,
-            syntheticFiles = sourceFiles
+            library,
+            modulesContent,
+            sourcesScope,
+            builtIns
         )
 
-        val container = resolver.resolverForModule(moduleInfo).componentProvider
+//        val libraryModuleDescriptor = resolverForProject.descriptorForModule(library)
+        val moduleDescriptor = resolverForProject.descriptorForModule(moduleInfo)
+        builtIns.initialize(moduleDescriptor, true)
 
-        return container
+//        val resolverForLibrary =
+//            resolverForProject.resolverForModule(library) // Required before module to initialize library properly
+        val resolverForModule = resolverForProject.resolverForModule(moduleInfo)
+//        val resolverForModule = resolver.resolverForModule(moduleInfo)
+        return resolverForModule.componentProvider
+    }
+
+    private fun createJvmResolverForProject(
+        projectContext: ProjectContext,
+        module: ModuleInfo,
+        library: LibraryModuleInfo,
+        modulesContent: (ModuleInfo) -> ModuleContent<ModuleInfo>,
+        sourcesScope: GlobalSearchScope,
+        builtIns: KotlinBuiltIns,
+    ): ResolverForProject<ModuleInfo> {
+        val javaRoots = classPath
+            .mapNotNull { file ->
+                val rootFile = when (file.extension) {
+                    "jar" -> StandardFileSystems.jar().findFileByPath("${file.absolutePath}${Companion.JAR_SEPARATOR}")
+                    else -> StandardFileSystems.local().findFileByPath(file.absolutePath)
+                }
+                rootFile?.let { JavaRoot(it, JavaRoot.RootType.BINARY) }
+            }
+
+        return object : AbstractResolverForProject<ModuleInfo>("Doc", projectContext, modules = listOf(module, library)) {
+            override fun modulesContent(module: ModuleInfo): ModuleContent<ModuleInfo> =
+                when (module) {
+                    library -> ModuleContent(module, emptyList(), GlobalSearchScope.notScope(sourcesScope))
+                    module -> ModuleContent(module, emptyList(), sourcesScope)
+                    else -> throw IllegalArgumentException("Unexpected module info")
+                }
+
+            override fun builtInsForModule(module: ModuleInfo): KotlinBuiltIns = builtIns
+
+            override fun createResolverForModule(
+                descriptor: ModuleDescriptor,
+                moduleInfo: ModuleInfo,
+            ): ResolverForModule = JvmResolverForModuleFactory(
+                JvmPlatformParameters(packagePartProviderFactory = { content ->
+                    JvmPackagePartProvider(
+                        configuration.languageVersionSettings,
+                        content.moduleContentScope
+                    )
+                        .apply {
+                            addRoots(javaRoots, messageCollector)
+                        }
+                }, moduleByJavaClass = {
+                    val file =
+                        (it as? BinaryJavaClass)?.virtualFile ?: (it as JavaClassImpl).psi.containingFile.virtualFile
+                    if (file in sourcesScope)
+                        module
+                    else
+                        library
+                }, resolverForReferencedModule = null,
+                    useBuiltinsProviderForModule = { false }),
+                CompilerEnvironment,
+                JvmPlatforms.unspecifiedJvmPlatform
+            ).createResolverForModule(
+                descriptor as ModuleDescriptorImpl,
+                projectContext.withModule(descriptor),
+                modulesContent(moduleInfo),
+                this,
+                configuration.languageVersionSettings,
+                CliSealedClassInheritorsProvider,
+            )
+
+            override fun sdkDependency(module: ModuleInfo): ModuleInfo? = null
+        }
+    }
+
+    companion object {
+        const val JAR_SEPARATOR = "!/"
     }
 }
--- doc_generator/src/main/kotlin/org/archguard/doc/generator/compiler/SourceModuleInfo.kt
+++ doc_generator/src/main/kotlin/org/archguard/doc/generator/compiler/SourceModuleInfo.kt
@@ -7,12 +7,13 @@ import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.platform.CommonPlatforms
 import org.jetbrains.kotlin.platform.TargetPlatform
 import org.jetbrains.kotlin.resolve.PlatformDependentAnalyzerServices
+import org.jetbrains.kotlin.resolve.jvm.platform.JvmPlatformAnalyzerServices
 
 class SourceModuleInfo(
     override val name: Name,
     override val capabilities: Map<ModuleCapability<*>, Any?>,
     private val dependOnOldBuiltIns: Boolean,
-    override val analyzerServices: PlatformDependentAnalyzerServices = CommonPlatformAnalyzerServices,
+    override val analyzerServices: PlatformDependentAnalyzerServices = JvmPlatformAnalyzerServices,
     override val platform: TargetPlatform = CommonPlatforms.defaultCommonPlatform,
 ) : ModuleInfo {
     override fun dependencies() = listOf(this)
