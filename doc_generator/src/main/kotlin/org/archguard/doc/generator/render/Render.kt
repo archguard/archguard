0f4e9d1ae14e0f3441aa1b2cd80ffe08eb967875
Phodal Huang <h@phodal.com>
2022-04-26 15:11:00 +0800

    feat(docs): init basic fm

--- doc_generator/src/main/kotlin/org/archguard/doc/generator/render/Render.kt
+++ doc_generator/src/main/kotlin/org/archguard/doc/generator/render/Render.kt
@@ -13,3 +13,25 @@ data class DocHeader(val title: String, val content: List<ContentNode>, val leve
 data class DocText(val text: String) : ContentNode
 data class DocBreakLine(val text: String = "") : ContentNode
 data class DocCodeBlock(val language: String, val text: String) : ContentNode
+
+data class CustomJekyllFrontMatter(
+    val layout: String = "default",
+    var title: String,
+    val parent: String = "Governance",
+    val navOrder: Int,
+    val permalink: String,
+) : ContentNode {
+
+    fun toMarkdown(): String {
+        return """
+---
+layout: $layout
+title: $title
+parent: $parent
+nav_order: $navOrder
+permalink: /governance/$permalink
+---
+
+""".trimIndent()
+    }
+}
