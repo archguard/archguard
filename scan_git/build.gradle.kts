a13e7684fdec0857b5b2d55d06ff10968335d91a
Phodal Huang <h@phodal.com>
2022-04-28 20:33:09 +0800

    chore: clean unused libs

--- scan_git/build.gradle.kts
+++ scan_git/build.gradle.kts
@@ -9,12 +9,9 @@ plugins {
 dependencies {
     implementation("com.github.ajalt.clikt:clikt:3.4.0")
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
-
     implementation("org.eclipse.jgit:org.eclipse.jgit:6.0.0.202111291000-r")
 
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 }
 
