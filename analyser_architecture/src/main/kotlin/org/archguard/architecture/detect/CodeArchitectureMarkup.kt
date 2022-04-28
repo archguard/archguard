7af7ff5498cca3b0e204d45928dfccaff638f7e3
Phodal Huang <h@phodal.com>
2022-04-28 17:20:48 +0800

    feat(arch): make architecture extends api works

--- analyser_architecture/src/main/kotlin/org/archguard/architecture/detect/CodeArchitectureMarkup.kt
+++ analyser_architecture/src/main/kotlin/org/archguard/architecture/detect/CodeArchitectureMarkup.kt
@@ -5,6 +5,8 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
+typealias Language = String
+
 @Serializable
 class CodeArchitectureMarkup(
     val language: String,
@@ -17,7 +19,30 @@ class CodeArchitectureMarkup(
     companion object {
         fun fromResource(): Array<CodeArchitectureMarkup> {
             val fileContent = this.javaClass.classLoader.getResource("frameworks.json").readText()
-            return Json.decodeFromString(fileContent)
+            val markups = Json.decodeFromString<Array<CodeArchitectureMarkup>>(fileContent)
+
+            val needExtends: MutableMap<Language, Language> = mutableMapOf()
+
+            val markupMap: MutableMap<String, CodeArchitectureMarkup> = mutableMapOf()
+            markups.forEach {
+                if (it.extends.isNotEmpty()) {
+                    needExtends[it.language] = it.extends
+                }
+
+                markupMap[it.language] = it
+            }
+
+            if (needExtends.isNotEmpty()) {
+                needExtends.forEach {
+                    if (markupMap[it.key] != null && markupMap[it.value] != null) {
+                        markupMap[it.key]!!.appTypes += markupMap[it.value]!!.appTypes
+                        markupMap[it.key]!!.protocolMapping += markupMap[it.value]!!.protocolMapping
+                    }
+                }
+            }
+
+
+            return markupMap.map { it.value }.toTypedArray()
         }
     }
 }
--- analyser_architecture/src/test/kotlin/org/archguard/architecture/detect/CodeArchitectureMarkupTest.kt
+++ analyser_architecture/src/test/kotlin/org/archguard/architecture/detect/CodeArchitectureMarkupTest.kt
@@ -5,8 +5,15 @@ import kotlin.test.assertEquals
 
 internal class CodeArchitectureMarkupTest {
     @Test
-    internal fun serial() {
+    internal fun serial_json() {
         val markups = CodeArchitectureMarkup.fromResource()
         assertEquals("Java", markups[0].language)
+    }
+
+    @Test
+    internal fun extend_api_for_kotlin_extend_java() {
+        val markups = CodeArchitectureMarkup.fromResource()
+        assertEquals("Kotlin", markups[1].language)
+        assert(markups[1].appTypes.isNotEmpty())
     }
 }
