19ae576a295af0da6cbbcd042c1bc187a8554ed1
Phodal Huang <h@phodal.com>
2022-05-02 16:01:16 +0800

    docs: add archview

---
+++ analyser_architecture/src/main/kotlin/org/archguard/architecture/core/ArchitectureView.kt
@@ -0,0 +1,16 @@
+package org.archguard.architecture.core
+
+class ArchitectureView(
+    val codeArchitecture: CodeArchitecture,
+    val moduleArchitecture: ModuleArchitecture,
+    val executionArchitecture: ExecutionArchitecture,
+    val conceptArchitecture: ConceptArchitecture
+)
+
+class CodeArchitecture()
+
+class ModuleArchitecture()
+
+class ExecutionArchitecture()
+
+class ConceptArchitecture()
