b878789167e6b79085d4e162faea8856b45d7455
Phodal Huang <h@phodal.com>
2022-05-06 23:52:07 +0800

    refactor(rule): add basic execute for rule dispatch

--- scanner_cli/src/main/kotlin/org/archguard/scanner/ctl/loader/rule/RuleDispatcher.kt
+++ scanner_cli/src/main/kotlin/org/archguard/scanner/ctl/loader/rule/RuleDispatcher.kt
@@ -1,14 +1,20 @@
 package org.archguard.scanner.ctl.loader.rule
 
+import org.archguard.rule.core.Issue
+import org.archguard.rule.core.RuleSetProvider
+import org.archguard.rule.core.RuleVisitor
 import org.archguard.scanner.ctl.command.ScannerCommand
 
 /**
  * **RuleDispatcher** is to load rules
  */
-class RuleDispatcher {
+object RuleDispatcher {
     fun dispatch(command: ScannerCommand) {
-        command.getAnalyserSpecs().forEach {
 
-        }
+    }
+
+    // todo: add disable rule checks
+    fun executeOne(ruleVisitor: RuleVisitor, ruleSetProvider: RuleSetProvider): List<Issue> {
+        return ruleVisitor.visitor(listOf(ruleSetProvider.get()))
     }
 }
