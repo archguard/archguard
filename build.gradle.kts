e07a8c722a665a64667b83686d3fed0f207e9546
Phodal Huang <h@phodal.com>
2022-05-10 10:36:22 +0800

    chore: update version for tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ allprojects {
     apply(plugin = "org.jetbrains.dokka")
 
     group = "org.archguard.scanner"
-    version = "1.7.0"
+    version = "2.0.0-alpha.1"
     java.sourceCompatibility = JavaVersion.VERSION_1_8
     java.targetCompatibility = JavaVersion.VERSION_1_8
 
