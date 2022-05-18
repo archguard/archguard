7fd3f443cdb7fe3ce4d40029791e20b6c8379d9d
Phodal Huang <h@phodal.com>
2022-05-18 17:29:04 +0800

    feat(archdoc): add auth info

--- archdoc/doc-executor/src/main/kotlin/org/archguard/dsl/client/RepoClient.kt
+++ archdoc/doc-executor/src/main/kotlin/org/archguard/dsl/client/RepoClient.kt
@@ -3,5 +3,6 @@ package org.archguard.dsl.client
 import org.archguard.dsl.model.Repository
 
 interface RepoClient {
+    fun saveRepos(repo: Iterable<Repository>)
     fun save(repo: Repository)
 }
--- archdoc/doc-executor/src/main/kotlin/org/archguard/dsl/model/Repository.kt
+++ archdoc/doc-executor/src/main/kotlin/org/archguard/dsl/model/Repository.kt
@@ -5,8 +5,13 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.archguard.dsl.Element
 
+// todo: align to backend API
+
 @Serializable
-class Repository(val name: String, val language: String, val scmUrl: String) : Element {
+class RepositoryAuth (val name: String, val password: String) {}
+
+@Serializable
+class Repository(val name: String, val language: String, val scmUrl: String) {
     override fun toString(): String {
         return Json.encodeToString(this)
     }
