79dad075ccb0f778ed9648e8e430d55e45bd09e8
Phodal Huang <h@phodal.com>
2022-05-30 20:00:19 +0800

    chore: release 2.0.0-alpha.15 :smiling_face_with_tear::smiling_face_with_tear::smiling_face_with_tear::smiling_face_with_tear::smiling_face_with_tear::smiling_face_with_tear::smiling_face_with_tear:

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ allprojects {
     apply(plugin = "org.jetbrains.dokka")
 
     group = "org.archguard.scanner"
-    version = "2.0.0-alpha.14"
+    version = "2.0.0-alpha.15"
     java.sourceCompatibility = JavaVersion.VERSION_1_8
     java.targetCompatibility = JavaVersion.VERSION_1_8
 
