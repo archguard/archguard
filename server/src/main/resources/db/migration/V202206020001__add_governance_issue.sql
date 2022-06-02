a6a7d0627eac46efd71c2a00bd5f4c54ecf6e7b2
Phodal Huang <h@phodal.com>
2022-06-02 11:15:04 +0800

    feat(rule): init governance issue table

---
+++ server/src/main/resources/db/migration/V202206020001__add_governance_issue.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `governance_issue`
+(
+    `id`         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `system_id`  BIGINT        NOT NULL,
+    `position`   VARCHAR(1024) NOT NULL DEFAULT '',
+    `rule_id`    VARCHAR(1024) NOT NULL,
+    `name`       VARCHAR(1024) NOT NULL DEFAULT '',
+    `detail`     MEDIUMTEXT    NOT NULL DEFAULT '',
+    `rule_type`  VARCHAR(1024) NOT NULL DEFAULT '',
+    `severity`   VARCHAR(1024) NOT NULL DEFAULT '',
+    `fullName`   VARCHAR(1024) NOT NULL DEFAULT '',
+    `source`     VARCHAR(1024) NOT NULL DEFAULT '',
+    `created_at` TIMESTAMP(3)  NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `publish_at` TIMESTAMP(3)  NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
+);
