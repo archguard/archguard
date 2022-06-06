6aff3ac4ea597a229e7839902c62c6a62bc8afc4
Phodal Huang <h@phodal.com>
2022-06-06 14:53:47 +0800

    refactor: simplify query logic for performancE

--- server/src/main/kotlin/com/thoughtworks/archguard/report/infrastructure/redundancy/RedundancyRepositoryImpl.kt
+++ server/src/main/kotlin/com/thoughtworks/archguard/report/infrastructure/redundancy/RedundancyRepositoryImpl.kt
@@ -9,13 +9,12 @@ import org.springframework.stereotype.Repository
 @Repository
 class RedundancyRepositoryImpl(val jdbi: Jdbi) : RedundancyRepository {
     private val table = "select sum(1) sum, m.clzname as name, m.module as module " +
-        "from code_method m, code_class c, method_access ma, code_class_access ca " +
-        "where ca.class_id=c.id  and ca.is_interface = false and ca.is_abstract = false " +
-        "and ma.method_id = m.id and m.name not like '%$%'" +
+        "from code_method m, code_class c " +
+        "where m.name not like '%$%'" +
         "and m.name != '<clinit>' and m.name != 'main' and m.name != '<init>'" +
         "and m.name != 'toString' and m.name != 'equals' and m.name != 'hashCode' and m.name != 'clone'" +
         "and m.clzname = c.name and c.module=m.module and c.is_thirdparty = false and c.is_test != true " +
-        "and m.system_id=:system_id and c.system_id=:system_id and ma.system_id=:system_id " +
+        "and m.system_id=:system_id and c.system_id=:system_id " +
         "group by m.clzname, m.module having sum = 1"
 
     override fun getOneMethodClassCount(systemId: Long): Long {
