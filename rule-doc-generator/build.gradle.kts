4f025a3f7c1baea3be89d7edb327192e085b2cec
Phodal Huang <h@phodal.com>
2022-06-07 16:22:38 +0800

    fix: fix rename issue

--- rule-doc-generator/build.gradle.kts
+++ rule-doc-generator/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     api(project(":rule-core"))
     api(project(":rule-linter:rule-code"))
     api(project(":rule-linter:rule-sql"))
-    api(project(":rule-linter:rule-test-code"))
+    api(project(":rule-linter:rule-test"))
     api(project(":rule-linter:rule-webapi"))
 
     api("org.jetbrains.kotlin:kotlin-compiler:1.6.21")
