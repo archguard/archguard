2fc389750aea50bc1ade8017562e20631f2ab473
Phodal Huang <h@phodal.com>
2022-06-08 08:50:22 +0800

    feat(layer): add lost slot logic

--- rule-linter/rule-layer/src/main/kotlin/org/archguard/linter/rule/layer/LayerRuleSlot.kt
+++ rule-linter/rule-layer/src/main/kotlin/org/archguard/linter/rule/layer/LayerRuleSlot.kt
@@ -1,27 +1,28 @@
 package org.archguard.linter.rule.layer
 
+import chapi.domain.core.CodeDataStruct
 import org.archguard.meta.Coin
 import org.archguard.meta.Materials
 import org.archguard.meta.OutputType
 import org.archguard.meta.Slot
+import org.archguard.rule.core.Issue
+import org.archguard.rule.core.RuleSet
 
-class LayerRuleSlot: Slot {
-    override var material: Materials
-        get() = TODO("Not yet implemented")
-        set(value) {}
-    override var outClass: String
-        get() = TODO("Not yet implemented")
-        set(value) {}
+class LayerRuleSlot : Slot {
+    override var material: Materials = listOf()
+    override var outClass: String = Issue.Companion::class.java.name
 
     override fun ticket(): Coin {
-        TODO("Not yet implemented")
+        return listOf(CodeDataStruct::class.java.name)
     }
 
     override fun prepare(items: List<Any>): List<Any> {
-        TODO("Not yet implemented")
+        val ruleSets = listOf(LayerRuleSetProvider().get())
+        this.material = ruleSets
+        return ruleSets
     }
 
     override fun process(items: List<Any>): OutputType {
-        TODO("Not yet implemented")
+        return LayerRuleVisitor(items as List<CodeDataStruct>).visitor(this.material as Iterable<RuleSet>)
     }
 }
