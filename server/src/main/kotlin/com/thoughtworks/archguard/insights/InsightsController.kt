6f6fb01e8f9483608306189b2be498779fc08939
Phodal Huang <h@phodal.com>
2022-06-20 21:00:54 +0800

    feat(insights): init controller

---
+++ server/src/main/kotlin/com/thoughtworks/archguard/insights/InsightsController.kt
@@ -0,0 +1,13 @@
+package com.thoughtworks.archguard.insights
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/insights")
+class InsightsController {
+    // query by expression with cron config
+    // count after DSL with kotlin scripting ?
+
+    // save by expression and to influx db
+}
