3743f1ddb6cad5b278ee6c214ab76076f811c3f7
Phodal Huang <h@phodal.com>
2022-06-21 15:48:05 +0800

    refactor(insights): change dsl design for insight #95

--- architecture-as-code/dsl/src/main/kotlin/org/archguard/dsl/insight/InsightDecl.kt
+++ architecture-as-code/dsl/src/main/kotlin/org/archguard/dsl/insight/InsightDecl.kt
@@ -2,26 +2,32 @@ package org.archguard.dsl.insight
 
 import org.archguard.dsl.context
 
+class InsightCondition(
+    val type: String = "regexp",
+    val value: String = "",
+)
+
+fun regexp(value: String): InsightCondition {
+    return InsightCondition("regex", value)
+}
+
 class InsightDecl() {
+    private lateinit var _fieldCondition: InsightCondition
     private var _field: String = ""
     private var _name: String = ""
-    private var _version: String = ""
-    private var _comparison: String = ""
+    private var _condition: String = ""
 
-    fun comparison(comparisonStr: String) {
-        this._comparison = comparisonStr
-    }
-
-    fun version(versionStr: String) {
-        this._version = versionStr
-    }
-
-    fun field(typeStr: String) {
-        this._field = typeStr
+    fun condition(comparisonStr: String) {
+        this._condition = comparisonStr
     }
 
     fun name(name: String) {
         this._name = name
+    }
+
+    fun field(typeStr: String, function: () -> InsightCondition) {
+        this._field = typeStr
+        this._fieldCondition = function()
     }
 }
 
--- architecture-as-code/dsl/src/test/kotlin/org/archguard/dsl/InsightTest.kt
+++ architecture-as-code/dsl/src/test/kotlin/org/archguard/dsl/InsightTest.kt
@@ -1,6 +1,7 @@
 package org.archguard.dsl
 
 import org.archguard.dsl.insight.insight
+import org.archguard.dsl.insight.regexp
 import org.junit.jupiter.api.Test
 
 internal class InsightTest {
@@ -8,9 +9,12 @@ internal class InsightTest {
     internal fun basic_insight() {
         val insight = insight {
             name("demo")
-            comparison(">=")
-            version("1.2.3")
-            field("version")
+            condition(">= 1.2.3")
+            field("version") {
+                regexp("org.apache.logging.log4j")
+            }
         }
+
+        println(insight)
     }
 }
