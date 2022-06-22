7fd5d93a36bcb9be2a2a319c3dcf96deb6640806
Phodal Huang <h@phodal.com>
2022-06-22 16:03:48 +0800

    refactor(insights): add lost tests #95

--- server/src/main/kotlin/com/thoughtworks/archguard/insights/domain/ScaModelDto.kt
+++ server/src/main/kotlin/com/thoughtworks/archguard/insights/domain/ScaModelDto.kt
@@ -1,9 +1,10 @@
 package com.thoughtworks.archguard.insights.domain
 
 data class ScaModelDto(
+    // name = `artifact.group`
     val dep_name: String,
-    val dep_artifact: String,
     val dep_group: String,
+    val dep_artifact: String,
     val dep_version: String,
 ) {
 }
--- server/src/test/kotlin/com/thoughtworks/archguard/insights/domain/ScaInsightFilterTest.kt
+++ server/src/test/kotlin/com/thoughtworks/archguard/insights/domain/ScaInsightFilterTest.kt
@@ -1,10 +1,19 @@
 package com.thoughtworks.archguard.insights.domain
 
+import org.archguard.domain.insight.InsightModel
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class ScaInsightFilterTest {
     @Test
-    internal fun name() {
+    internal fun should_filter_version_by_condition() {
+        val insights = InsightModel.parse("field:name == /.*dubbo/ field:version > 1.12.3")
+        val dtos = listOf(
+            ScaModelDto("org.apache.dubbo:dubbo", "org.apache.dubbo", "dubbo", "1.12.2"),
+            ScaModelDto("spring", "spring", "spring", "1.12.4"),
+            ScaModelDto("org.apache.dubbo:dubbo", "org.apache.dubbo", "dubbo", "1.12.4"),
+        )
 
+        assertEquals(1, ScaInsightFilter.filterByInsight(insights, dtos).size)
     }
 }
