f80cf46c9bc1a26f94dc57f0d908e7bf258d11fd
Phodal Huang <h@phodal.com>
2022-06-25 11:50:49 +0800

    fix(sca): update name length && fixed #84

---
+++ server/src/main/resources/db/migration/V202206250002__sca_name_length.sql
@@ -0,0 +1,2 @@
+alter table project_composition_dependencies change column name name varchar(255) not null;
+alter table project_composition_dependencies change column version version varchar(255) not null;
