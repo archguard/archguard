a32b8044ed0b4cf68c36766884a1c94ae2e88f67
Anddd7 <liaoad_sapce@sina.com>
2022-07-02 22:18:33 +0800

    test: fix test for streamchannel

--- server/src/test/kotlin/com/thoughtworks/archguard/v2/backyard/streamchannel/StreamChannelTest.kt
+++ server/src/test/kotlin/com/thoughtworks/archguard/v2/backyard/streamchannel/StreamChannelTest.kt
@@ -1,8 +1,6 @@
 package com.thoughtworks.archguard.v2.backyard.streamchannel
 
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 import org.archguard.scanner.core.event.AnalyserEvent
 import org.archguard.scanner.core.event.AnalyserEventType
 import org.junit.jupiter.api.Test
@@ -13,9 +11,11 @@ internal class StreamChannelTest {
     fun `should trigger related subscribers when publish a new event`() {
         val subscriber1 = mockk<AnalyserEventSubscriber> {
             every { this@mockk.type } returns AnalyserEventType.CODE_DATA_STRUCT
+            every { process(any()) } just Runs
         }
         val subscriber2 = mockk<AnalyserEventSubscriber> {
             every { this@mockk.type } returns AnalyserEventType.API_CONTAINERS
+            every { process(any()) } just Runs
         }
         val event = mockk<AnalyserEvent> {
             every { this@mockk.type } returns AnalyserEventType.CODE_DATA_STRUCT
