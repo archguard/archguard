4edbe3549336b5c4d15f6001d687e54c5c500cce
Jason <m.jason.liu@outlook.com>
2022-08-24 14:56:52 +0800

    test(insights): enable filter_version_by_condition && remove TODO

--- server/src/test/kotlin/com/thoughtworks/archguard/insights/application/ScaFieldFilterTest.kt
+++ server/src/test/kotlin/com/thoughtworks/archguard/insights/application/ScaFieldFilterTest.kt
@@ -2,14 +2,11 @@ package com.thoughtworks.archguard.insights.application
 
 import com.thoughtworks.archguard.insights.application.sca.ScaInsightFilter
 import org.archguard.domain.insight.InsightsParser
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 internal class ScaFieldFilterTest {
-    // TODO(CGQAQ): Supporting verison in postqueries as well
     @Test
-    @Disabled
     fun filter_version_by_condition() {
         // Postqueries only
         val insights = InsightsParser.parse("dep_name = /.*dubbo/ and dep_version > 1.12.3")
