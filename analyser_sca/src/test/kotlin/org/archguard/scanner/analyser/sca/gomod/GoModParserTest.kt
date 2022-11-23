5d6a9d3384c0b334717ece0d8331ccf440f29a96
Phodal Huang <h@phodal.com>
2022-11-23 10:38:14 +0800

    docs(sca): fix tests

--- analyser_sca/src/test/kotlin/org/archguard/scanner/analyser/sca/gomod/GoModParserTest.kt
+++ analyser_sca/src/test/kotlin/org/archguard/scanner/analyser/sca/gomod/GoModParserTest.kt
@@ -3,6 +3,17 @@ package org.archguard.scanner.analyser.sca.gomod
 import org.archguard.scanner.core.sca.DeclFileTree
 
 internal class GoModParserTest {
+    val modSample = """module github.com/boyter/scc/v3
+
+go 1.14
+
+require (
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
+	golang.org/x/text v0.3.0
+	gopkg.in/yaml.v2 v2.2.8
+)
+"""
 
     @org.junit.jupiter.api.Test
     fun basicLookupSource() {
