0ebb340ce258c7ef9a8f792d3bb6bc3302f90296
Phodal Huang <h@phodal.com>
2022-11-23 10:32:48 +0800

    docs(sca): add some refs

--- analyser_sca/src/test/kotlin/org/archguard/scanner/analyser/sca/gomod/GoModParserTest.kt
+++ analyser_sca/src/test/kotlin/org/archguard/scanner/analyser/sca/gomod/GoModParserTest.kt
@@ -6,19 +6,7 @@ internal class GoModParserTest {
 
     @org.junit.jupiter.api.Test
     fun basicLookupSource() {
-        val modSample = """module github.com/boyter/scc/v3
-
-go 1.14
-
-require (
-	github.com/spf13/cobra v0.0.3
-	github.com/spf13/pflag v1.0.3 // indirect
-	golang.org/x/text v0.3.0
-	gopkg.in/yaml.v2 v2.2.8
-)
-"""
-
-        val declFileTree = DeclFileTree("coca", "mo.md", modSample)
+        val declFileTree = DeclFileTree("coca", "god.md", modSample)
         val depDecls = GoModParser().lookupSource(declFileTree)
     }
 }
