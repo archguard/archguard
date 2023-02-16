b0a2943e12ebc7d7ace64f0a2bf357ecdc8d0426
Phodal Huang <h@phodal.com>
2023-02-16 15:55:44 +0800

    chore: update chapi to 2.1.2 and fix git differ error

--- analyser_diff_changes/src/main/kotlin/org/archguard/scanner/analyser/diffchanges/GitDiffer.kt
+++ analyser_diff_changes/src/main/kotlin/org/archguard/scanner/analyser/diffchanges/GitDiffer.kt
@@ -24,7 +24,7 @@ import java.nio.charset.StandardCharsets
 @Serializable
 class DifferFile(
     val path: String,
-    val dataStructs: Array<CodeDataStruct>,
+    val dataStructs: List<CodeDataStruct>,
 )
 
 class ChangedEntry(
@@ -174,7 +174,7 @@ class GitDiffer(val path: String, val branch: String, val loopDepth: Int) {
 
             val blobId = treeWalk.getObjectId(0)
 
-            val newDataStructs: Array<CodeDataStruct>
+            val newDataStructs: List<CodeDataStruct>
 
             val extension = File(filePath).extension
             newDataStructs = diffFileFromBlob(repository, blobId, filePath, extension)
@@ -194,12 +194,12 @@ class GitDiffer(val path: String, val branch: String, val loopDepth: Int) {
                         // in first version, if field changed, just make data structure change will be simple
                         if (ds.Fields.size != oldDataStructs[index].Fields.size) {
                             this.changedClasses[filePath] = ChangedEntry(filePath, filePath, ds.Package, ds.NodeName)
-                        } else if (!ds.Fields.contentEquals(oldDataStructs[index].Fields)) {
+                        } else if (ds.Fields != oldDataStructs[index].Fields) {
                             this.changedClasses[filePath] = ChangedEntry(filePath, filePath, ds.Package, ds.NodeName)
                         }
 
                         // compare for function sizes
-                        if (!ds.Functions.contentEquals(oldDataStructs[index].Functions)) {
+                        if (ds.Functions != oldDataStructs[index].Functions) {
                             val difference = ds.Functions.filterNot { oldDataStructs[index].Functions.contains(it) }
                             difference.forEach {
                                 this.changedFunctions[filePath] =
@@ -267,7 +267,7 @@ class GitDiffer(val path: String, val branch: String, val loopDepth: Int) {
         blobId: ObjectId,
         pathString: String,
         extension: String
-    ): Array<CodeDataStruct> {
+    ): List<CodeDataStruct> {
         val fileName = File(pathString).name
         val content = repository.newObjectReader().use { objectReader ->
             String(objectReader.open(blobId).bytes, StandardCharsets.UTF_8)
@@ -277,19 +277,21 @@ class GitDiffer(val path: String, val branch: String, val loopDepth: Int) {
                 val analyser = chapi.ast.kotlinast.KotlinAnalyser()
                 analyser.analysis(content, fileName, ParseMode.Full).run {
                     DataStructures.map { it.apply { it.FilePath = pathString } }
-                }.toTypedArray()
+                }
             }
+
             "java" -> {
                 val analyser = chapi.ast.javaast.JavaAnalyser()
                 val basicNodes = analyser.identBasicInfo(content, fileName).run {
                     DataStructures.map { ds -> ds.apply { ds.Imports = Imports } }
-                }.toTypedArray()
-                val classes = basicNodes.map { it.getClassFullName() }.toTypedArray()
+                }
+                val classes = basicNodes.map { it.getClassFullName() }
                 analyser.identFullInfo(content, fileName, classes, basicNodes).run {
                     DataStructures.map { ds -> ds.apply { ds.Imports = Imports } }
-                }.toTypedArray()
+                }
             }
-            else -> emptyArray()
+
+            else -> emptyList()
         }
     }
 
