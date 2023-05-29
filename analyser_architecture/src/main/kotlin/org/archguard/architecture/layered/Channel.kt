7790d2833dc3f2b99c9a5b4599a786dc09f10b54
Phodal Huang <h@phodal.com>
2023-05-30 00:09:33 +0800

    feat: make all values method

--- analyser_architecture/src/main/kotlin/org/archguard/architecture/layered/Channel.kt
+++ analyser_architecture/src/main/kotlin/org/archguard/architecture/layered/Channel.kt
@@ -1,20 +1,26 @@
 package org.archguard.architecture.layered
 
-enum class ChannelType {
-    WEBSITE,
-    MOBILE_APP,
-    PHONE,
-    EMAIL,
-    SOCIAL_MEDIA,
-    ONLINE_CHAT,
-    IN_PERSON_STORE,
-    SELF_SERVICE_KIOSK,
-    VOICE_ASSISTANT,
-    VIDEO_CONFERENCING,
-    SMS,
-    FAX,
-    MAIL,
-    INTERACTIVE_VOICE_RESPONSE,
-    VIRTUAL_REALITY,
-    AUGMENTED_REALITY
+enum class ChannelType(val displayName: String) {
+    WEBSITE("Website"),
+    MOBILE_APP("Mobile App"),
+    PHONE("Phone"),
+    EMAIL("Email"),
+    SOCIAL_MEDIA("Social Media"),
+    ONLINE_CHAT("Online Chat"),
+    IN_PERSON_STORE("In-person Store"),
+    SELF_SERVICE_KIOSK("Self-service Kiosk"),
+    VOICE_ASSISTANT("Voice Assistant"),
+    VIDEO_CONFERENCING("Video Conferencing"),
+    SMS("SMS"),
+    FAX("Fax"),
+    MAIL("Mail"),
+    INTERACTIVE_VOICE_RESPONSE("Interactive Voice Response"),
+    VIRTUAL_REALITY("Virtual Reality"),
+    AUGMENTED_REALITY("Augmented Reality");
+
+    companion object {
+        fun allValues(): List<String> {
+            return values().map { it.displayName }
+        }
+    }
 }
