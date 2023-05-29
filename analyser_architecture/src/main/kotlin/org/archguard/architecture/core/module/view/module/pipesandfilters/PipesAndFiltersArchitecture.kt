464a292dafffb7e6f7a75bdf42a939ae336f64d3
Phodal Huang <h@phodal.com>
2023-05-29 23:44:05 +0800

    feat: add for pipe and filters arch

--- analyser_architecture/src/main/kotlin/org/archguard/architecture/core/module/view/module/pipesandfilters/PipesAndFiltersArchitecture.kt
+++ analyser_architecture/src/main/kotlin/org/archguard/architecture/core/module/view/module/pipesandfilters/PipesAndFiltersArchitecture.kt
@@ -1,5 +1,27 @@
 package org.archguard.architecture.core.module.view.module.pipesandfilters
 
+import org.archguard.architecture.core.module.view.module.DependencyType
 import org.archguard.architecture.core.module.view.module.ModuleArchitecture
 
-class PipesAndFiltersArchitecture : ModuleArchitecture
+class PipesAndFiltersArchitecture : ModuleArchitecture {
+    val pipes = mutableListOf<Pipe>()
+    val filters = mutableListOf<Filter>()
+    val dependencies = mutableListOf<Dependency>()
+}
+
+class Pipe(val name: String, val type: PipeType)
+
+enum class PipeType {
+    Data,
+    Control
+}
+
+class Filter(val name: String, val type: FilterType)
+
+enum class FilterType {
+    Transform,
+    Source,
+    Sink
+}
+
+class Dependency(val type: DependencyType, val dependent: String, val dependence: String)
