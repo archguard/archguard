b7805216e7ec0acea7b89001f8e06816e51260da
Phodal Huang <h@phodal.com>
2023-09-16 21:40:03 +0800

    chore: release for 2.0.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
     apply(plugin = "org.jetbrains.dokka")
 
     group = "org.archguard.scanner"
-    version = "2.0.6"
+    version = "2.0.7"
 
     java.sourceCompatibility = JavaVersion.VERSION_1_8
     java.targetCompatibility = JavaVersion.VERSION_1_8
