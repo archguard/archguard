18e6128ced6b67ec0aeba1a63a8f0bd4e86ec42f
Phodal Huang <h@phodal.com>
2023-12-07 13:37:01 +0800

    refactor: clean comments

--- rule-core/src/main/kotlin/org/archguard/rule/core/RuleVisitor.kt
+++ rule-core/src/main/kotlin/org/archguard/rule/core/RuleVisitor.kt
@@ -5,15 +5,4 @@ package org.archguard.rule.core
  */
 abstract class RuleVisitor(val data: Iterable<Any>) {
     abstract fun visitor(ruleSets: Iterable<RuleSet>): List<Issue>
-    // for disabled rules, we can set in backend.
-    // abstract fun concurrentVisitor(enabledRules: Map<String, Rule>, ruleSets: Iterable<RuleSet>): List<Issue>
-
-//    // accept rules to continue process
-//    override fun prepare(items: List<Any>): List<RuleSet> {
-//        return listOf()
-//    }
-//
-//    override fun process(items: List<Any>): List<Issue> {
-//        return this.visitor(items as Iterable<RuleSet>)
-//    }
 }
