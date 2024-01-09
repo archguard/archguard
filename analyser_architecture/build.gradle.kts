b58309ef5242525bc6d3e0d09ea958bf7ff4270e
Phodal Huang <h@phodal.com>
2024-01-09 20:34:56 +0800

    feat(build): add shadowJar task to build.gradle.kts
    Add shadowJar task to the build.gradle.kts file in analyser_architecture module. The shadowJar task is used to create a fat JAR file that includes all dependencies. This task is configured to exclude Kotlin and Kotlinx dependencies and minimize the resulting JAR file.

--- analyser_architecture/build.gradle.kts
+++ analyser_architecture/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     application
     alias(libs.plugins.jvm)
     alias(libs.plugins.serialization)
+    alias(libs.plugins.shadow)
 }
 
 dependencies {
@@ -11,4 +12,18 @@ dependencies {
     implementation(projects.scannerCore)
 
     testImplementation(libs.bundles.test)
+}
+
+application {
+    mainClass.set("org.archguard.scanner.core.AnalyserKt")
+}
+
+tasks {
+    shadowJar {
+        dependencies {
+            exclude(dependency("org.jetbrains.kotlin:.*:.*"))
+            exclude(dependency("org.jetbrains.kotlinx:.*:.*"))
+        }
+        minimize()
+    }
 }
