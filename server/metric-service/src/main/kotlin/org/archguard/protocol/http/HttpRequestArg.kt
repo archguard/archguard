e363e921898ce1eff5e98364a00ce3e707784075
Phodal Huang <h@phodal.com>
2024-01-09 12:20:48 +0800

    test: fix tests

--- server/metric-service/src/main/kotlin/org/archguard/protocol/http/HttpRequestArg.kt
+++ server/metric-service/src/main/kotlin/org/archguard/protocol/http/HttpRequestArg.kt
@@ -5,8 +5,7 @@ import org.archguard.json.JsonUtils
 class HttpRequestArg(var paths: List<String> = mutableListOf(), var methods: List<String> = mutableListOf()) {
     constructor(values: Map<String, String>) : this() {
         paths = JsonUtils.json2obj(values.getOrDefault("value", JsonUtils.obj2json("")))
-        val temp: List<List<String>> =
-            JsonUtils.json2obj(values.getOrDefault("method", JsonUtils.obj2json(listOf(listOf("", "get")))))
+        val temp: List<List<String>> = JsonUtils.json2obj(values.getOrDefault("method", JsonUtils.obj2json(listOf(listOf("", "GET")))))
         methods = temp.map { it[1] }
     }
 }
