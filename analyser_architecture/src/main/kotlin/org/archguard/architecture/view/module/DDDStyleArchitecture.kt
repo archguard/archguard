120cd07eaaf531688e3584e3985ca917a40aeb98
Phodal Huang <h@phodal.com>
2024-01-11 15:48:00 +0800

    refactor(view): update DDDLayered and DDDStyleArchitecture
    - Update DDDLayered and DDDStyleArchitecture classes to use CodeDataStruct instead of specific classes for aggregates, repositories, domain services, factories, infrastructure, application, and interface layers.
    - This change allows for more flexibility and reusability in the codebase.

--- analyser_architecture/src/main/kotlin/org/archguard/architecture/view/module/DDDStyleArchitecture.kt
+++ analyser_architecture/src/main/kotlin/org/archguard/architecture/view/module/DDDStyleArchitecture.kt
@@ -1,19 +1,17 @@
 package org.archguard.architecture.view.module
 
+import chapi.domain.core.CodeDataStruct
 import kotlinx.serialization.Serializable
 import org.archguard.architecture.detect.PotentialExecArch
-import org.archguard.architecture.view.module.ddd.ApplicationLayer
 import org.archguard.architecture.view.module.ddd.DomainLayer
-import org.archguard.architecture.view.module.ddd.InfrastructureLayer
-import org.archguard.architecture.view.module.ddd.InterfaceLayer
 import org.archguard.architecture.view.module.shared.Dependency
 
 @Serializable
 class DDDStyleArchitecture(
-    val infra: InfrastructureLayer,
-    val application: ApplicationLayer,
+    val infra: List<CodeDataStruct>,
+    val application: List<CodeDataStruct>,
     val domainLayer: DomainLayer,
-    val interfaceLayer: InterfaceLayer,
+    val interfaceLayer: List<CodeDataStruct>,
     val dependencies: List<Dependency>
 ) : ArchitectureStyle {
     override fun buildArchitecture(identPotential: PotentialExecArch) {
--- analyser_architecture/src/main/kotlin/org/archguard/architecture/view/module/ddd/DDDLayered.kt
+++ analyser_architecture/src/main/kotlin/org/archguard/architecture/view/module/ddd/DDDLayered.kt
@@ -1,40 +1,12 @@
 package org.archguard.architecture.view.module.ddd
 
+import chapi.domain.core.CodeDataStruct
 import kotlinx.serialization.Serializable
 
 @Serializable
 class DomainLayer(
-    val aggregates: List<Aggregate>,
-    val repositories: List<Repository>,
-    val domainServices: List<DomainService>,
-    val factories: List<Factory>
+    val aggregates: List<CodeDataStruct>,
+    val repositories: List<CodeDataStruct>,
+    val domainServices: List<CodeDataStruct>,
+    val factories: List<CodeDataStruct>
 )
-
-@Serializable
-class ApplicationLayer(val applicationServices: List<ApplicationService>)
-
-@Serializable
-class InfrastructureLayer(val adaptors: List<Adaptor>)
-
-@Serializable
-class InterfaceLayer(val controllers: List<Controller>)
-
-@Serializable
-data class ApplicationService(val name: String)
-@Serializable
-data class Aggregate(val name: String)
-
-@Serializable
-data class Repository(val name: String)
-
-@Serializable
-class DomainService(val name: String)
-
-@Serializable
-data class Factory(val name: String)
-
-@Serializable
-data class Controller (val name: String)
-
-@Serializable
-data class Adaptor (val name: String)
