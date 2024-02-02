b5cdc2d2480ec46f61746afd704cdb86e1daeb3a
Phodal Huang <h@phodal.com>
2024-02-02 11:17:12 +0800

    chore: release 2.1.4

--- scanner_cli/src/main/kotlin/org/archguard/scanner/ctl/impl/OfficialAnalyserSpecs.kt
+++ scanner_cli/src/main/kotlin/org/archguard/scanner/ctl/impl/OfficialAnalyserSpecs.kt
@@ -2,7 +2,7 @@ package org.archguard.scanner.ctl.impl
 
 import org.archguard.scanner.core.AnalyserSpec
 
-const val ARCHGUARD_VERSION = "2.1.1"
+const val ARCHGUARD_VERSION = "2.1.4"
 private const val TAG = "v$ARCHGUARD_VERSION"
 private const val RELEASE_REPO_URL = "https://github.com/archguard/archguard/releases/download/$TAG"
 
