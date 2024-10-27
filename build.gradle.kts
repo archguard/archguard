35bc0314e9e43457ce0b19b462d6907b891b9161
Phodal Huang <gmszone@qq.com>
2024-10-27 13:22:43 +0800

    chore: release to 2.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
     apply(plugin = "org.jetbrains.dokka")
 
     group = "org.archguard.scanner"
-    version = "2.2.1"
+    version = "2.2.2"
 
     java.sourceCompatibility = JavaVersion.VERSION_1_8
     java.targetCompatibility = JavaVersion.VERSION_1_8
