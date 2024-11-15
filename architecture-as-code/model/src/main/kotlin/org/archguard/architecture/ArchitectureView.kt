3e857ac6db04aff33984022f747466e34762cef7
Phodal Huang <gmszone@qq.com>
2024-11-15 21:55:51 +0800

    refactor(architecture): simplify ArchitectureView constructor and imports
    Updated the ArchitectureView class by cleaning up the constructor parameters and imports. Removed unnecessary fully qualified names and default arguments for conceptArchitecture and architectureStyle. Simplified the codeArchitecture default value by removing the unnecessary parentheses.

--- architecture-as-code/model/src/main/kotlin/org/archguard/architecture/ArchitectureView.kt
+++ architecture-as-code/model/src/main/kotlin/org/archguard/architecture/ArchitectureView.kt
@@ -4,7 +4,9 @@ import kotlinx.serialization.Serializable
 import org.archguard.architecture.graph.TreeNode
 import org.archguard.architecture.view.code.CodeArchitecture
 import org.archguard.architecture.view.code.RepositoryType
+import org.archguard.architecture.view.concept.ConceptArchitecture
 import org.archguard.architecture.view.execution.ExecutionArchitecture
+import org.archguard.architecture.view.module.ArchitectureStyle
 import org.archguard.architecture.view.module.LayeredArchitecture
 import org.archguard.context.CodeDatabaseRelation
 import org.archguard.context.ContainerService
@@ -12,12 +14,10 @@ import org.archguard.context.LanguageEstimate
 
 @Serializable
 class ArchitectureView(
-    var conceptArchitecture: org.archguard.architecture.view.concept.ConceptArchitecture = org.archguard.architecture.view.concept.ConceptArchitecture(),
-    var architectureStyle: org.archguard.architecture.view.module.ArchitectureStyle = LayeredArchitecture(),
+    var conceptArchitecture: ConceptArchitecture = ConceptArchitecture(),
+    var architectureStyle: ArchitectureStyle = LayeredArchitecture(),
     var executionArchitecture: ExecutionArchitecture = ExecutionArchitecture(),
-    var codeArchitecture: CodeArchitecture = CodeArchitecture(
-        RepositoryType.Service,
-    ),
+    var codeArchitecture: CodeArchitecture = CodeArchitecture(RepositoryType.Service),
     var physicalStructure: PhysicalStructure = PhysicalStructure(),
     var outboundService: List<ContainerService> = listOf(),
 )
